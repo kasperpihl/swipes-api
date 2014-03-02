ea2c201f3471df1222c58eaab6be3f32c0160368
Kasper Pihl Tornøe <kasper@pihl.it>
2014-03-02 15:09:30 +0200

    Identifier log

--- server/logger.js
+++ server/logger.js
@@ -1,5 +1,20 @@
 var keys = require('../conf/keys.js');
+var Parse = require('parse').Parse;
+var forceLog = false;
 var live = keys.live();
+var identifier;
+function makeid(length)
+{
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for( var i=0; i < length; i++ )
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+};
 exports.log = function(message,force){
-	if(!live || force) console.log(message);
+	if(!identifier && Parse.User.current()) identifier = Parse.User.current().id;
+	if(!identifier) identifier = makeid(5);
+	if(!live || force || forceLog) console.log(message);
 };
