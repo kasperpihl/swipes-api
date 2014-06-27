7eadab8106a72cd6e9177634b1d5d1dc3051ae35
Kasper Pihl Tornøe <kasper@pihl.it>
2014-06-27 10:28:58 +0200

    Added maxconnection socket for 25

--- instance.js
+++ instance.js
@@ -1,9 +1,11 @@
 require('strong-agent').profile("4805c27d826dec99b06108df1b5dab80","SwipesAPI");
 var express =       require( 'express' ),
+    http    =       require( 'http' ),
     bodyParser =    require( 'body-parser' ),
     _ =             require( 'underscore' );
 var Parse = require('parse').Parse;
 var keys = require('./utilities/keys.js');
+http.globalAgent.maxSockets = 25;
 var app = express();
 app.use(bodyParser.json( { limit: 3000000 } ) );
 app.use(function(req, res, next) {
