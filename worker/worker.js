55d4425bc9f62591eaeaa0f1bf149f57e97e3189
Kasper Pihl Tornøe <kasper@pihl.it>
2015-05-15 09:03:57 +0300

    Made new worker file

---
+++ worker/worker.js
@@ -0,0 +1,21 @@
+//require('strong-agent').profile("4805c27d826dec99b06108df1b5dab80","SwipesAPI");
+var express =       require( 'express' ),
+    http    =       require( 'http' ),
+    bodyParser =    require( 'body-parser' ),
+    _ =             require( 'underscore' ),
+    crypto    = require('crypto');
+var Parse = require('parse').Parse;
+var keys = require('../utilities/keys.js');
+http.globalAgent.maxSockets = 25;
+
+var app = express();
+app.use(bodyParser.json( { limit: 3000000 } ) );
+
+app.route( '/').get( function(req,res,next){
+  res.send("Swipes synchronization services - online");
+});
+app.route( '/work').post( function(req,res,next){
+  console.log(req);
+});
+var port = Number(process.env.PORT || 5000);
+app.listen(port);
