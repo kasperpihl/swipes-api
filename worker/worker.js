d2e5b283dee425acd1431cfa94aa77150d3fdcc1
Kasper Pihl Tornøe <kasper@pihl.it>
2015-06-19 18:40:17 +0300

    Removed query creator

--- worker/worker.js
+++ worker/worker.js
@@ -46,3 +46,48 @@ app.route('/test').get( function(req,res,next){
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port);
+
+
+
+ /*
+getAllTasksFromServiceThatIsNotCompletedNorDeleted:(String)service ()
+getMatchingTasksFromListOfIds:(Array)ids andService:(String)service
+
+What should happen if:
+
+A user deletes a task in Swipes (that is linked to an email)
+- Remove Swipes list-label
+
+A user completes a task in Swipes (that is linked to an email)
+- Remove Swipes list-label
+
+A user moves an email from the Swipes List (that has a task in Swipes)
+- Leave the task, but unlink it
+
+Object to be created:
+
+// Generate a collection to prepare for save
+var todoCollection = new Collections.Todo();
+todoCollection.loadObjects( arrayOfJsonFormatBelow )
+var saveQueries = todoCollection.getInsertAndSaveQueries()
+pgClient.performQueries( saveQueries )
+
+json format:
+{
+	tempId: util.generateId(12)
+	order: -1
+	schedule: util.convertDate( new Date() )
+	title: thread title
+	origin: "gmail"
+	originIdentifier: "threadId"
+	attachments: [
+		{
+			service: "gmail"
+			identifier: "json:{"threadid":"14c15bfa50b6dd93","email":"spwatton@gmail.com”}”
+			title: "threadId"
+			sync: 1
+		}
+	]
+}
+
+*/
