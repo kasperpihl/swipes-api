b28f57e7233e2d9e063cc0adf7c91bb4f64ae8e5
Kasper Pihl Tornøe <kasper@pihl.it>
2015-06-20 12:23:00 +0300

    Added promise structure and comments to handler

--- worker/handlers/gmail_handler.js
+++ worker/handlers/gmail_handler.js
@@ -18,36 +18,85 @@ function GmailHandler(userId, client, logger){
 // Main function - starting and handling the whole process 
 // ===========================================================================================================
 GmailHandler.prototype.run = function(settings, action){
-	var deferred = Q.defer();
+	var deferred = Q.defer(), self = this;
 	this.settings = settings;
 	this.action = action;
 
 	var connector = new GmailConnector();
 	connector.delegate = this;
 
+	this.fetchEmails()
+	.then(function(){ return self.fetchTasks(); })
+	.then(function(){ return self.compare(); })
+	.then(function(){ return self.saveTasks(); })
+	.then(function(){ return self.saveEmails(); })
+	.then(function(){
+		// Successfully synced with Gmail
+		deferred.resolve();
+	})
+	.fail(function(error){
+		deferred.reject(error);
+	})
+	.catch(function(error){
+		deferred.reject(error);
+	});
 
 	return deferred.promise;
 }
 
 
+// ===========================================================================================================
+// Fetch emails from Gmail that fits the settings
+// ===========================================================================================================
 GmailHandler.prototype.fetchEmails = function(){
+	var deferred = Q.defer(), self = this;
 
+	deferred.resolve();
+	return deferred.promise;
 }
 
+
+// ===========================================================================================================
+// Fetch tasks from Swipes that comes from Gmail
+// ===========================================================================================================
 GmailHandler.prototype.fetchTasks = function(){
+	var deferred = Q.defer(), self = this;
 
+	deferred.resolve();
+	return deferred.promise;
 }
 
+
+// ===========================================================================================================
+// Compare the tasks and emails and prepare tasks and email to be saved
+// ===========================================================================================================
 GmailHandler.prototype.compare = function(){
+	var deferred = Q.defer(), self = this;
 
+	deferred.resolve();
+	return deferred.promise;
 }
 
+
+// ===========================================================================================================
+// Save tasks to Swipes from Gmail
+// ===========================================================================================================
 GmailHandler.prototype.saveTasks = function(){
+	var deferred = Q.defer(), self = this;
 
+	deferred.resolve();
+	return deferred.promise;
 }
 
-GmailHandler.prototype.saveEmails = function(){
 
+// ===========================================================================================================
+// Save to Gmail with changes from Swipes
+// ===========================================================================================================
+GmailHandler.prototype.saveEmails = function(){
+	var deferred = Q.defer(), self = this;
+
+	deferred.resolve();
+	return deferred.promise;
 }
 
 
