8afd4b337497334884a1e80e08cde5d0deb34909
Kasper Pihl Tornøe <kasper@pihl.it>
2015-06-20 11:28:07 +0300

    Defining initial handler structure

--- worker/handlers/gmail_handler.js
+++ worker/handlers/gmail_handler.js
@@ -1,5 +1,37 @@
+var COMMON = '../../common/';
+var Collections = 	require(COMMON + 'collections/collections.js');
+var util =			require(COMMON + 'utilities/util.js');
+var Q = require("q");
+
+function GmailHandler(userId, client, logger){
+	this.collection = new Collections.Todo();
+
+	this.logger = logger;
+	this.client = client;
+
+}
+
+GmailHandler.prototype.run = function(){
+
+}
 
 
-function GmailHandler(){
-	
+GmailHandler.prototype.fetchEmails = function(){
+
+}
+
+GmailHandler.prototype.fetchTasks = function(){
+
+}
+
+GmailHandler.prototype.compare = function(){
+
+}
+
+GmailHandler.prototype.saveTasks = function(){
+
+}
+
+GmailHandler.prototype.saveEmails = function(){
+
 }
