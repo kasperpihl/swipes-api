d4bd32a2d5ec8224a6fefc55cde3899272c18dad
Kasper Pihl Tornøe <kasper@pihl.it>
2015-10-08 17:14:15 -0700

    Tests

--- instance.js
+++ instance.js
@@ -130,6 +130,7 @@ function handleSync( req, res, next ){
     util.sendBackError({ code: 123, message: "update required" }, res );
     return;
   }
+  console.log(JSON.stringify(req.body));
 
   var logger = new Logger();
   var client = new PGClient( logger, 12000 );
@@ -184,4 +185,5 @@ function handleSync( req, res, next ){
 
 
 var port = Number(process.env.PORT || 5000);
-app.listen(port);
+//app.listen(port);
+exports = app;
