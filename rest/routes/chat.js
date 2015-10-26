67fffdff7dac98c479e099f22177e80a6f8683bd
Tihomir Valkanov <tihomir.valkanov@gmail.com>
2015-10-26 19:32:31 +0200

    make ts a string in milisecons from unix epoch + 4 random numbers

--- rest/routes/chat.js
+++ rest/routes/chat.js
@@ -10,6 +10,7 @@ let validator = require('validator');
 let util = require('../util.js');
 let db = require('../db.js');
 let generateId = util.generateSlackLikeId;
+let randomNumber = util.randomNumber;
 
 let router = express.Router();
 
@@ -31,7 +32,7 @@ router.post('/chat.send', (req, res, next) => {
     return res.status(409).json({err: 'You can\'t send an empty message!'});
   }
 
-  let ts = moment().unix();
+  let ts = moment().valueOf() + randomNumber(4) + "";
   let doc = {
     channel_id: channelId,
     user: user,
