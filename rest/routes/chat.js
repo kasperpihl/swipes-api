1262e58422af17514fcb12b3cd9f3e0316456184
Tihomir Valkanov <tihomir.valkanov@gmail.com>
2015-10-30 19:00:23 +0200

    creating DM channel when you pass userId for channel_id

--- rest/routes/chat.js
+++ rest/routes/chat.js
@@ -3,8 +3,10 @@
 const TEAM_ID = process.env.TEAM_ID;
 
 let express = require( 'express' );
+let Promise = require('bluebird');
 let getSlug = require('speakingurl');
 let r = require('rethinkdb');
+let moment = require('moment');
 let validator = require('validator');
 let util = require('../util.js');
 let db = require('../db.js');
@@ -12,6 +14,57 @@ let generateId = util.generateSlackLikeId;
 let generateSlackLikeTs = util.generateSlackLikeTs;
 
 let router = express.Router();
+
+let insertMessage = (res, next, doc) => {
+  let query = r.table('messages').insert(doc);
+
+  db.rethinkQuery(query)
+    .then((results) => {
+      res.status(200).json({ok: true});
+    }).catch((err) => {
+      return next(err);
+    });
+}
+
+let createChannel = (creatorId, receiverId) => {
+  // T_TODO check if there is a DM channel already with these users
+  return new Promise((resolve, reject) => {
+    let doc = {
+      id: generateId('D'),
+      created: moment().unix(),
+      teamId: TEAM_ID,
+      creator_id: creatorId
+    };
+
+    let createDMChannelQ =
+      r.table('channels')
+        .insert(doc, {returnChanges: true});
+
+    db.rethinkQuery(createDMChannelQ)
+      .then((inserted) => {
+        let channelId = inserted.changes[0].new_val.id;
+        let channelToAppend = {
+          id: channelId
+        };
+        let updateQ =
+          r.table('users')
+            .getAll(creatorId, receiverId)
+            .update({
+              channels: r.row('channels').append(channelToAppend)
+            })
+
+        db.rethinkQuery(updateQ)
+          .then(() => {
+            return resolve(channelId);
+          })
+          .catch((err) => {
+            return reject(err);
+          })
+      }).catch((err) => {
+        return reject(err);
+      });
+  });
+}
 
 router.post('/chat.send', (req, res, next) => {
   // T_TODO check if there is a channel with that id
@@ -35,14 +88,15 @@ router.post('/chat.send', (req, res, next) => {
     ts: ts
   };
 
-  let query = r.table('messages').insert(doc);
+  if (channelId.indexOf('U') === 0) {
+    createChannel(userId, channelId).then ((channelId) => {
+      doc.channel_id = channelId;
 
-  db.rethinkQuery(query)
-    .then((results) => {
-      res.status(200).json({ok: true});
-    }).catch((err) => {
-      return next(err);
-    });
+      insertMessage(res, next, doc);
+    })
+  } else {
+    insertMessage(res, next, doc);
+  }
 });
 
 module.exports = router;
