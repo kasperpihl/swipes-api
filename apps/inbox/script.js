07a77ad4e09ee2bb8f6b15f8744ca9fea1e3fa4d
iremlopsum <kristjanfou@gmail.com>
2015-11-03 12:34:02 +0200

    updated design for email client

--- apps/inbox/script.js
+++ apps/inbox/script.js
@@ -1,22 +1,16 @@
 var $ = window.jQuery;
-var checkbox = $('.checkbox');
-var check = $('.check');
 $(document).ready(function(){
 	parent.postMessage(JSON.stringify({ok:true}), "http://localhost:9000");
-	checkbox.on('click', function() {
-		if ($(this).children(check).hasClass('checked')) {
-			$(this).children(check).removeClass('checked');
-		} else {
-			$(this).children(check).addClass('checked');
-		}
+	
+	$('.profile').on('click', function() {
+		$('.profile-dropdown').toggleClass('active');
 	})
-
-	$('.more-checkboxes').on('click', function() {
-		if ($('.dropdown').hasClass('dropped')) {	
-			$('.dropdown').removeClass('dropped');
-		} else {	
-			$('.dropdown').addClass('dropped');		
-		}
+	
+	$('.an-email').on('click', function() {
+		$('.email-view').toggleClass('open');
+		$('.email-list').toggleClass('open');
+		$(this).toggleClass('open');
+		$('.email-one').toggleClass('open');
 	})
 })
 
