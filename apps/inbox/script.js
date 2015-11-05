36e488b7da991d053249d12957b09373f42e4157
iremlopsum <kristjanfou@gmail.com>
2015-11-05 16:39:21 +0200

    single email view and icons

--- apps/inbox/script.js
+++ apps/inbox/script.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
 	
 	var email = $('li.email');
 	var emailCard = $('.email-card');
+	var multiSelect = $('.multiple-selection');
 	var refreshEmail = $('.refresh');
 	var archiveEmail = $('.archive');
 	var deleteEmail = $('.delete');
@@ -16,42 +17,63 @@ $(document).ready(function(){
 				$(this).addClass('selected');
 				archiveEmail.add(deleteEmail).add(createTask).hide();
 				archiveEmail.add(deleteEmail).show();
+				emailCard.removeClass('open');
+				multiSelect.addClass('open');
+				var numSelectedEmails = $('.selected').length;
+				var emails = numSelectedEmails > 1 ? ' emails' : ' email';
+				$('.items-selected').html('You have selected ' + numSelectedEmails + emails);
+				$('.action-icons').removeClass('open');
 			}
   		} else { // open email
     		if ($(this).hasClass('selected')) {
 				$(this).removeClass('selected');
 				archiveEmail.add(deleteEmail).add(createTask).hide();
 				emailCard.removeClass('open');
+				$('.action-icons').removeClass('open');
 			} else {
 				archiveEmail.add(deleteEmail).add(createTask).show();
 				email.removeClass('selected');
 				$(this).addClass('selected');
 				$(this).find('.dot').removeClass('unread');
 				emailCard.addClass('open');
+				multiSelect.removeClass('open');
+				$('.items-selected').html('')
+				$('.action-icons').addClass('open');
 			}
  		 }
 	})
 	
 	archiveEmail.on('click', function() {
 		var numSelectedEmails = $('.selected').length;
+		var emails = numSelectedEmails > 1 ? ' emails' : ' email';
 		 $('li.email.selected').fadeOut( "slow", function() {
 			 emailCard.removeClass('open');
 			 archiveEmail.add(deleteEmail).add(createTask).hide();
-			 $('.action-noti').addClass('notify').html('<p> ' + numSelectedEmails + ' emails archived</p>').delay(3000).queue(function() {
+			 
+			 $('.action-noti').addClass('notify').html('<p> ' + numSelectedEmails + emails + ' archived</p>').delay(3000).queue(function() {
 				 $('.action-noti').empty().removeClass('notify');
 			 });
 		});
+		
+		multiSelect.removeClass('open');
+		$('.items-selected').html('');
+		$('.action-icons').removeClass('open');
 	})
 	
 	deleteEmail.on('click', function() {
 		var numSelectedEmails = $('.selected').length;
+		var emails = numSelectedEmails > 1 ? ' emails' : ' email';
 		 $('li.email.selected').fadeOut( "slow", function() {
 			 emailCard.removeClass('open');
 			 archiveEmail.add(deleteEmail).add(createTask).hide();
-			 $('.action-noti').addClass('notify').html('<p> ' + numSelectedEmails + ' emails deleted</p>').delay(3000).queue(function() {
+			 $('.action-noti').addClass('notify').html('<p> ' + numSelectedEmails + emails + ' deleted</p>').delay(3000).queue(function() {
 				 $('.action-noti').empty().removeClass('notify');
 			 });
 		});
+		
+		multiSelect.removeClass('open');
+		$('.items-selected').html('');
+		$('.action-icons').removeClass('open');
 	})
 	
 	refreshEmail.on('click', function() {
@@ -60,6 +82,9 @@ $(document).ready(function(){
 			$('.new-email').css('display', 'flex');
 		})
 	})
+	
+	multiSelect.removeClass('open');
+	$('.items-selected').html('')
 })
 
 
