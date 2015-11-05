ed96509ca453140af1437c07f59577e332d4cdaa
Tihomir Valkanov <tihomir.valkanov@gmail.com>
2015-11-05 17:58:09 +0200

    stars added and removed events

--- rest/socketio/socketio.js
+++ rest/socketio/socketio.js
@@ -5,6 +5,7 @@ const TEAM_ID = process.env.TEAM_ID;
 let channels = require('./channels-io.js').channelChanges;
 let messages = require('./messages-io.js').channelMessages;
 let im = require('./im-io.js');
+let stars = require('./stars-io.js');
 
 module.exports = (io) => {
   io.sockets.on('connection', (socket) => {
@@ -21,5 +22,6 @@ module.exports = (io) => {
     messages(socket, userId);
     im.channelsIm(socket, userId);
     im.userIm(socket, userId);
+    stars.stars(socket, userId);
   });
 };
---
+++ rest/socketio/stars-io.js
@@ -0,0 +1,54 @@
+"use strict";
+
+let r = require('rethinkdb');
+let db = require('../db.js');
+
+let stars = (socket, userId) => {
+  let listenQ =
+    r.table('stars')
+      .filter((star) => {
+        return star('user_id').eq(userId)
+      })
+      .changes()
+
+  db.rethinkQuery(listenQ, {feed: true})
+    .then((cursor) => {
+      cursor.each((err, row) => {
+        if (err) {
+          console.log(err);
+          // T_TODO how to handle erros here?!
+          // Sending error message on the socket?
+          return;
+        }
+
+        let data;
+        let type;
+
+        if (!row.old_val) {
+          let n = row.new_val;
+
+          type = 'star_added';
+          data = {
+            channel_id: n.channel_id,
+            type: 'channel'
+          };
+        }
+
+        if (!row.new_val) {
+          let o = row.old_val;
+
+          type = 'star_removed';
+          data = {
+            channel_id: o.channel_id,
+            type: 'channel'
+          };
+        }
+
+        socket.emit('message', {type: type, data: data});
+      })
+    })
+}
+
+module.exports = {
+  stars: stars
+}
