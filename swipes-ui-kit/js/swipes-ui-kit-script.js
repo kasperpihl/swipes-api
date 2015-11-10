27c92c7f55bb8a6c0e0348b927b7c97ac698fd8a
iremlopsum <kristjanfou@gmail.com>
2015-11-10 13:30:27 +0200

    input with a floating label and input range

--- swipes-ui-kit/js/swipes-ui-kit-script.js
+++ swipes-ui-kit/js/swipes-ui-kit-script.js
@@ -1,3 +1,35 @@
+$(document).ready(function() {
+	// Set the width and the height of the floating-label container for label positioning
+	var sizeW = $('.floating-label').children('input').width();
+	var sizeH = $('.floating-label').children('input').height();
+	$('.floating-label').css('width', sizeW);	
+	$('.floating-label').css('height', sizeH);
+})
+
+// Floating the label
+$('.floating-input').on('click', function() {
+	$(this).next('label').addClass('active');
+})
+
+// Remove active class from floating label if nothing typed in the input
+$(document).mouseup(function (e) {
+   var container = $('#email');
+
+   if (!container.is(e.target) && container.has(e.target).length === 0) {
+      
+		var valueOfCont = container.val();
+		if (valueOfCont.length > 0) {
+			
+		} else {
+			$('label').removeClass('active');
+		}
+		
+   } else {
+		
+	}
+});
+
+// button.primary hover
 $('.hover-full').mouseenter(function() {
 	var borderColor = $(this).css('border-color');
 	$(this).css('background-color', borderColor);	
@@ -9,6 +41,7 @@ $('.hover-full').mouseenter(function() {
 	$(this).css('color', borderColor);
 })
 
+// button.outline hover
 $('.hover-lighten').mouseenter(function() {
 	var bgColor = $(this).css('background-color');
 	var bgColorRGBA = $(this).css('background-color').replace(')', ', 0.75)').replace('rgb', 'rgba');
