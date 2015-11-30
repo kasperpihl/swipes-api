0b7b62617b45489e49ee301dca3d01a66fe05ac8
Kasper Pihl Tornøe <kasper@pihl.it>
2015-11-30 17:33:59 +0200

    Added stanimir data stuff for adding button

--- apps/stanimir/script.js
+++ apps/stanimir/script.js
@@ -6,7 +6,21 @@ $(function(){
 	}).then(function(){
 
 	});
-
+	swipes.navigation.setButtons([
+		{
+			id: "add_button",
+			title: "Add"
+		},
+		{
+			id: "delete_button",
+			title: "Delete"
+		}
+	]);
+	swipes.navigation.onButtonPressed(function(data){
+		console.log("button was pressed");
+		console.log(data);
+	});
+	
 	swipes.navigation.push("New title here");
 
 	swipes.navigation.onPop(function(){
