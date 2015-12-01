760a566f03ff0c887cca19203885b1aa3296b902
iremlopsum <kristjanfou@gmail.com>
2015-12-01 11:38:37 +0200

    Adding stuff to extra message

--- apps/chat2/app/scripts/components/chat_item.jsx
+++ apps/chat2/app/scripts/components/chat_item.jsx
@@ -2,9 +2,11 @@ var React = require('react');
 
 var ChatItem = React.createClass({
 	render: function() {
-
+		var className = "chat-message ";
+		if(this.props.data.isExtraMessage)
+			className += "extra-message ";
 		return (
-			<div className="chat-message">
+			<div className={className}>
 				<div className="chat-avatar">
 					<div className="avatar">
 						<img src="https://unsplash.it/50/?random" />
@@ -12,13 +14,14 @@ var ChatItem = React.createClass({
 				</div>
 				<div className="chat-content">
 					<div className="chat-top-side-container">
-						<p className="name">{this.props.data.user.name} <span className="chat-timestamp">{this.props.data.timeStr}22:14</span></p>
+						<p className="name">{this.props.data.user.name} <span className="chat-timestamp">{this.props.data.timeStr}</span></p>
 					</div>
-					<div className="chat-bottom-side-container">
+					<div className="chat-bottom-side-container" data-time={this.props.data.timeStr}>
 						<p className="message-container" dangerouslySetInnerHTML={{__html: this.props.data.text}}/>
 					</div>
 				</div>
 			</div>
+			
 		);
 	}
 });
