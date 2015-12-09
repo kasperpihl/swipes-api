d92b2ebc4b38f36ed0ad46efc243f9cc41fe9940
iremlopsum <kristjanfou@gmail.com>
2015-12-09 13:51:18 +0200

    login screen UI update

--- webapp/app/scripts/components/login.jsx
+++ webapp/app/scripts/components/login.jsx
@@ -16,6 +16,11 @@ var Login = React.createClass({
 			return this.transitionTo('/');
 		}
 	},
+	componentDidMount:function() {
+		$(document).ready(function() {
+			$.swFloatingLabelInput();
+		})
+	},
 	componentWillMount:function(){
 		this.listenTo(stateStore, this.onStateChange, this.onStateChange);
 	},
@@ -63,16 +68,24 @@ var Login = React.createClass({
 	render: function() {
 
 		return (
-			<div className="login">
-
-				<div className="input-wrapper" error-attribute="">
-					<input ref="username" type="text" className="username borderless" placeholder="Enter your username" />
+			<div className="login-wrapper">
+				
+				<div className="login">
+					<h5>Hi, logging in?</h5>
+					<div className="swipes-floating-label" data-error-message="Error message">
+						<input ref="username" className="swipes-floating-input username borderless" type="text" id="email"/>
+						<label for="email">Enter your username</label>
+					</div>
+					
+					{/* <div className="input-wrapper" error-attribute="">
+						<input ref="username" type="text" className="username borderless" placeholder="Enter your username" />
+					</div>*/} 
+					<div >
+						<input type="radio" name="choice" defaultChecked/>Login
+						<input ref="create" type="radio" id="create-choice" name="choice" />Create Account
+					</div>
+					<input type="button" onClick={this.onContinue} value="Sign in" />
 				</div>
-				<div >
-					<input type="radio" name="choice" defaultChecked/>Login
-					<input ref="create" type="radio" id="create-choice" name="choice" />Create Account
-				</div>
-				<input type="button" onClick={this.onContinue} value="Continue" />
 			</div>
 		);
 	}
