addfc71af3113ae1e7d47ebe4a933e4c2cd8c93e
iremlopsum <kristjanfou@gmail.com>
2015-12-18 16:15:50 +0200

    possible fix for blurring chat input focus on sending message on mobile

--- apps/chat/app/scripts/components/chat_input.jsx
+++ apps/chat/app/scripts/components/chat_input.jsx
@@ -65,12 +65,16 @@ var ChatInput = React.createClass({
 		}
 		this.debouncedCheck();
 	},
-	onClick: function(){
+	onClick: function(e){
 		var $textarea = $(this.refs.textarea);
 		var message = $textarea.val();
 		if(message && message.length > 0){
 			this.sendMessage(message);
+			$textarea.focus();
 		}
+	},
+	onFocus: function() {
+		return false;
 	},
 	sendMessage: function(message){
 		$(this.refs.textarea).val("");
@@ -106,7 +110,7 @@ var ChatInput = React.createClass({
 				<i className="material-icons chat-input-attach-icon" >attach_file</i>
 				<div className="hint">Write message</div>
 				<textarea ref="textarea" data-autoresize tabIndex="1" onBlur={this.onBlur} onChange={this.onTextChange} onKeyDown={this.onKeyDown} onKeyUp={this.onKeyUp} id="new-message-textarea" rows="1"></textarea>  
-				<i className="material-icons chat-input-mobile-send" onClick={this.onClick}>send</i>
+				<i className="material-icons chat-input-mobile-send" onClick={this.onClick} onFocus={this.onFocus}>send</i>
 			</div>
 		);
 	}
