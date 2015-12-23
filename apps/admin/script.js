a78a6b2bf94e58fd8d25798d8769beb173727265
Tihomir Valkanov <tihomir.valkanov@gmail.com>
2015-12-23 10:55:26 +0200

    removing somethings and fix search

--- apps/admin/script.js
+++ apps/admin/script.js
@@ -8,14 +8,6 @@ $(document).ready(function() {
 
 
 swipes.onReady(function(){
-
-	swipes.modal.confirm( {title:"Hey Stanimir"}, function(res){
-		if(res){
-			alert("confirmed");
-		}
-	});
-	
-	//swipes.modal.alert("Hey", "You're cool");
 	// Holder for the apps objects
 	window._apps = [];
 	// Underscore rendered template, check admin.html for app-row-template
@@ -38,7 +30,7 @@ swipes.onReady(function(){
 			}
 		});
 	}
-	
+
 	function render(){
 		$(".app-list").html("");
 		$(".app-list.active").append("<h5>enabled</h5>");
@@ -114,7 +106,7 @@ swipes.onReady(function(){
 			}
 		});
 	}
-	
+
 	$(window).resize(function() {
 		var adminTabs = $('.nav');
 		var selectedLine = $('.selected-line');
