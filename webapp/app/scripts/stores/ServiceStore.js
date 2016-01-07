28b9bdf8fb367171a31149e4e8cc1aa592c8c112
Kasper Pihl Tornøe <kasper@pihl.it>
2016-01-07 14:21:39 -0800

    Made client side OAuth flow for service store

--- webapp/app/scripts/stores/ServiceStore.js
+++ webapp/app/scripts/stores/ServiceStore.js
@@ -3,9 +3,30 @@ var serviceActions = require('../actions/ServiceActions');
 var ServiceStore = Reflux.createStore({
 	listenables: [ serviceActions ],
 	onAuthorize: function(serviceName){
+		var self = this;
 		swipes.service(serviceName).authorize(function(res, err){
-			console.log('auth cb service store', res, err);
+			if(res && res.ok){
+				var auth = res.auth;
+				if(auth.type === 'oauth'){
+					window.OAuthHandler = serviceActions;	
+					var win = window.open(auth.url, serviceName, "height=700,width=500");
+					if(!win || win.closed || typeof win.closed=='undefined'){
+						return alert('Please allow popups to authorize services');
+					}
+					var timer = setInterval(function() {   
+						if(win.closed) {  
+							clearInterval(timer);  
+							alert('closed');  
+						}  
+					}, 1000);
+				}
+			}
 		});
+	},
+	onHandleOAuthSuccess: function(serviceName, query){
+		swipes.service(serviceName).authSuccess(query, function(res, err){
+			console.log('oauth success!', res, err);
+		})
 	}
 });
 
