ebaeecae18cdee8ecc187138a4fb405c606e8b55
Kasper Pihl Tornøe <kasper@pihl.it>
2016-01-10 18:45:34 -0800

    Minor update to asana boilerplate, added real credentials and some comments etc

--- services/asana/asana.js
+++ services/asana/asana.js
@@ -1,10 +1,10 @@
-var AsanaConnector = require('./asana_connector.js');
+var Asana = require('asana');
 var asana = {
 	// This should be dynamically set from database, but for now this is okay
 	connectionData: {
 		redirect_uri: 'http://dev.swipesapp.com/oauth-success.html',
-		client_id: 'ASANA_CLIENT_ID',
-		client_secret: 'ASANA_SECRET'
+		client_id: '79054685042235',
+		client_secret: 'eeac87abedd54540813d0eba2901bdad'
 	},
 	request:function(authData, method, options, callback){
 		// authData is the data that was saved in beforeAuthSave
@@ -13,16 +13,20 @@ var asana = {
 		callback(err, res);
 	},
 	beforeAuthSave: function(data, callback){
+		console.log('ready for auth save', data);
+		callback('not_built_yet');
 		// Data is the get parameters from the callback on oauth.
 		// Exchange if needed for a token
 		// Callback what should be saved to the user
 		// callback(err, res)
+		// needs to call the oauth_token stuff
 	},
 	authorize: function(callback){
 		var URL = 'https://app.asana.com/-/oauth_authorize';
 		URL += '?client_id=' + this.connectionData.client_id;
-		URL += '&scope=client';
+		URL += '&response_type=code';
 		URL += '&redirect_uri=' + this.connectionData.redirect_uri;
+		URL += '&state=hahaha';
 		return {
 			type: 'oauth',
 			service: 'asana',
