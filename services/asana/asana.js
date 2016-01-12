86cdd66edf3c3a8143e8c58743ff1e29ae2bc609
Kasper Pihl Tornøe <kasper@pihl.it>
2016-01-12 15:48:53 -0800

    Updated keys and callback (still not working)

--- services/asana/asana.js
+++ services/asana/asana.js
@@ -3,9 +3,9 @@ var Asana = require('asana');
 var asana = {
 	// This should be dynamically set from database, but for now this is okay
 	connectionData: {
-		redirect_uri: 'http://localhost:3000/oauth-success.html',
-		client_id: '55104337606444',
-		client_secret: 'ef862c7ddea4a517d61e5c345812e9eb'
+		redirect_uri: 'http://dev.swipesapp.com/oauth-success.html',
+		client_id: 79054685042235,
+		client_secret: 'eeac87abedd54540813d0eba2901bdad'
 	},
 	request: function (authData, method, options, callback) {
 		// authData is the data that was saved in beforeAuthSave
@@ -15,28 +15,27 @@ var asana = {
 	},
 	beforeAuthSave: function (data, callback) {
 		var asanaClient = Asana.Client.create({
-	    clientId: this.connectionData.client_id,
-	    clientSecret: this.connectionData.client_secret,
-	    redirectUri: this.connectionData.redirect_uri
-  	});
-
-		asanaClient.app.accessTokenFromCode(data.code).then(function (credentials) {
-			console.log(credentials);
+			clientId: this.connectionData.client_id,
+			clientSecret: this.connectionData.client_secret,
+			redirectUri: this.connectionData.redirect_uri
 		});
-		console.log('ready for auth save', data);
-		callback('not_built_yet');
-		// Data is the get parameters from the callback on oauth.
-		// Exchange if needed for a token
-		// Callback what should be saved to the user
-		// callback(err, res)
-		// needs to call the oauth_token stuff
+
+		asanaClient.useOauth();
+		asanaClient.app.accessTokenFromCode(data.code).then(function (credentials) {
+			console.log('success asana', credentials);
+			callback(null, credentials);
+		}).catch(function(e){
+			callback('error asana', e);
+			console.log('eer', e);
+		});
 	},
 	authorize: function (callback) {
 		var URL = 'https://app.asana.com/-/oauth_authorize';
 		URL += '?client_id=' + this.connectionData.client_id;
 		URL += '&response_type=code';
+		URL += '&scope=default';
 		URL += '&redirect_uri=' + this.connectionData.redirect_uri;
-		URL += '&state=hahaha';
+		URL += '&state=hahaha2';
 
 		return {
 			type: 'oauth',
