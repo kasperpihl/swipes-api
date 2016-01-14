a39815eb710285de63d54c20fca2431e761c9fa3
Tihomir Valkanov <tihomir.valkanov@gmail.com>
2016-01-14 13:26:29 +0200

    more explanation about jira redirects

--- rest/routes/services_no_auth.js
+++ rest/routes/services_no_auth.js
@@ -3,10 +3,13 @@
 let express = require( 'express' );
 let router = express.Router();
 
-// Work around for the stupid jira
-// we can't set a url like http://localhost:3000/oauth-success.html
-// because they put `/` behind it...
-// maybe there is a smarter way around this
+/*
+  Work around for the stupid jira
+  we can't set a url like http://localhost:3000/oauth-success.html
+  because they put `/` behind it...
+  So right know we redirect to our API, get the query parameters
+  and redirect back to our oauth-success.html page
+*/
 router.get('/services.authsuccess', (req, res, next) => {
   let oauthToken = 'oauth_token=' + req.query.oauth_token + '&';
   let oauthVerifier = 'oauth_token=' + req.query.oauth_verifier;
