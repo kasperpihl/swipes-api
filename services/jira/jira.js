f7aadcea5dbda303db14c41bfb1ef233905aab3d
Tihomir Valkanov <tihomir.valkanov@gmail.com>
2016-01-15 17:45:27 +0200

    TIHOMIR:JIRA 2:0 - the mapping is done

--- services/jira/jira.js
+++ services/jira/jira.js
@@ -6,18 +6,67 @@ var JiraClient = require('jira-connector');
 
 var privateKeyData = fs.readFileSync(serviceDir + '/jira-swipes.pem', 'utf8');
 
+var getJiraApiMethod = function (method, jiraClient) {
+	var arr = method.split('.');
+	var len = arr.length;
+	var jiraMethod = jiraClient;
+	var prevJiraMethod = jiraMethod;
+
+	for (var i=0; i<len; i++) {
+		if (!jiraMethod[arr[i]]) {
+			return null;
+		}
+
+		if (!jiraMethod[arr[i]].bind) {
+			jiraMethod = jiraMethod[arr[i]];
+		} else {
+			jiraMethod = jiraMethod[arr[i]].bind(prevJiraMethod);
+		}
+
+		prevJiraMethod = jiraMethod;
+	}
+
+	return jiraMethod;
+}
+
 var jira = {
 	connectionData: {
 		consumerKey: 'SwipesUnicorns',
 		host: 'swipes.atlassian.net'
 	},
-	// request: function (authData, method, options, callback) {
-	// 	if (typeof authData !== 'object' || !authData.access_token) {
-	// 		return callback('no_access_token');
-	// 	}
-	//
-	//
-	// },
+	request: function (authData, method, params, callback) {
+		if (!authData && !authData.access_token) {
+			return callback('no_access_token');
+		}
+
+		if (!authData && !authData.token_secret) {
+			return callback('no_token_secret');
+		}
+
+		var jiraClient = new JiraClient({
+			host: this.connectionData.host,
+	    oauth: {
+	        consumer_key: this.connectionData.consumerKey,
+	        private_key: privateKeyData,
+	        token: authData.access_token,
+	        token_secret: authData.token_secret
+	    }
+		});
+
+		var jiraMethod = getJiraApiMethod(method, jiraClient);
+
+		if (!jiraMethod) {
+			return callback('jira-connector_not_supported_method');
+		}
+
+		jiraMethod(params, function (error, issue) {
+			if (error) {
+				return callback(error);
+			}
+
+			return callback(null, issue);
+		});
+	},
 	beforeAuthSave: function (data, callback) {
 		var that = this;
 		var userId = data.userId;
