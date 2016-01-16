c326e8f9a41211305f0e968e5a31a625ee57decf
Kasper Pihl Tornøe <kasper@pihl.it>
2016-01-16 15:37:57 -0800

    Added worker sdk load for working with webworker

--- rest/routes/sdk.js
+++ rest/routes/sdk.js
@@ -39,5 +39,31 @@ router.get('/sdk.load', (req, res, next) => {
 	res.setHeader('Content-Type', 'application/javascript');
 	res.send(insertString);
 });
+router.get('/sdk.worker.load', (req, res, next) => {
+	let apiUrl = 'http://' + req.headers.host;
+
+	let _defUrlDir = apiUrl + '/apps/app-loader/';
+	// Insert dependencies, SwipesSDK and other scripts right after head
+	let insertString = '';
+
+	function wrap(text){
+		return 'document.write(\'' + text + '\');\r\n';
+	}
+
+	// Temporary solution, server shouldn't include them, they should be packed together
+	insertString += util.getFile(appDir + 'app-loader' + '/q.min.js');
+	insertString += util.getFile(appDir + 'app-loader' + '/swipes-api-connector.js');
+	insertString += util.getFile(appDir + 'app-loader' + '/swipes-app-sdk.js');
+	insertString += 'var window = this; var swipes = new SwipesAppSDK("' + apiUrl + '");';
+
+
+	// Instantiate objects and add runtime stuff
+
+	// If apps load sends the referer, use it for parent
+	insertString += 'if(typeof this.postMessage === "function") swipes._client.setListener(this);';
+	
+	res.setHeader('Content-Type', 'application/javascript');
+	res.send(insertString);
+});
 
 module.exports = router;
