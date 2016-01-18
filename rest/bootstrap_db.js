c0047a9d53afa1b1584c9749c99395d62e4a4132
Kasper Pihl Tornøe <kasper@pihl.it>
2016-01-18 12:08:11 -0800

    Simplified the bootstrap, no need for apps anymore

--- rest/bootstrap_db.js
+++ rest/bootstrap_db.js
@@ -2,11 +2,7 @@
 
 let config = require('config');
 let r = require('rethinkdb');
-let util = require('./util.js');
 let db = require('./db.js');
-let Promise = require('bluebird'); // we should use native promises one day
-let generateId = util.generateSlackLikeId;
-let moment = require('moment');
 let dbConfig = config.get('database');
 
 require('rethinkdb-init')(r);
@@ -20,40 +16,18 @@ let tables = [
     name: 'services',
     indexes: ['manifest_id']
   },
+  {
+    name: 'workflows',
+    indexes: ['manifest_id']
+  },
   'events',
-  'apps',
   'mentions',
-  'organizations',
-  'workflows'
+  'organizations'
+
 ];
 
 r.init(dbConfig,
   tables
-).then((conn) => {
-  createApps();
+).then(function(){
+  console.log("DONE")
 })
-
-
-let createApps = () => {
-  console.log('creating some apps');
-
-  let apps = [
-    {
-      id: generateId('A'),
-      "name": "Admin",
-      "manifest_id": "admin",
-      "version": "0.1",
-      "description": "The Admin App For Swipes",
-      admin_only: true,
-      required: true,
-      is_installed: true,
-      main_app: {"index": "admin.html"}
-    }
-  ];
-
-  let insertQ = r.table('apps').insert(apps);
-
-  db.rethinkQuery(insertQ).then(res => {
-    console.log('DONE adding Apps');
-  })
-}
