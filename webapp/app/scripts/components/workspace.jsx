92dd801506625b52b581d265d61e09dad88c7b95
Kasper Pihl Tornøe <kasper@pihl.it>
2016-01-28 20:34:34 -0800

    Using Workspace Store, and fixing grid layout on the go.

--- webapp/app/scripts/components/workspace.jsx
+++ webapp/app/scripts/components/workspace.jsx
@@ -4,17 +4,17 @@ var Reflux = require('reflux');
 var PureRenderMixin = require('react/lib/ReactComponentWithPureRenderMixin');
 var ReactGridLayout = require('react-grid-layout');
 
-var WorkflowStore = require('../stores/WorkflowStore');
+var WorkspaceStore = require('../stores/WorkspaceStore');
+
 var CardLoader = require('./card_loader');
 var Card = require('material-ui/lib').Card;
 
 var Workspace = React.createClass({
-    mixins: [PureRenderMixin],
+    mixins: [PureRenderMixin, WorkspaceStore.connect('workflows')],
     getDefaultProps() {
         return {
             className: "layout",
             cols: 12,
-            draggableCancel: '.card-top-bar',
             items: 3,
             rowHeight: 50,
             isDraggable: true,
@@ -23,40 +23,10 @@ var Workspace = React.createClass({
             //verticalCompact: false
         };
     },
-    getInitialState(){
-        var workspace = {
-            id: "workspace_id_1",
-            layout: [{
-                workflow_id: "W9T38GJ22",
-                _grid: {
-                    i: 0,
-                    x: 0,
-                    y: 0,
-                    w: 4,
-                    h: 8
-                }
-            },
-            {
-                workflow_id: "WREUQJXBX",
-                _grid: {
-                    i: 2,
-                    x: 8,
-                    y: 0,
-                    w: 4,
-                    h: 8
-                }
-            }]
-        }
-        var layout = _.pluck(workspace.layout, '_grid');
-        console.log('lay lay', layout);
-        return {
-            workspace: workspace,
-            layout: layout
-        }
-    },
     generateDOM() {
         var self = this;
-        return _.map(this.state.workspace.layout, function(workflow, i) {
+        return _.map(this.state.workflows, function(workflow, i) {
+            console.log(i, workflow);
             return (
                 <Card key={workflow._grid.i}>
                     <CardLoader data={{id: workflow.workflow_id}} />
@@ -65,19 +35,14 @@ var Workspace = React.createClass({
         });
     },
     onLayoutChange(layout) {
-        for(var i = 0 ; i < layout.length ; i++){
-            if(layout[i].moved){
-                console.log('moved', layout[i].i, layout[i].x, layout[i].y);
-            }
-        }
-        this.setState({layout:layout});
+        console.log(layout);
     },
 
     render() {
         return (
             <div className="grid-scroll-container">
                 <ReactGridLayout
-                layout={this.state.layout} 
+                layout={_.pluck(this.state.workflows, '_grid')} 
                 onLayoutChange={this.onLayoutChange}
                 {...this.props}>
                     {this.generateDOM()}
--- webapp/app/scripts/stores/WorkspaceStore.js
+++ webapp/app/scripts/stores/WorkspaceStore.js
@@ -1,6 +1,44 @@
 var Reflux = require('reflux');
+var WorkflowStore = require('./WorkflowStore');
 var WorkspaceStore = Reflux.createStore({
-	localStorage: "WorkspaceStore"
+	localStorage: "WorkspaceStore2",
+	getAvailableI:function(){
+		var sortedI = _.map(_.sortBy(this.getAll(), function(el){ return el._grid.i; }), function(el) { return el._grid.i });
+		for(var i = 0 ; i <= sortedI.length ; i++){
+			if(i === sortedI.length){
+				return i;
+			}
+			if(i != sortedI[i]){
+				return i;
+			}
+		}
+	},
+	onWorkflowStore: function(workflows){
+		for(var i = 0 ; i < workflows.length ; i++){
+			var workflow = workflows[i];
+			if(!this.get(workflow.id)){
+				var insertObj = {
+					workflow_id: workflow.id,
+					_grid: {
+						i: this.getAvailableI(),
+						w: 4,
+						h: 8,
+						x: 0,
+						y: 0
+					}
+
+				}
+				console.log('insert', insertObj);
+				this.set(workflow.id, insertObj, {trigger: false});
+			}
+		}
+		this.manualTrigger();
+	},
+	init:function(){
+		console.log('init workspace');
+		this.manualLoadData();
+		this.listenTo(WorkflowStore, this.onWorkflowStore);
+	}
 });
 
 module.exports = WorkspaceStore;
