70bd0e10a5497428581dbd8cff8d1fdda1d65fae
Tihomir Valkanov <tihomir.valkanov@gmail.com>
2016-02-24 18:47:44 +0200

    make https work on dev

--- rest/routes/rtm.js
+++ rest/routes/rtm.js
@@ -39,10 +39,10 @@ router.post('/rtm.start', (req, res, next) => {
     .then(data => {
       let rtmResponse = {
         ok: true,
-        url: config.get('clientPort') === '80' ?
+        url: config.get('clientPort') === '443' ?
             config.get('origin') :
             config.get('origin') + ':' + config.get('clientPort'),
-        workflow_base_url: config.get('clientPort') === '80' ?
+        workflow_base_url: config.get('clientPort') === '443' ?
                           config.get('origin')  + '/workflows/' :
                           config.get('origin') + ':' + config.get('clientPort')  + '/workflows/',
         self: data[0],
