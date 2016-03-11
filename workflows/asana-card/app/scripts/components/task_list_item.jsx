ede4c09863f1ae981b723ce97ce9b90a6eaed5d5
Tihomir Valkanov <tihomir.valkanov@gmail.com>
2016-03-11 15:06:23 +0200

    cleanup

--- workflows/asana-card/app/scripts/components/task_list_item.jsx
+++ workflows/asana-card/app/scripts/components/task_list_item.jsx
@@ -1,14 +1,6 @@
 var React = require('react');
-// var Reflux = require('reflux');
-// var Paper = require('material-ui/lib').Paper;
-// var Avatar = require('material-ui/lib').Avatar;
-// var IconMenu = require('material-ui/lib').IconMenu;
-// var IconButton = require('material-ui/lib').IconButton;
-// var MenuItem = require('material-ui/lib').MenuItem;
-// var Colors = require('material-ui/lib/styles/colors');
 var MainStore = require('../stores/MainStore');
 var UserStore = require('../stores/UserStore');
-// var MainActions = require('../actions/MainActions');
 var ProjectActions = require('../actions/ProjectActions');
 var FontIcon = require('material-ui/lib/font-icon');
 var IconMenu = require('material-ui/lib/menus/icon-menu');
