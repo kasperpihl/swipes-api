580647de9586eced701032750043ee5f6a836b83
iremlopsum <kristjanfou@gmail.com>
2016-04-26 11:44:49 +0300

    update for lightbox modal

--- webapp/app/scripts/components/modals/lightbox_modal.jsx
+++ webapp/app/scripts/components/modals/lightbox_modal.jsx
@@ -1,22 +1,48 @@
 var React = require('react');
 var moment = require('moment');
+var modalActions = require('../../actions/ModalActions');
+var Loading = require('../loading');
+var CircularProgress = require('material-ui/lib/circular-progress');
 
 var LightboxModal = React.createClass({
   getInitialState: function() {
     return {
+      loading: 'active',
+      lightboxImg: 'inactive'
     }
+  },
+  componentDidMount: function(){
+
+    window.focus();
+	},
+  closeModal: function(e) {
+    var lightboxWrapper = this.refs.lightboxwrapper;
+
+    if(e.target !== lightboxWrapper) {
+       return;
+    }
+
+    modalActions.hide();
+  },
+  imgLoaded: function() {
+    this.setState({lightboxImg: 'active'});
+    this.setState({loading: 'inactive'});
+  },
+  renderImage: function() {
   },
   render: function() {
     var options = this.props.data.options;
-    console.log(options);
 
     var ww = window.innerWidth;
     var wh = window.innerHeight;
 
     return (
-      <div className="lightbox" style={{width: ww, height: wh}}>
+      <div className="lightbox" ref="lightboxwrapper" style={{width: ww, height: wh}} onClick={this.closeModal} >
         <h2 className="image-title">{options.message}</h2>
-        <img src={options.title}/>
+        <img src={options.title} className={'lightbox-image ' + this.state.lightboxImg} ref='lightboxImage' onLoad={this.imgLoaded}/>
+        <div className={'lightbox-loader ' + this.state.loading}>
+  				<CircularProgress color="#fff" size={1}/>
+  			</div>
       </div>
     )
   }
