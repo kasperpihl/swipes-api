217a4cf05ddbfc28774fa1ccd1436975207e3ad9
Kasper Pihl Tornøe <kasper@pihl.it>
2016-05-01 16:21:27 -0700

    secret.feedback endpoint. hacky hack. Requires token though haha

--- rest/routes/feedback.js
+++ rest/routes/feedback.js
@@ -9,7 +9,16 @@ const util = require('../util.js');
 const db = require('../db.js');
 const SwipesError = require( '../swipes-error' );
 const router = express.Router();
-
+router.get('/secret.feedback', (req, res, next) => {
+  const getFeedbackQ = r.table('feedback');
+  db.rethinkQuery(getFeedbackQ)
+  .then((res) => {
+      return res.status(200).send(res);
+    })
+    .catch((e) => {
+      return next(e);
+    })
+});
 router.post('/feedback.add', (req, res, next) => {
   const user = req.user;
   const feedback = req.body.feedback;
