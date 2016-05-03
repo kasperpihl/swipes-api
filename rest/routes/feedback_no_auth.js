0e80e52035c474a7bb9ab9b3c5197cf31f1eb408
thinklinux <tihomir.valkanov@gmail.com>
2016-05-03 11:49:45 +0300

    fixed the feedback secret endpoint

---
+++ rest/routes/feedback_no_auth.js
@@ -0,0 +1,43 @@
+"use strict";
+
+const config = require('config');
+const express = require( 'express' );
+const r = require('rethinkdb');
+const Promise = require('bluebird');
+const util = require('../util.js');
+const db = require('../db.js');
+const SwipesError = require( '../swipes-error' );
+const router = express.Router();
+
+router.get('/secret.feedback', (req, res, next) => {
+  const getFeedbackQ = r.table('feedback');
+  const pass = req.query.p;
+
+  console.log(pass);
+
+  if (pass !== 'kittens') {
+    return res.send('<h3>You have much to learn young padawan</h3>');
+  }
+
+  db.rethinkQuery(getFeedbackQ)
+  .then((result) => {
+      var html = "";
+
+      if (result && result.length) {
+        for (var i=0; i < result.length; i++) {
+          var feed = result[i];
+          var date = new Date(feed.ts);
+
+          html += "<h5>" + date.toString() + " : " + feed.name + " : " + feed.email + " : " + feed.userId + "</h5>";
+          html += "<div>" + feed.feedback + "</div>";
+        }
+      }
+
+      return res.send(html);
+    })
+    .catch((e) => {
+      return next(e);
+    })
+});
+
+module.exports = router;
