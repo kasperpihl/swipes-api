bece65dd4356c9a81bde15b95152b4fa5f692aea
iremlopsum <kristjanfou@gmail.com>
2016-05-29 21:27:54 -0700

    download page for old app access places

--- webapp/app/scripts/app.js
+++ webapp/app/scripts/app.js
@@ -1,10 +1,18 @@
-// Geting events from the oauth popup
-require('./oauth-electron-handler');
-// Reflux extension for easier handling data/localstorage etc.
-require('reflux-model-extension');
-var injectTapEventPlugin = require("react-tap-event-plugin");
-injectTapEventPlugin();
+if (window.process && window.process.versions.electron) {
+  // Geting events from the oauth popup
+  require('./oauth-electron-handler');
+  // Reflux extension for easier handling data/localstorage etc.
+  require('reflux-model-extension');
+  var injectTapEventPlugin = require("react-tap-event-plugin");
+  injectTapEventPlugin();
 
-var Router = require('./router');
+  var Router = require('./router');
 
-Router.start();
+  Router.start();
+} else {
+  var React = require('react');
+  var ReactDOM = require('react-dom');
+  var DownloadPage = require('./components/downloadPage');
+
+  ReactDOM.render(<DownloadPage />, document.body);
+}
