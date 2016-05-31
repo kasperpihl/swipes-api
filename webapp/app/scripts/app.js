47887a496886bd2d7c6e3d1641e51f94a8316fd1
Kasper Pihl Tornøe <kasper@pihl.it>
2016-05-30 17:38:21 -0700

    set a top-level menu

--- webapp/app/scripts/app.js
+++ webapp/app/scripts/app.js
@@ -1,4 +1,6 @@
 if (window.process && window.process.versions.electron) {
+  const Router = require('./router');
+  const defaultMenu = require('./electron-default-menu');
   // Geting events from the oauth popup
   require('./oauth-electron-handler');
   // Reflux extension for easier handling data/localstorage etc.
@@ -6,13 +8,17 @@ if (window.process && window.process.versions.electron) {
   var injectTapEventPlugin = require("react-tap-event-plugin");
   injectTapEventPlugin();
 
-  var Router = require('./router');
+  // Set a top-level application menu
+  const {Menu} = domRequire('electron').remote;
+  const menu = defaultMenu();
+
+  Menu.setApplicationMenu(Menu.buildFromTemplate(menu));
 
   Router.start();
 } else {
-  var React = require('react');
-  var ReactDOM = require('react-dom');
-  var DownloadPage = require('./components/downloadPage');
+  const React = require('react');
+  const ReactDOM = require('react-dom');
+  const DownloadPage = require('./components/downloadPage');
 
   ReactDOM.render(<DownloadPage />, document.body);
 }
