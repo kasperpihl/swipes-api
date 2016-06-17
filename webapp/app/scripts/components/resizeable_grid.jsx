5e6acff4ec45b61902561ebe7b6c32a2a86db51f
iremlopsum <kristjanfou@gmail.com>
2016-06-17 13:43:03 -0700

    change

--- webapp/app/scripts/components/resizeable_grid.jsx
+++ webapp/app/scripts/components/resizeable_grid.jsx
@@ -19,33 +19,13 @@ var Grid = React.createClass({
     this.resizingColumnIndex = columnIndex;
   },
   columnResize(diffX){
-    
+
 
     var percentages = this.columnsArrayPercentages();
     var pixels = this.columnsArrayPixels();
-    
+
 
     var addedWidth = -diffX;
-<<<<<<< HEAD
-    var column = this.columnForIndex(this.resizingColumnIndex);
-    var size = this.pixelsFromPercentage(column.w);
-    var newSize = size + addedWidth;
-
-    var columns = this.state.columns;
-    var i = this.resizingColumnIndex - 1;
-    columns[this.resizingColumnIndex].w = this.percentageFromPixels(newSize);
-    do{
-      var column = columns[i];
-      var cw = this.pixelsFromPercentage(column.w);
-
-      columns[i].w = this.percentageFromPixels(cw - addedWidth);
-      i--;
-      console.log('cw', cw, 'nw', (cw - addedWidth), 'c%', this.percentageFromPixels(cw - addedWidth));
-    }
-    while (i >= 0);
-
-    // Check if percentages is 100%
-=======
     var newSize = pixels[this.resizingColumnIndex] + addedWidth;
 
 
@@ -54,7 +34,7 @@ var Grid = React.createClass({
     var prevI = this.resizingColumnIndex - 1;
     percentages[prevI] = this.percentageFromPixels(pixels[prevI] - addedWidth);
 
-    
+
     // Add percentages to columns and check if 100%
     var total = 0;
     var columns = this.state.columns;
@@ -68,8 +48,6 @@ var Grid = React.createClass({
       console.log(total);
       columns[this.resizingColumnIndex - 1].w += (100 - total);
     }
-    
->>>>>>> 2e4d97f8cb00101028cfd0d8adbffcabfae9e028
 
     this.setState({columns: columns});
 
@@ -84,7 +62,7 @@ var Grid = React.createClass({
       return this.props.delegate[name].apply(null, [this].concat(Array.prototype.slice.call(arguments, 1)));
     }
   },
-  
+
   // ======================================================
   // Conversions
   // ======================================================
@@ -93,7 +71,7 @@ var Grid = React.createClass({
     const grid = document.querySelector('.grid');
     const gw = grid.clientWidth;
     var percentage = (pixels / gw * 100);
-    
+
     return this.roundedDecimal(percentage);
   },
   pixelsFromPercentage(percentage){
@@ -114,8 +92,8 @@ var Grid = React.createClass({
     var sizeToBe = {
       w: 100 / curSizeX,
       h : 100 / curSizeY
-    }; 
-    
+    };
+
     return sizeToBe
   },
 
@@ -163,7 +141,7 @@ var Grid = React.createClass({
     const columnLength = this.state.columns.length;
     const rowsInColumn = this.state.columns[columnIndex].rows.length;
     const grid = document.querySelector('.grid');
-    const gw = grid.clientWidth;    
+    const gw = grid.clientWidth;
     const gh = grid.clientHeight;
     const rw = row.clientWidth;
     const rh = row.clientHeight;
@@ -175,18 +153,18 @@ var Grid = React.createClass({
 
     let originX = 50;
     let originY = 50;
-    
+
     row.parentNode.classList.toggle('maximize');
     row.classList.toggle('maximize');
-    
+
     if(currentScale) {
-      row.style.transform = '';   
-      // row.style.transformOrigin = '';  
+      row.style.transform = '';
+      // row.style.transformOrigin = '';
     } else {
-      
+
       // column logic
       if (columnIndex === 0) { // First Column
-        originX = 0;       
+        originX = 0;
       }
       if (columnIndex === (columnLength - 1)) { // Last column
         originX = 100;
@@ -194,7 +172,7 @@ var Grid = React.createClass({
       if (columnIndex > 0 && columnIndex < (columnLength - 1)) { // Columns in the middle
         originX = centerXPercentage;
       }
-      
+
       // Row logic
       if (rowIndex === 0 && (rowsInColumn - 1) > 0) { // first row, but more than one
         originY = 0;
@@ -205,7 +183,7 @@ var Grid = React.createClass({
       if (rowIndex > 0 && rowIndex < (rowsInColumn - 1)) { // Rows in the middle
         originY = centerYPercentage;
       }
-      
+
       row.style.transformOrigin = originX + '% ' + originY + '%';
       row.style.transform = 'scaleX(' + scaleTo.w + ') scaleY(' + scaleTo.h + ')';
     }
@@ -296,79 +274,8 @@ Grid.Row = React.createClass({
       return <div className="resize-horizontal" />;
     }
   },
-<<<<<<< HEAD
-  calcScale(gw, gh, rw, rh) {
-    var curSizeX = (rw * 100) / gw;
-    var curSizeY = 1;
-    if (rh < gw) {
-      var curSizeY = (rh * 100) / gh;
-    }
-    var sizeToBe = {
-      w: 100 / curSizeX,
-      h : 100 / curSizeY
-    };
-
-    return sizeToBe
-  },
-  onMaximize(e) {
-     const {
-      data,
-      initData
-    } = this.props;
-    const row = this.refs.row;
-    const columnLength = 4;
-    const grid = document.querySelector('.grid');
-    const gw = grid.clientWidth;
-    const gh = grid.clientHeight;
-    const rw = row.clientWidth;
-    const rh = row.clientHeight;
-    const rPos = row.getBoundingClientRect();
-    const currentScale = row.style.transform;
-    const scaleTo = this.calcScale(gw, gh, rw, rh);
-    const centerXPercentage = (rPos.left * 100) / ((gw - rPos.right) + rPos.left);
-    const centerYPercentage = (rPos.top * 100) / ((gh - rPos.bottom) + rPos.top);
-
-    let originX = 50;
-    let originY = 50;
-
-    row.parentNode.classList.toggle('maximize');
-    row.classList.toggle('maximize');
-
-    if(currentScale) {
-      row.style.transform = '';
-      // row.style.transformOrigin = '';
-    } else {
-
-      // column logic
-      if (columnIndex === 0) { // First Column
-        originX = 0;
-      }
-      if (columnIndex === (columnLength - 1)) { // Last column
-        originX = 100;
-      }
-      if (columnIndex > 0 && columnIndex < (columnLength - 1)) { // Columns in the middle
-        originX = centerXPercentage;
-      }
-
-      // Row logic
-      if (rowIndex === 0 && (rowsInColumn - 1) > 0) { // first row, but more than one
-        originY = 0;
-      }
-      if (rowIndex === (rowsInColumn - 1) && (rowsInColumn - 1) > 0) { // last row, but more than one
-        originY = 100;
-      }
-      if (rowIndex > 0 && rowIndex < (rowsInColumn - 1)) { // Rows in the middle
-        originY = centerYPercentage;
-      }
-
-      row.style.transformOrigin = originX + '% ' + originY + '%';
-      row.style.transform = 'scaleX(' + scaleTo.w + ') scaleY(' + scaleTo.h + ')';
-    }
-
-=======
   onMaximize(e){
     this.props.delegate.maximizeColumnWithRow(this.refs.row, this.props.columnIndex, this.props.rowIndex);
->>>>>>> 2e4d97f8cb00101028cfd0d8adbffcabfae9e028
   },
   render(){
     const {
