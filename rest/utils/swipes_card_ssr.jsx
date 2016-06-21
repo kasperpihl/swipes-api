6fa6d319010c1ffcb8a0d26df120d4812e0eae2a
thinklinux <tihomir.valkanov@gmail.com>
2016-06-21 00:53:21 -0700

    rendering a react component server side

--- rest/utils/swipes_card_ssr.jsx
+++ rest/utils/swipes_card_ssr.jsx
@@ -3,7 +3,7 @@
 import fs from 'fs';
 import path from 'path';
 import React from 'react';
-import renderToString from 'react-dom/server';
+import { renderToString } from 'react-dom/server'
 import {
   Provider,
   Card
@@ -12,19 +12,29 @@ import {
 const indexPath = path.join(__dirname, '../../webapp/ssr_card_dev/index.html');
 
 const renderIndex = (data) => {
+  // T_TODO map data to card format
+  data = {
+    title: 'swipes-icons.png'
+  }
+
   const SwipesCard = new Provider(Card);
   const cardHtml = renderToString(<SwipesCard data={data} />)
-  console.log(cardHtml)
 
   let indexHtml;
   try {
     indexHtml = fs.readFileSync(indexPath, 'utf8');
   }
   catch (e) {
+    //T_TODO better error handling
     console.log(e);
   }
 
-  return indexHtml;
+  const idContentIndex = indexHtml.indexOf('<div id="content"></div>');
+  // http://stackoverflow.com/questions/4364881/inserting-string-at-position-x-of-another-string
+  // Inserting the card html to the div within indexHtml that is with class content
+  const finalHtml = [indexHtml.slice(0, idContentIndex), cardHtml, indexHtml.slice(idContentIndex)].join('');
+
+  return finalHtml;
 }
 
 export {renderIndex}
