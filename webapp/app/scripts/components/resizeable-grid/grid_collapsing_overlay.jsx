63c19f206757225f1d664c8dd3a0126050c19e50
iremlopsum <kristjanfou@gmail.com>
2016-07-01 16:04:17 +0300

    collapse overlay WIP

--- webapp/app/scripts/components/resizeable-grid/grid_collapsing_overlay.jsx
+++ webapp/app/scripts/components/resizeable-grid/grid_collapsing_overlay.jsx
@@ -3,6 +3,8 @@ var CollapsingOverlay = React.createClass({
   render(){
     return (
       <div className="sw-collapsing-overlay">
+        <img src="styles/img/collapse-icon.svg" alt=""/>
+        Collapse Chat
       </div>
     )
   }
