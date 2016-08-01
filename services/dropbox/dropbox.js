42319dacfa18764311b26e92948c1acdf6120cda
thinklinux <tihomir.valkanov@gmail.com>
2016-08-01 19:43:48 +0300

    first event in our database from dropbox webhook

--- services/dropbox/dropbox.js
+++ services/dropbox/dropbox.js
@@ -2,6 +2,7 @@ var config = require('config');
 var request = require('request');
 var r = require('rethinkdb');
 var db = require('../../rest/db.js'); // T_TODO I should make this one a local npm module
+var ac = require('async');
 
 var serviceDir = __dirname;
 var dropboxConfig = config.get('dropbox');
@@ -38,9 +39,10 @@ var dropbox = {
 		request(options, function (err, res, body) {
 		  if (err) {
 		    console.log(err);
+				return callback(err);
 		  }
 
-			callback(err, body);
+			return callback(null, body);
 		});
 	},
 	processWebhook: function (accounts, callback) {
@@ -62,7 +64,7 @@ var dropbox = {
 				console.log(error);
 			}
 
-			console.log(result);
+			processChanges({account, result});
 
 			var cursor = result.cursor;
 
@@ -174,5 +176,92 @@ var dropbox = {
 		callback(null, {type: 'oauth', url: URL});
 	}
 };
+
+var processChanges = function ({account, result}) {
+	var entries = result.entries;
+	// Care only for .tag 'file' just for simplicity
+	var filteredEntries = entries.filter(function (entry) {
+		return entry['.tag'] === 'file';
+	});
+
+	filteredEntries.forEach(function (entry) {
+		processFileChange({account, entry});
+	})
+}
+
+var processFileChange = function ({account, entry}) {
+	var authData = account.authData;
+	var callbacks = [];
+
+	// Get revisions
+	var listRevisionsMethod = 'files.listRevisions';
+	var listRevisionsParams = {
+		path: entry.path_lower,
+		limit: 2 // We just want to know if it's created or modified file
+	}
+
+	callbacks.push(function (callback) {
+		dropbox.request({authData, method: listRevisionsMethod, params: listRevisionsParams}, function (err, res) {
+			if (err) {return callback(err)};
+
+			callback(null, res);
+		})
+	});
+
+	// Get which user modified the file
+	var getAccountMethod = 'users.getAccount';
+	var getAccountParams = {
+		account_id: entry.sharing_info.modified_by
+	}
+
+	callbacks.push(function (callback) {
+		dropbox.request({authData, method: getAccountMethod, params: getAccountParams}, function (err, res) {
+			if (err) {return callback(err)};
+
+			callback(null, res);
+		})
+	});
+
+	ac.parallel(callbacks, function (error, result) {
+		if (error) {
+			console.log(error);
+			return;
+		}
+
+		var revisions = result[0];
+		var user = result[1];
+		// That's a hack just for now
+		var swipesUserId = account.user_id;
+		var changed = revisions.length > 0 ? true : false;
+		var eventMessage = '';
+
+		if (changed) {
+			eventMessage = 'File have been changed';
+		} else {
+			eventMessage = 'File have been created';
+		}
+
+		console.log('USER INFO',user);
+
+		var event = {
+			toUser: swipesUserId,
+			date: new Date(),
+			modified_by: user.name.display_name || user.email,
+			profile_photo: user.profile_photo_url || '',
+			message: eventMessage,
+			service: 'dropbox'
+		};
+		var query = r.table('events').insert(event);
+
+		console.log('EVENT OBJECT', event);
+		db.rethinkQuery(query)
+			.then(() => {
+				console.log('event inserted')
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	})
+}
 
 module.exports = dropbox;
