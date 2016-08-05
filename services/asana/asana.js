8043516a767a581c612ef1b0a98423582eb8b869
thinklinux <tihomir.valkanov@gmail.com>
2016-08-05 16:54:23 +0300

    refactor asana service to es6

--- services/asana/asana.js
+++ services/asana/asana.js
@@ -1,27 +1,30 @@
-var config = require('config');
-var Asana = require('asana');
-var Promise = require('bluebird');
-var r = require('rethinkdb');
-var db = require('../../rest/db.js'); // T_TODO I should make this one a local npm module
-var SwipesError = require( '../../rest/swipes-error' );
+import config from 'config';
+import Asana from 'asana';
+import Promise from 'bluebird';
+import r from 'rethinkdb';
+import db from '../../rest/db.js'; // T_TODO I should make this one a local npm module
+import SwipesError from '../../rest/swipes-error';
 
-var asanaConfig = config.get('asana');
+const webhooksHost = config.get('webhooksHost');
+const asanaConfig = config.get('asana');
 
-var createClient = function () {
-	return Asana.Client.create({
-		clientId: asanaConfig.clientId,
-		clientSecret: asanaConfig.clientSecret,
-		redirectUri: asanaConfig.redirectUri
-	});
+const createClient = () => {
+	const {
+		clientId,
+		clientSecret,
+		redirectUri
+	} = asanaConfig;
+
+	return Asana.Client.create({ clientId, clientSecret, redirectUri });
 }
 
-var getAsanaApiMethod = function (method, client) {
-	var arr = method.split('.');
-	var len = arr.length;
-	var asanaMethod = client;
-	var prevAsanaMethod = asanaMethod;
+const getAsanaApiMethod = (method, client) => {
+	const arr = method.split('.');
+	const len = arr.length;
+	let asanaMethod = client;
+	let prevAsanaMethod = asanaMethod;
 
-	for (var i=0; i<len; i++) {
+	for (let i=0; i<len; i++) {
 		if (!asanaMethod[arr[i]]) {
 			return null;
 		}
@@ -38,19 +41,19 @@ var getAsanaApiMethod = function (method, client) {
 	return asanaMethod;
 }
 
-var refreshAccessToken = function (authData, user, service) {
-	return new Promise(function (resolve, reject) {
-		var now = new Date().getTime() / 1000;
-		var expires_in = authData.expires_in - 30; // 30 seconds margin of error
-		var ts_last_token = authData.ts_last_token;
-		var client = createClient();
-		var userId = user.userId;
-		var serviceId = service.serviceId;
-		var accessToken;
+const refreshAccessToken = (authData, user, service) => {
+	return new Promise((resolve, reject) => {
+		const now = new Date().getTime() / 1000;
+		const expires_in = authData.expires_in - 30; // 30 seconds margin of error
+		const ts_last_token = authData.ts_last_token;
+		const client = createClient();
+		const userId = user.userId;
+		const serviceId = service.serviceId;
+		let accessToken;
 
 		if (now - ts_last_token > expires_in) {
 			client.app.accessTokenFromRefreshToken(authData.refresh_token)
-				.then(function (response) {
+				.then((response) => {
 					accessToken = response.access_token;
 					// T_TODO
 					// Update service in our database
@@ -74,10 +77,10 @@ var refreshAccessToken = function (authData, user, service) {
 
 					return db.rethinkQuery(query);
 				})
-				.then(function () {
+				.then(() => {
 					resolve(accessToken);
 				})
-				.catch(function (error) {
+				.catch((error) => {
 					reject(error);
 				})
 		} else {
@@ -86,25 +89,23 @@ var refreshAccessToken = function (authData, user, service) {
 	});
 }
 
-var asana = {
-	request: function ({authData, method, params, user, service}, callback) {
-		var id, asanaPromise, asanaMethod;
-		var client = createClient();
+const asana = {
+	request: ({ authData, method, params, user, service }, callback) => {
+		const client = createClient();
 
 		refreshAccessToken(authData, user, service)
-			.then(function (accessToken) {
-				client.useOauth({
-					credentials: accessToken
-				});
+			.then((credentials) => {
+				let asanaPromise;
+				client.useOauth({ credentials });
 
-				asanaMethod = getAsanaApiMethod(method, client);
+				const asanaMethod = getAsanaApiMethod(method, client);
 
 				// T_TODO We have to return null if the method don't exist
 				if (!asanaMethod) {
 					return Promise.reject(new SwipesError('asana_sdk_not_supported_method'));
 				}
 
-				var id = null;
+				let id = null;
 
 				if (params.id) {
 					id = params.id;
@@ -119,7 +120,7 @@ var asana = {
 
 				return asanaPromise;
 			})
-			.then(function (response) {
+			.then((response) => {
 				// For some reason sometimes the response is without .data
 				// In the web explorer where one can test the api
 				// there is no such problem.
@@ -132,36 +133,30 @@ var asana = {
 
 				callback(null, data);
 			})
-			.catch(function (error) {
+			.catch((error) => {
 				callback(error);
 			})
 	},
-	shareRequest: function ({authData, type, params, user, service}, callback) {
-		var self = this;
-		var method = '';
-		var actions;
-		var mappedData;
+	shareRequest: ({ authData, type, params, user, service }, callback) => {
+		let method = '';
 
 		if (type === 'task') {
 			method = 'tasks.findById'
 		}
 
-		this.request({authData, method, params, user, service}, function (err, res) {
+		asana.request({authData, method, params, user, service}, (err, res) => {
 			if (err) {
 				return callback(err);
 			}
 
-			actions = self.cardActions(type, res);
-			mappedData = self.cardData(type, res);
+			const serviceActions = asana.cardActions(type, res);
+			const serviceData = asana.cardData(type, res);
 
-			return callback(null,  {
-				serviceData: mappedData,
-				serviceActions: actions
-			});
+			return callback(null,  { serviceData, serviceActions });
 		})
 	},
-	cardData: function (type, data) {
-		var mappedData;
+	cardData: (type, data) => {
+		let mappedData;
 
 		if (type === 'task') {
 			mappedData = {
@@ -171,8 +166,8 @@ var asana = {
 
 		return mappedData;
 	},
-	cardActions: function (type, data) {
-		var actions = [];
+	cardActions: (type, data) => {
+		const actions = [];
 
 		if (type === 'task') {
 			if (!data.completed) {
@@ -202,32 +197,29 @@ var asana = {
 
 		return actions;
 	},
-	beforeAuthSave: function (data, callback) {
-		var client = createClient();
-		var code = data.code;
-		var data = {};
+	beforeAuthSave: (data, callback) => {
+		const client = createClient();
+		const code = data.code;
 
 		client.app.accessTokenFromCode(code)
-			.then(function (response) {
+			.then((response) => {
+				const authData = response;
+				const { id, email: show_name } = response.data;
 				// Need that for the refresh token
 				response.ts_last_token = new Date().getTime() / 1000;
 
-				data = {
-					authData: response,
-					id: response.data.id,
-					show_name: response.data.email
-				}
+				const data = { authData, id, show_name };
 
 				callback(null, data);
 			})
-			.catch(function (error) {
+			.catch((error) => {
 				console.log(error);
 				callback(error);
 			})
 	},
-	authorize: function (data, callback) {
-		var client = createClient();
-		var url = client.app.asanaAuthorizeUrl();
+	authorize: (data, callback) => {
+		const client = createClient();
+		const url = client.app.asanaAuthorizeUrl();
 
 		callback(null, {
 			type: 'oauth',
