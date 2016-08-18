e3b0c441f99a7482bf35734ce1c86cd735013e5d
iremlopsum <kristjanfou@gmail.com>
2016-08-18 21:27:27 +0300

    activities structure

--- webapp/src/components/find/Activities.js
+++ webapp/src/components/find/Activities.js
@@ -1,4 +1,6 @@
 import React, { Component, PropTypes } from 'react'
+import Activity from './Activity'
+
 class Activities extends Component {
   constructor(props) {
     super(props)
@@ -6,17 +8,38 @@ class Activities extends Component {
   }
   componentDidMount() {
   }
-  renderActivities(){
-    
+  renderHeader(title, subtitle) {
+    return (
+      <div className="header">
+        <div className="header__title">{title}</div>
+        <div className="header__subtitle">{subtitle}</div>
+      </div>
+    )
+  }
+  renderActivities(activities){
+    if (!activities) {
+      return;
+    }
+    return activities.map( (activity) => {
+      <Activity data={activity} />
+    })
   }
   render() {
+    const { title, subtitle, activities } = this.props;
+
     return (
-      <div></div>
+      <div className="swipes-activites">
+        {this.renderHeader(title, subtitle)}
+        {this.renderActivities(activities)}
+      </div>
     )
   }
 }
+
 export default Activities
 
 Activities.propTypes = {
+  title: PropTypes.string.isRequired,
+  subtitle: PropTypes.string,
   activities: PropTypes.arrayOf(PropTypes.object)
 }
--- webapp/src/components/find/Activity.js
+++ webapp/src/components/find/Activity.js
@@ -9,13 +9,32 @@ class Activity extends Component {
   }
   componentDidMount() {
   }
-  render() {
+  renderStory(date, message) {
     return (
-      <div>
+      <div className="story">
+        <div className="story__timestamp">{date}</div>
+        <div className="story__message">{message}</div>
+      </div>
+    )
+  }
+  renderCard(shortUrl) {
+
+    return (
+      <SwipesCard title="Testing file" subtitle="Uploaded yesterday" />
+    )
+  }
+  render() {
+    const { shortUrl, date, message } = this.props.data;
+
+    return (
+      <div className="activity">
+        {this.renderStory(date, message)}
+        {this.renderCard(shortUrl)}
       </div>
     )
   }
 }
+
 export default Activity
 
 Activity.propTypes = {
