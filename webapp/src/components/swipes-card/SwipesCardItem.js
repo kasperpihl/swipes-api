82692822f1ef8be264c977e6a9c4e767fe470c43
iremlopsum <kristjanfou@gmail.com>
2016-08-23 14:04:53 +0300

    fixed a dumb error

--- webapp/src/components/swipes-card/SwipesCardItem.js
+++ webapp/src/components/swipes-card/SwipesCardItem.js
@@ -31,7 +31,7 @@ class SwipesCardItem extends Component {
   }
   renderHeader(actions, title, subtitle, headerImage) {
     const noSubtitleClass = !subtitle ? "swipes-card__header__content--no-subtitle" : '';
-
+    console.log(subtitle);
     return (
       <div className="swipes-card__header">
         <div className="swipes-card__header__dot">
@@ -124,7 +124,10 @@ SwipesCardItem.propTypes = {
       PropTypes.string,
       PropTypes.array
     ]).isRequired,
-    subtitle: PropTypes.string,
+    subtitle:  PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.array
+    ]),
     description: PropTypes.oneOfType([
       PropTypes.string,
       PropTypes.array
