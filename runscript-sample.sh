a02766774d12d22b609d3cda71d150e288f2bfe7
thinklinux <tihomir.valkanov@gmail.com>
2016-09-06 11:36:19 +0300

    change db to the azure's one

--- runscript-sample.sh
+++ runscript-sample.sh
@@ -1,7 +1,7 @@
 # Before you run this script install pm2 module
 # sudo npm install -g pm2
-# Amazon database
-export SW_DATABASE_HOST=52.23.188.131
-export SW_DATABASE_PORT=28019
+# Azure database
+export SW_DATABASE_HOST=rethinkdb-staging6110.cloudapp.net
+
 cd rest
 pm2-dev start swipes.json
--- win-runscript-sample.bat
+++ win-runscript-sample.bat
@@ -1,7 +1,6 @@
 REM Before you run this script install pm2 module
 REM sudo npm install -g pm2
-REM Amazon database
-set SW_DATABASE_HOST=52.23.188.131
-set SW_DATABASE_PORT=28019
+REM Azure database
+set SW_DATABASE_HOST=rethinkdb-staging6110.cloudapp.net
 cd rest
 pm2-dev start swipes.json
