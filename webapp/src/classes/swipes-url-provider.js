64c75b2d11bb5761d499c11e52dac72d755df01e
Kasper Pihl Tornøe <kasper@pihl.it>
2016-09-18 16:21:14 -0700

    Refactored url provider to use new subscriber class

--- webapp/src/classes/swipes-url-provider.js
+++ webapp/src/classes/swipes-url-provider.js
@@ -1,12 +1,13 @@
 import { bindAll, debounce } from './utils'
+import Subscriber from './subscriber'
 import { api } from '../actions'
 
 export default class SwipesUrlProvider {
   constructor(store){
     bindAll(this, ['subscribe', 'unsubscribe', 'fetch', 'setThrottleThreshold'])
-    this._listenersObj = {}
     this._shortUrlData = {}
     this.store = store;
+    this.subscriber = new Subscriber();
     this.urlsToFetch = []
     this.fetchingUrls = null;
     this.setThrottleThreshold(30)
@@ -50,29 +51,11 @@ export default class SwipesUrlProvider {
     this.notify(url, data);
   }
   notify(shortUrl, data){
-    const currentListeners = this._listenersObj[shortUrl];
-    
-    if(currentListeners){
-      currentListeners.forEach(( { listener }) => {
-        listener(data);
-      })
-    }
+    this.subscriber.notify(shortUrl, data);
   }
 
   subscribe(shortUrl, listener, ctx){
-    if(!shortUrl || typeof shortUrl !== 'string'){
-      return console.warn('SwipesUrlProvider: addListener param1 (shortUrl): not set or not string', shortUrl);
-    }
-    if(!listener || typeof listener !== 'function'){
-      return console.warn("SwipesUrlProvider: addListener param2 (listener): not set or not function");
-    }
-    if(typeof ctx !== 'string'){
-      ctx = '';
-    }
-
-    const currentListeners = this._listenersObj[shortUrl] || [];
-    currentListeners.push({listener: listener, context: ctx});
-    this._listenersObj[shortUrl] = currentListeners;
+    this.subscriber.add(shortUrl, listener, ctx);
     const currentData = this.get(shortUrl);
     if(currentData){
       listener(currentData);
@@ -83,41 +66,7 @@ export default class SwipesUrlProvider {
     }
   }
   unsubscribe(shortUrl, listener, ctx){
-    if(!shortUrl && !listener && !ctx){
-      return console.warn('SwipesUrlProvider: removeListener: no params provided');
-    }
-    if(shortUrl){
-      this._removeListenersForShortUrl(shortUrl, listener, ctx);
-    }
-    else{
-      for(var key in this._listenersObj){
-        this._clearEventName(key, listener, context);
-      }
-    }
-  }
-  _removeListenersForShortUrl(shortUrl, listener, ctx){
-
-    const currentListeners = this._listenersObj[shortUrl];
-    if(!currentListeners){
-      return;
-    }
-    // If only event name is provided, remove all
-    if(!listener && !ctx){
-      return delete this._listenersObj[shortUrl];
-    }
-
-    const newListeners = [];
-    for(var i = 0 ; i < currentListeners.length ; i++){
-      const listener = currentListeners[i];
-      if(listener.listener !== listener && listener.context !== ctx)
-        newListeners.push(listener);
-    }
-    if(!newListeners.length){
-      return delete this._listenersObj[shortUrl];
-    }
-    else if(newListeners.length && newListeners.length !== currentListeners.length){
-      this._listenersObj[shortUrl] = newListeners;
-    }
+    this.subscriber.remove(shortUrl, listener, ctx);
   }
 
 }
