98d9b292cc045901121c839527ab3a8e8dd954ef
thinklinux <tihomir.valkanov@gmail.com>
2016-09-24 14:29:26 -0700

    goals.templateList API

--- rest/routes/goals.js
+++ rest/routes/goals.js
@@ -1,33 +1,56 @@
 "use strict";
 
-const express = require('express');
+import express from 'express';
+import r from 'rethinkdb';
+import db from '../db.js';
 
 const router = express.Router();
 
-const fetchGoal = () => {}
+router.post('/goals.templatesList', (req, res, next) => {
+  const q = r.table('goals_templates').orderBy('title');
 
-router.post('/goals.do', (req, res, next) => {
-  const action = req.body.action;
-  /*
-    action.type
-    action.payload
-   */
-  const goalId = req.body.goalId;
-
-  const goal = fetchGoal(goalId);
-  let folder;
-  let file;
-  if(goal.type === 'deliver'){
-    folder = 'deliver';
-    if(goal.subtype === 'collection'){
-      file = 'collection.js';
-    }
-  }
-  const loadedFile = require('../reducers/' + folder + '/' + file);
-  const existingData = goal.data;
-  let newData = loadedFile.do(existingData, action);
-
-  //rethinkdb.save(goal, newData);
-});
+  return db.rethinkQuery(q)
+    .then((templates) => {
+      return res.status(200).json({ok: true, data: templates});
+    })
+    .catch((err) => {
+      return next(err);
+    })
+})
 
 module.exports = router;
+
+
+// "use strict";
+//
+// const express = require('express');
+//
+// const router = express.Router();
+//
+// const fetchGoal = () => {}
+//
+// router.post('/goals.do', (req, res, next) => {
+//   const action = req.body.action;
+//   /*
+//     action.type
+//     action.payload
+//    */
+//   const goalId = req.body.goalId;
+//
+//   const goal = fetchGoal(goalId);
+//   let folder;
+//   let file;
+//   if(goal.type === 'deliver'){
+//     folder = 'deliver';
+//     if(goal.subtype === 'collection'){
+//       file = 'collection.js';
+//     }
+//   }
+//   const loadedFile = require('../reducers/' + folder + '/' + file);
+//   const existingData = goal.data;
+//   let newData = loadedFile.do(existingData, action);
+//
+//   //rethinkdb.save(goal, newData);
+// });
+//
+// module.exports = router;
