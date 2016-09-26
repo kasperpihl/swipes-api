99a198d457ec6d92cca0fd3e7a6d891f213380cd
thinklinux <tihomir.valkanov@gmail.com>
2016-09-26 14:22:44 -0700

    goals.create API endpoint

--- rest/middlewares/goals.js
+++ rest/middlewares/goals.js
@@ -3,11 +3,19 @@ import validator from 'validator';
 import r from 'rethinkdb';
 import db from '../db.js';
 import SwipesError from '../swipes-error.js';
+import {
+  generateSlackLikeId
+} from '../util.js';
 
 const goalsValidate = (req, res, next) => {
   const goal = req.body.goal;
+  const goalId = generateSlackLikeId('G');
 
   goal.steps.map((step) => {
+    const stepId = generateSlackLikeId('');
+
+    step.id = goalId + '-' + stepId;
+
     if (step.type === 'deliver') {
       step.data = {
         deliveries: [{collection: []}]
@@ -17,6 +25,7 @@ const goalsValidate = (req, res, next) => {
     return step;
   })
 
+  res.locals.goalId = goalId;
   res.locals.goal = goal;
 
   return next();
@@ -24,19 +33,29 @@ const goalsValidate = (req, res, next) => {
 
 const goalsCreate = (req, res, next) => {
   const userId = req.userId;
-  const timestamp = new Date().UTC();
+  const organizationId = req.body.organization_id;
   const processId = req.body.process_id;
   const {
+    goalId,
     goal
   } = res.locals;
-  const insertQ = r.table('goals').insert({
-    timestamp,
-    goal,
+  const insertObj = {
+    id: goalId,
     process_id: processId, // T_TODO check if this one exists!
+    organization_id: organizationId,
+    timestamp: r.now(),
     created_by: userId
-  })
+  }
 
-  return next();
+  const insertQ = r.table('goals').insert(Object.assign({}, goal, insertObj))
+
+  return db.rethinkQuery(insertQ)
+    .then(() => {
+      return next();
+    })
+    .catch((err) => {
+      return next(err);
+    })
 }
 
 export {
