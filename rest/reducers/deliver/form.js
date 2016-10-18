64a36379b1252f0c257c8c12c5ff53768f15cdba
Kasper Pihl Tornøe <kasper@pihl.it>
2016-10-18 12:07:39 -0700

    Fixed bug not creating meeting workflow

---
+++ rest/reducers/deliver/form.js
@@ -0,0 +1,17 @@
+"use strict";
+
+import {
+  fromJS,
+  Map
+} from 'immutable';
+
+const init = (step) => {
+  const data = fromJS({});
+
+  return step.merge(new Map({data: data}));
+}
+
+
+export {
+  init
+}
