7b73d9d697d2d0fa6f19bd37d255c4715c45acde
iremlopsum <kristjanfou@gmail.com>
2016-10-23 20:45:22 -0700

    ratio header image for swipes card && preview styles && empty preview

--- webapp/src/components/modals/PreviewModal.js
+++ webapp/src/components/modals/PreviewModal.js
@@ -13,19 +13,24 @@ class PreviewModal extends Component {
     this.state = {
       fileLoaded: false
     }
-    bindAll(this, [ 'fileLoaded']);
+    bindAll(this, [ 'fileLoaded', 'closeModal']);
   }
   componentDidMount() {
+  }
+  closeModal() {
+    const { callback } = this.props;
+
+    callback(null);
   }
   renderTopbar() {
     const { title} = this.props;
 
     return (
       <div className="preview-modal__topbar">
-        <div className="preview-modal__close">
+        <div className="preview-modal__close" onClick={this.closeModal}>
           <CloseIcon className="preview-modal__icon" />
         </div>
-        <div className="preview-modal__title">{title}</div>
+        <div className="preview-modal__title preview-modal__title--topbar">{title}</div>
         {this.renderActions()}
       </div>
     )
@@ -40,13 +45,30 @@ class PreviewModal extends Component {
     return <i className="material-icons preview-modal__icon preview-modal__icon--font">{icon}</i>
   }
   renderActions() {
-    const { actions } = this.props;
+    const { actions, type } = this.props;
 
-    if (actions) {
+    if (actions && type !== null) {
       const icons = actions.map( (action, i) => {
         return (
           <div key={"action-" + i} className="preview-modal__action" data-content={action.title} onClick={action.onClick}>
             {this.renderIcon(action.icon)}
+          </div>
+        )
+      })
+
+      return (
+        <div className="preview-modal__actions">
+          {icons}
+        </div>
+      )
+    }
+
+    if (actions && type === null) {
+      const icons = actions.map( (action, i) => {
+        return (
+          <div key={"action-" + i} className="preview-modal__action" onClick={action.onClick}>
+            {this.renderIcon(action.icon)}
+            <div className="preview-modal__title preview-modal__title--action">{action.title}</div>
           </div>
         )
       })
@@ -67,7 +89,7 @@ class PreviewModal extends Component {
     const { pdf } = this.props;
     const { loaded } = this.state;
 
-    if(!pdf && !loaded) return;
+    if(!pdf || !loaded) return;
 
     return (
       <div className="preview-modal__pdf">
@@ -79,35 +101,63 @@ class PreviewModal extends Component {
     const { img } = this.props;
     const { loaded } = this.state;
 
-    if(!img && !loaded) return;
+    if(!img) return;
+
+    let imageClass = 'preview-modal__image';
+
+    if (loaded) {
+      imageClass += ' preview-modal__image--shown'
+    }
 
     return (
-      <div className="preview-modal__image">
+      <div className={imageClass} onLoad={this.fileLoaded}>
         <img src={img} />
       </div>
     )
   }
-  renderLoader() {
+  renderEmpty() {
+    const { type, actions, title } = this.props;
     const { loaded } = this.state;
 
-    if (!loaded) {
+    if (type === null && !loaded) {
+
+      return (
+        <div className="preview-modal__empty">
+          <div className="preview-modal__title preview-modal__title--empty">{title}</div>
+          {this.renderActions()}
+        </div>
+      )
+    }
+
+  }
+  renderLoader() {
+    const { loaded } = this.state;
+    const { type } = this.props;
+
+    if (!loaded && type !== null) {
       return <Loader center={true} />
     }
   }
   render() {
-    let className = 'preview-modal'
+    const { type } = this.props;
+    let className = 'preview-modal';
+
+    if (type === null) {
+      className += ' preview-modal--empty'
+    }
 
     return (
       <div className={className}>
         {this.renderTopbar()}
         {this.renderImage()}
         {this.renderPDF()}
-
+        {this.renderEmpty()}
         {this.renderLoader()}
       </div>
     )
   }
 }
+
 export default PreviewModal
 
 const { string } = PropTypes;
