11fb08412c8e7058ba8904543d83d7a14d760803
thinklinux <tihomir.valkanov@gmail.com>
2016-10-24 14:56:39 -0700

    selecting a forked pattern

--- webapp/src/containers/overlays/StartGoal.js
+++ webapp/src/containers/overlays/StartGoal.js
@@ -2,7 +2,7 @@ import React, { Component, PropTypes } from 'react'
 import { connect } from 'react-redux'
 
 import { fromJS } from 'immutable'
-import { workflows, overlay } from '../../actions'
+import { workflows, overlay, modal } from '../../actions'
 import { bindAll } from '../../classes/utils'
 
 import WorkflowList from '../../components/start-goal/WorkflowList'
@@ -15,12 +15,66 @@ class StartGoal extends Component {
     bindAll( this, ['didSelectItem', 'clickedStore']);
   }
   didSelectItem(id){
-    const { pushOverlay, workflows } = this.props;
-    pushOverlay({component: "ConfirmGoal", title: "Confirm", props: {data: workflows.get(id).toJS()}});
+    const { pushOverlay, workflows, loadModal, users } = this.props;
+    const idToCheck = 'PGR5OHKL6';
+
+    if (workflows.get(id).toJS().id === idToCheck) {
+      const filteredWorkspaces = workflows.toArray().filter((x) => {
+        if (x.get('id') === idToCheck || x.get('parent_id') === idToCheck) {
+          return true;
+        }
+
+        return false;
+      });
+
+      const mappedItems = filteredWorkspaces.map((x) => {
+        let title;
+        let img;
+
+        if (x.get('created_by')) {
+          title = x.get('title');
+          img = users.getIn([x.get('created_by'), 'profile_pic']);
+        } else {
+          title = 'Generic';
+          img = {
+            element: x.get('img')
+          }
+        }
+
+        return {
+          title,
+          img
+        }
+      })
+
+      loadModal({
+        title: 'Choose Pattern',
+        data: {
+          list: {
+            items: mappedItems,
+            emptyText: 'No patterns found!'
+          }
+        }
+      }, (e) => {
+        if (e) {
+          pushOverlay({component: "ConfirmGoal", title: "Confirm", props: {data: filteredWorkspaces[e.item]}});
+        }
+      })
+    } else {
+      pushOverlay({component: "ConfirmGoal", title: "Confirm", props: {data: workflows.get(id).toJS()}});
+    }
   }
   renderList(){
     const { workflows } = this.props;
-    return <WorkflowList data={workflows.toArray().map((i) => i.toJS())} callback={this.didSelectItem} />
+    const filteredWorkflows = [];
+
+    workflows.toArray().forEach((i) => {
+      if (!i.get('parent_id')) {
+        filteredWorkflows.push(i.toJS());
+      }
+    });
+
+    return <WorkflowList data={filteredWorkflows} callback={this.didSelectItem} />
   }
   clickedStore(){
     const { pushOverlay } = this.props;
@@ -39,11 +93,13 @@ class StartGoal extends Component {
 
 function mapStateToProps(state) {
   return {
-    workflows: state.get('workflows')
+    workflows: state.get('workflows'),
+    users: state.get('users')
   }
 }
 
 const ConnectedStartGoal = connect(mapStateToProps, {
+  loadModal: modal.load,
   pushOverlay: overlay.push
 })(StartGoal)
 export default ConnectedStartGoal
