2374fc10f172c595f233c4931fd1c0d91dbb48a7
thinklinux <tihomir.valkanov@gmail.com>
2016-10-25 14:27:50 -0700

    hacked tags with autosuggestion. Need styles to actually work

--- webapp/src/components/confirm-goal/TagsInput.js
+++ webapp/src/components/confirm-goal/TagsInput.js
@@ -1,5 +1,10 @@
+// Modules used to implement tags input with autosuggest
+// https://github.com/olahol/react-tagsinput
+// https://github.com/moroshko/react-autosuggest
+
 import React, { Component, PropTypes } from 'react'
 import Tags from 'react-tagsinput'
+import Autosuggest from 'react-autosuggest'
 import { bindAll } from '../../classes/utils'
 
 import './styles/tags-input.scss'
@@ -7,19 +12,72 @@ import './styles/tags-input.scss'
 class TagsInput extends Component {
   constructor(props) {
     super(props)
-    this.state = {tags: []}
-    bindAll(this, ['handleChange'])
+    this.state = {
+      tags: [],
+      suggestions: [
+        'Product Team',
+        'Marketing Team',
+        'Bug',
+        'Improvement'
+      ]
+    }
+    bindAll(this, [
+      'handleChange',
+      'autosuggestRenderInput',
+      'onSuggestionsFetchRequested',
+      'onSuggestionsClearRequested'
+    ])
   }
   componentDidMount() {
   }
   handleChange(tags) {
-    console.log(tags)
     this.setState({tags})
+  }
+  getSuggestions(value) {
+    const suggestions = this.state.suggestions;
+    const inputValue = value.trim().toLowerCase();
+    const inputLength = inputValue.length;
+
+    return inputLength === 0 ? [] : suggestions.filter(s =>
+      s.toLowerCase().slice(0, inputLength) === inputValue
+    );
+  }
+  onSuggestionsFetchRequested({ value }) {
+     this.setState({
+       suggestions: this.getSuggestions(value)
+     })
+  }
+  onSuggestionsClearRequested() {
+    console.log('Clear suggestions');
+    // this.setState({
+    //   suggestions: []
+    // })
+  }
+  autosuggestRenderInput(props) {
+    // Removing addTag attr
+    // We need it only if we are using the default RenderInput
+    delete props.addTag;
+
+    return (
+      <Autosuggest
+        ref={props.ref}
+        suggestions={this.state.suggestions}
+        shouldRenderSuggestions={(value) => value && value.trim().length > 0}
+        onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+        onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+        getSuggestionValue={(suggestion) => suggestion.name}
+        renderSuggestion={(suggestion) => <span>{suggestion.name}</span>}
+        inputProps={props}
+        onSuggestionSelected={(e, {suggestion}) => {
+          this.refs.tagsinput.addTag(suggestion.name)
+        }}
+      />
+    )
   }
   render() {
 
     return (
-      <Tags value={this.state.tags} onChange={this.handleChange} />
+      <Tags ref="tagsinput" value={this.state.tags} onChange={this.handleChange} renderInput={this.autosuggestRenderInput} />
     )
   }
 }
