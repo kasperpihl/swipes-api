d159ccd708f1b2bc644fb49e5604def13faa8164
thinklinux <tihomir.valkanov@gmail.com>
2016-10-26 11:18:33 -0700

    thank you Kriiis. New autosuggest tag something

--- webapp/src/components/confirm-goal/TagsInput.js
+++ webapp/src/components/confirm-goal/TagsInput.js
@@ -3,8 +3,7 @@
 // https://github.com/moroshko/react-autosuggest
 
 import React, { Component, PropTypes } from 'react'
-import Tags from 'react-tagsinput'
-import Autosuggest from 'react-autosuggest'
+import ReactTags from 'react-tag-autocomplete'
 import { bindAll } from '../../classes/utils'
 
 import './styles/tags-input.scss'
@@ -13,10 +12,10 @@ class TagsInput extends Component {
   constructor(props) {
     super(props)
     this.storeSuggestions = [
-      'Product Team',
-      'Marketing Team',
-      'Bug',
-      'Improvement'
+      {id: 3, name: 'Product Team'},
+      {id: 4, name: 'Marketing Team'},
+      {id: 5, name: 'Bug'},
+      {id: 7, name: 'Improvement'}
     ]
 
     this.state = {
@@ -24,70 +23,33 @@ class TagsInput extends Component {
       suggestions: []
     }
     bindAll(this, [
-      'handleChange',
-      'autosuggestRenderInput',
-      'onSuggestionsFetchRequested',
-      'onSuggestionsClearRequested',
-      'handleAutoSuggestSelectWithKey'
+      'handleDelete',
+      'handleAddition'
     ])
   }
   componentDidMount() {
   }
-  handleChange(tags) {
-    this.setState({tags})
+  handleDelete(i) {
+    console.log(i)
+    var tags = this.state.tags
+    tags.splice(i, 1)
+    this.setState({ tags: tags })
   }
-  getSuggestions(value) {
-    const inputValue = value.trim().toLowerCase()
-    const inputLength = inputValue.length
-
-    return inputLength === 0 ? [] : this.storeSuggestions.filter(s =>
-      s.toLowerCase().slice(0, inputLength) === inputValue
-    )
-  }
-  onSuggestionsFetchRequested({ value }) {
-     this.setState({
-       suggestions: this.getSuggestions(value)
-     })
-  }
-  onSuggestionsClearRequested() {
-    this.setState({
-      suggestions: []
-    })
-  }
-  // handleAutoSuggestSelectWithKey(event, {newValue, method}) {
-  //   if(method === 'enter') {
-  //     event.preventDefault()
-  //   } else {
-  //     this.props.onChange(event)
-  //   }
-  // }
-  autosuggestRenderInput(props) {
-    // Removing addTag attr
-    // We need it only if we are using the default RenderInput
-    delete props.addTag
-
-    //props.onChange = this.handleAutoSuggestSelectWithKey
-
-    return (
-      <Autosuggest
-        ref={props.ref}
-        suggestions={this.state.suggestions}
-        shouldRenderSuggestions={(value) => value && value.trim().length > 0}
-        onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
-        onSuggestionsClearRequested={this.onSuggestionsClearRequested}
-        getSuggestionValue={(suggestion) => {suggestion}}
-        renderSuggestion={(suggestion) => <span>{suggestion}</span>}
-        inputProps={props}
-        onSuggestionSelected={(e, {suggestion}) => {
-          this.refs.tagsinput.addTag(suggestion)
-        }}
-      />
-    )
+  handleAddition(tag) {
+    console.log(tag)
+    var tags = this.state.tags
+    tags.push(tag)
+    this.setState({ tags: tags })
   }
   render() {
 
     return (
-      <Tags ref="tagsinput" value={this.state.tags} onChange={this.handleChange} renderInput={this.autosuggestRenderInput} />
+      <ReactTags
+        allowNew={true}
+        tags={this.state.tags}
+        suggestions={this.storeSuggestions}
+        handleDelete={this.handleDelete}
+        handleAddition={this.handleAddition} />
     )
   }
 }
