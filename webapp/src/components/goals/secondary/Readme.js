7123ba8a6f2212ef4378961c123e7151b55317fc
iremlopsum <kristjanfou@gmail.com>
2016-10-30 15:32:35 -0700

    read me styles

--- webapp/src/components/goals/secondary/Readme.js
+++ webapp/src/components/goals/secondary/Readme.js
@@ -1,5 +1,5 @@
 import React, { Component, PropTypes } from 'react'
-
+import * as Icons from '../../icons'
 import '../styles/readme.scss'
 
 class Readme extends Component {
@@ -9,9 +9,36 @@ class Readme extends Component {
   }
   componentDidMount() {
   }
+  renderIcon(icon){
+    const Comp = Icons[icon];
+
+    if (Comp) {
+      return <Comp className="sw-read-me__icon sw-read-me__icon--svg"/>;
+    }
+
+    return <i className="material-icons sw-read-me__icon sw-read-me__icon--font">{icon}</i>
+  }
+  renderHeader() {
+
+    return (
+      <div className="sw-read-me__header">
+        {this.renderIcon('ListIcon')}
+        <div className="sw-read-me__title">Checklist</div>
+        <div className="sw-read-me__description">Things to improve</div>
+      </div>
+    )
+  }
   render() {
     return (
-      <div>Readme</div>
+      <div className="sw-read-me">
+        {this.renderHeader()}
+        <div className="sw-read-me__content">
+          Swipes helps companies work fast even as they grow. You can create simple workflows for meetings, projects or onboardings.
+          <br/>
+          <br/>
+          Share them with your team. So they can execute them across Slack, Dropbox, Evernote, Gmail etc. All from a single place.
+        </div>
+      </div>
     )
   }
 }
