d4d1dc4634911673ad5c5928d070030c4105a535
thinklinux <tihomir.valkanov@gmail.com>
2016-10-30 20:57:04 -0700

    add readme secondary to note

--- rest/reducers/deliver/note.js
+++ rest/reducers/deliver/note.js
@@ -4,11 +4,19 @@ import {
 } from 'immutable';
 
 const init = (step) => {
-  const data = fromJS({
-    initialData: {"entityMap":{},"blocks":[{"key":"9t428","text":"What are we designing for?\n...\nWho are we designing for?\n...\nWhat features do we need?\n...\nWhat’s an example use case?\n...","type":"unstyled","depth":0,"inlineStyleRanges":[{"offset":118,"length":1,"style":"BOLD"}],"entityRanges":[],"data":{}}]}
+  const setup = fromJS({
+    secondary: [
+      {
+        type: 'readme',
+        data: ''
+      }
+    ],
+    data: {
+      initialData: {"entityMap":{},"blocks":[{"key":"9t428","text":"What are we designing for?\n...\nWho are we designing for?\n...\nWhat features do we need?\n...\nWhat’s an example use case?\n...","type":"unstyled","depth":0,"inlineStyleRanges":[{"offset":118,"length":1,"style":"BOLD"}],"entityRanges":[],"data":{}}]}
+    }
   });
 
-  return step.merge(Map({data: data}));
+  return step.merge(new Map(setup));
 }
 
 
