20a0b2ea250b4e19f68a6ca0781c8915318c3769
thinklinux <tihomir.valkanov@gmail.com>
2016-12-01 15:38:58 -0800

    cleaning lady

--- runscript-sample.sh
+++
@@ -1,7 +0,0 @@
-# Before you run this script install pm2 module
-# sudo npm install -g pm2
-# Azure database
-export SW_DATABASE_HOST=rethinkdb-staging6110.cloudapp.net
-
-cd rest
-pm2-dev start swipes.json
