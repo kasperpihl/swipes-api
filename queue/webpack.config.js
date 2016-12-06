a9b87adbfea67cacd522dbedf22c1e6217a49cd3
thinklinux <tihomir.valkanov@gmail.com>
2016-12-05 16:57:15 -0800

    base for queue

---
+++ queue/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const fs = require('fs');
+
+const nodeModules = {};
+fs.readdirSync('node_modules')
+  .filter((x) => {
+    return ['.bin'].indexOf(x) === -1;
+  })
+  .forEach((mod) => {
+    nodeModules[mod] = `commonjs ${mod}`;
+  });
+
+module.exports = {
+  context: __dirname,
+  devtool: 'eval',
+  entry: './index',
+  target: 'node',
+  externals: nodeModules,
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'index.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ['babel-loader', 'eslint-loader'],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+};
