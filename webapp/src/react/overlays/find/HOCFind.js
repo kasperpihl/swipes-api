732f6f0f6f1071989a31cca8f5df75ddb29fef7a
Kasper Pihl Tornøe <kasper@pihl.it>
2016-12-07 10:11:19 -0800

    Cleaning up search

--- webapp/src/react/overlays/find/HOCFind.js
+++ webapp/src/react/overlays/find/HOCFind.js
@@ -2,7 +2,7 @@ import React, { Component, PropTypes } from 'react';
 import { map } from 'react-immutable-proptypes';
 import { connect } from 'react-redux';
 import ReactCSSTransitionGroup from 'react/lib/ReactCSSTransitionGroup';
-import { main, search, api, modal } from 'actions';
+import * as actions from 'actions';
 import { bindAll } from 'classes/utils';
 
 import TabBar from 'components/tab-bar/TabBar';
@@ -14,11 +14,8 @@ import SearchResults from './SearchResults';
 class HOCFind extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      currentTabIndex: 0,
-    };
 
-    bindAll(this, ['dotDragStart', 'onChange', 'onKeyUp', 'onCardClick', 'onCardShare', 'onCardAction']);
+    bindAll(this, ['onChange', 'onKeyUp', 'onCardClick']);
     this.unhandledDocs = [];
   }
   componentDidMount() {
@@ -61,14 +58,14 @@ class HOCFind extends Component {
               const fullFilePath = dropboxFolder + res.data.metadata.path_display;
               const type = doc.source_content_type;
               const link = res.data.link;
-              const actions = [];
+              const buttons = [];
               const newData = {
                 type: null,
                 title: doc.filename,
               };
 
               if (dropboxFolder) {
-                actions.push({
+                buttons.push({
                   icon: 'DesktopIcon',
                   title: 'Open on Desktop',
                   onClick: () => {
@@ -77,7 +74,7 @@ class HOCFind extends Component {
                 });
               }
 
-              actions.push({
+              buttons.push({
                 icon: 'EarthIcon',
                 title: 'Open in Dropbox.com',
                 onClick: () => {
@@ -92,7 +89,7 @@ class HOCFind extends Component {
                 },
               });
 
-              newData.actions = actions;
+              newData.actions = buttons;
 
               // const path = res.data.metadata.path_display;
 
@@ -112,54 +109,10 @@ class HOCFind extends Component {
       }
     }
   }
-  onCardShare(card, data) {
-    const { recent, searchResults } = this.props;
-
-    if (data.checksum) { // Is activity
-      const activity = recent.find(act => act.get('checksum') === data.checksum);
-      const share = {
-        checksum: data.checksum,
-        permission: {
-          type: 'public',
-          account_id: activity.get('account_id'),
-        },
-        meta: activity.get('meta'),
-      };
-      this.props.startDraggingDot('search', share);
-    } else if (data.xendo_id) {
-      const obj = searchResults.find((res) => {
-        const id = res.getIn(['doc', 'id']);
-        return id === data.xendo_id;
-      });
-
-      if (obj) {
-        this.props.startDraggingDot('search', obj.get('shareData').toJS());
-      }
-    }
-  }
   onKeyUp(e) {
     if (e.keyCode === 13) {
-      if (this.state.currentTabIndex !== 1) {
-        this.setState({ currentTabIndex: 1 });
-      }
       this.props.search(this.refs.searchInput.value);
     }
-  }
-  onChange(i) {
-    if (this.state.currentTabIndex !== i) {
-      this.setState({ currentTabIndex: i });
-    }
-  }
-  previewNumberOfItems() {
-    return 5;
-  }
-  previewRenderForItem() {
-  }
-  previewMetaForItem() {
-    return {
-      title: 'Lalala',
-      buttons: [],
-    };
   }
   renderSearchField() {
     return (
@@ -172,34 +125,23 @@ class HOCFind extends Component {
       />
     );
   }
-  renderTabs() {
-    const tabs = ['Activity', 'Search Results'];
+  renderContent() {
+    const { groupedResults, searching, searchQuery } = this.props;
 
     return (
-      <div className="find-overlay__tabs">
-        <TabBar
-          tabs={tabs}
-          onChange={this.onChange}
-          align="left"
-          activeTab={this.state.currentTabIndex}
-        />
-      </div>
+      <SearchResults
+        searching={searching}
+        query={searchQuery}
+        title="Search"
+        results={groupedResults.toJS()}
+        cardDelegate={this}
+      />
     );
-  }
-  renderContent() {
-    const { recent, groupedResults, searching, searchQuery } = this.props;
-
-    if (this.state.currentTabIndex === 0) {
-      return <Activities title="Recent" subtitle="Mine" key={`activities-${this.state.currentTabIndex}`} activities={recent.slice(0, 10)} cardDelegate={this} />;
-    }
-
-    return <SearchResults searching={searching} query={searchQuery} title="Search" key={`search-results-${this.state.currentTabIndex}`} results={groupedResults.toJS()} cardDelegate={this} />;
   }
   render() {
     return (
       <div className="find-overlay">
         {this.renderSearchField()}
-        {this.renderTabs()}
         <ReactCSSTransitionGroup
           transitionName="fade"
           component="div"
@@ -220,8 +162,6 @@ HOCFind.propTypes = {
   searchResults: map,
   request: func,
   loadModal: func,
-  recent: map,
-  startDraggingDot: func,
   search: func,
   groupedResults: map,
   searching: bool,
@@ -235,15 +175,12 @@ function mapStateToProps(state) {
     groupedResults: results.groupBy(res => res.getIn(['doc', 'source'])),
     searching: state.getIn(['search', 'searching']),
     searchQuery: state.getIn(['search', 'query']),
-    draggingDot: state.getIn(['main', 'draggingDot']),
-    recent: state.getIn(['activity', 'recent']),
   };
 }
 
 const ConnectedHOCFind = connect(mapStateToProps, {
-  search: search.search,
-  request: api.request,
-  loadModal: modal.load,
-  startDraggingDot: main.startDraggingDot,
+  search: actions.main.search,
+  request: actions.api.request,
+  loadModal: actions.modal.load,
 })(HOCFind);
 export default ConnectedHOCFind;
