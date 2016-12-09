25b52f01f0cfd8a0412cfc444142bf9c4c0c13bb
thinklinux <tihomir.valkanov@gmail.com>
2016-12-08 18:45:23 -0800

    notifications in rtm.start

--- server/api/routes/middlewares/db_utils/notifications.js
+++ server/api/routes/middlewares/db_utils/notifications.js
@@ -9,6 +9,17 @@ const dbNotificationsMarkAsSeen = ({ notification_ids }) => {
   return db.rethinkQuery(q);
 };
 
+const dbNotificationsGetAllByIdOrderByTs = ({ user_id }) => {
+  const q =
+    r.table('notifications')
+      .getAll(user_id, { index: 'user_id' })
+      .orderBy(r.desc('ts'))
+      .limit(100);
+
+  return db.rethinkQuery(q);
+};
+
 export {
   dbNotificationsMarkAsSeen,
+  dbNotificationsGetAllByIdOrderByTs,
 };
