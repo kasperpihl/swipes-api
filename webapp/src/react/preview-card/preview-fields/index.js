c7c3f8f0662a8782088fc70432cb245e695bac40
Kasper Pihl Tornøe <kasper@pihl.it>
2016-12-09 15:35:47 -0800

    Logic for the preview card

---
+++ webapp/src/react/preview-card/preview-fields/index.js
@@ -0,0 +1,11 @@
+import Attachment from './attachment/Attachment';
+import Header from './header/Header';
+import Message from './message/Message';
+import Note from './note/Note';
+
+export {
+  Attachment,
+  Header,
+  Message,
+  Note,
+};
