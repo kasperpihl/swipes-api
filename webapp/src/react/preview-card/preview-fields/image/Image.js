c7fd6ee299adfb5fb86d34c5923eef3030c2eefd
iremlopsum <kristjanfou@gmail.com>
2016-12-09 17:42:50 -0800

    preview layout

---
+++ webapp/src/react/preview-card/preview-fields/image/Image.js
@@ -0,0 +1,44 @@
+import React, { Component, PropTypes } from 'react';
+import { mapContains } from 'react-immutable-proptypes';
+
+import './styles/image';
+
+class Image extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      rawSize: false,
+    };
+    this.toggleRawSize = this.toggleRawSize.bind(this);
+  }
+  toggleRawSize() {
+    const { rawSize } = this.state;
+    this.setState({ rawSize: !rawSize });
+    console.log('name');
+  }
+  render() {
+    const { data } = this.props;
+    const { rawSize } = this.state;
+    let className = 'preview-image';
+
+    if (rawSize) {
+      className += ' preview-image--full-size';
+    }
+
+    return (
+      <div className={className} onClick={this.toggleRawSize}>
+        <img src={data.get('img')} className="preview-image__image" role="presentation" />
+      </div>
+    );
+  }
+}
+
+export default Image;
+
+const { string } = PropTypes;
+
+Image.propTypes = {
+  data: mapContains({
+    img: string,
+  }),
+};
--- webapp/src/react/preview-card/preview-fields/index.js
+++ webapp/src/react/preview-card/preview-fields/index.js
@@ -2,10 +2,12 @@ import Attachment from './attachment/Attachment';
 import Header from './header/Header';
 import Message from './message/Message';
 import Note from './note/Note';
+import Image from './image/Image';
 
 export {
   Attachment,
   Header,
   Message,
   Note,
+  Image,
 };
