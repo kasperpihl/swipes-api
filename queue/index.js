bb3722a8786fa98209884f03c5aa94700cd7c1b3
Kasper Pihl Tornøe <kasper@pihl.it>
2016-12-09 23:08:23 -0800

    Logging res

--- queue/index.js
+++ queue/index.js
@@ -37,13 +37,13 @@ app.use('/process', bodyParser.json(), (originalReq, originalRes, originalNext) 
     originalReq.body,
     ...middlewares[event_type],
     (req, res, next) => {
+      console.log('res', res);
       return originalRes.status(200).json({ ok: true, res: res.locals });
     },
     (err, req, res, next) => {
       return originalNext(err);
     },
   );
-
   return composer.run();
 });
 
