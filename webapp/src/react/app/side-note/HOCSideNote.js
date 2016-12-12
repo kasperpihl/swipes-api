f60e6527e6cec1442cc022199b9ef3e4c8904ae3
Kasper Pihl Tornøe <kasper@pihl.it>
2016-12-12 12:21:40 -0800

    Refactor minor on stylecontrol

--- webapp/src/react/app/side-note/HOCSideNote.js
+++ webapp/src/react/app/side-note/HOCSideNote.js
@@ -10,32 +10,39 @@ import './styles/side-note';
 class HOCSideNote extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = { editorState: this.parseInitialData() };
     this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
+    this.onChange = this.onChange.bind(this);
+  }
+  componentWillReceiveProps(nextProps) {
+
+  }
+  onChange(editorState) {
+    this.setState({ editorState });
   }
   saveData(data) {
     return data.set('editorState', convertToRaw(data.get('editorState').getCurrentContent()));
   }
-  parseInitialData(data) {
+  parseInitialData(initialState) {
     let editorState = NoteEditor.getEmptyEditorState();
-    if (data && data.get('editorState')) {
-      console.log(data.get('editorState').toJS());
-      const raw = JSON.parse(JSON.stringify(data.get('editorState').toJS()));
+    if (initialState) {
+      console.log(initialState.toJS());
+      const raw = JSON.parse(JSON.stringify(initialState.toJS()));
       editorState = EditorState.push(editorState, convertFromRaw(raw));
     }
 
-    return data.set('editorState', editorState);
+    return editorState;
   }
   componentDidMount() {
   }
   render() {
     const { note } = this.props;
+    const { editorState } = this.state;
 
-    const initialData = this.parseInitialData(Map());
     return (
       <div className="side-note">
         <NoteEditor
-          editorState={initialData.get('editorState')}
+          editorState={editorState}
           onChange={this.onChange}
         />
       </div>
