9b82826c8f57554aaf98388822e139b96b8d775a
thinklinux <tihomir.valkanov@gmail.com>
2017-01-06 18:43:17 +0200

    gmail authorization

--- server/api/services/gmail/auth.js
+++ server/api/services/gmail/auth.js
@@ -1,23 +1,12 @@
-import config from 'config';
 import google from 'googleapis';
-// import {
-//   request,
-// } from './request';
+import {
+  createClient,
+  scopes,
+} from './utils';
 
-const OAuth2 = google.auth.OAuth2;
-const gmailConfig = config.get('gmail');
+const gmail = google.gmail('v1');
 const authUrl = (data, callback) => {
-  const oauth2Client = new OAuth2(
-    gmailConfig.clientId,
-    gmailConfig.clientSecret,
-    gmailConfig.redirectUri,
-  );
-  const scopes = [
-    'https://mail.google.com/',
-    'https://www.googleapis.com/auth/gmail.modify',
-    'https://www.googleapis.com/auth/gmail.compose',
-    'https://www.googleapis.com/auth/gmail.send',
-  ];
+  const oauth2Client = createClient();
 
   const url = oauth2Client.generateAuthUrl({
     // 'online' (default) or 'offline' (gets refresh_token)
@@ -31,43 +20,37 @@ const authUrl = (data, callback) => {
   });
 };
 const authData = (data, callback) => {
-  // if (!data.query) {
-  //   return callback('no_query_object');
-  // }
-  //
-  // const code = data.query.code;
-  //
-  // if (!code) {
-  //   return callback('no_code');
-  // }
-  //
-  // const auth_data = {};
-  // const method = 'oauth.access';
-  // const params = {
-  //   code,
-  //   client_id: gmailConfig.clientId,
-  //   client_secret: gmailConfig.clientSecret,
-  //   redirect_uri: gmailConfig.redirectUri,
-  // };
+  if (!data.query) {
+    return callback('no_query_object');
+  }
 
-  console.log('authDATA');
-  // return request({ auth_data, method, params }, (err, res) => {
-  //   if (err) {
-  //     console.log(err);
-  //   }
-  //
-  //   const auth_data = Object.assign({}, res);
-  //
-  //   delete auth_data.ok;
-  //
-  //   const data = {
-  //     auth_data,
-  //     id: res.team_id,
-  //     show_name: res.team_name,
-  //   };
-  //
-  //   return callback(null, data);
-  // });
+  const code = data.query.code;
+
+  if (!code) {
+    return callback('no_code');
+  }
+
+  const oauth2Client = createClient();
+
+  oauth2Client.getToken(code, (err, tokens) => {
+    oauth2Client.setCredentials(tokens);
+
+    gmail.users.getProfile({
+      userId: 'me',
+      auth: oauth2Client,
+    }, (err, res) => {
+      const auth_data = Object.assign({}, res, tokens);
+      const data = {
+        auth_data,
+        id: res.emailAddress,
+        show_name: res.emailAddress,
+      };
+
+      return callback(null, data);
+    });
+  });
+
+  return null;
 };
 
 export {
---
+++ server/api/services/gmail/utils.js
@@ -0,0 +1,26 @@
+import config from 'config';
+import google from 'googleapis';
+
+const gmailConfig = config.get('gmail');
+const createClient = () => {
+  const OAuth2 = google.auth.OAuth2;
+  const oauth2Client = new OAuth2(
+    gmailConfig.clientId,
+    gmailConfig.clientSecret,
+    gmailConfig.redirectUri,
+  );
+
+  return oauth2Client;
+};
+
+const scopes = [
+  'https://mail.google.com/',
+  'https://www.googleapis.com/auth/gmail.modify',
+  'https://www.googleapis.com/auth/gmail.compose',
+  'https://www.googleapis.com/auth/gmail.send',
+];
+
+export {
+  createClient,
+  scopes,
+};
