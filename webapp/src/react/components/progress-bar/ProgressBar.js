34b4cfc70ecce34cf6757bfbcdbadd8f4bae7ce9
iremlopsum <kristjanfou@gmail.com>
2017-01-17 12:27:19 -0800

    ficed progress bar properly

--- webapp/src/react/components/progress-bar/ProgressBar.js
+++ webapp/src/react/components/progress-bar/ProgressBar.js
@@ -1,5 +1,4 @@
 import React, { Component, PropTypes } from 'react';
-import { bindAll } from 'classes/utils';
 
 import './styles/progress-bar';
 
@@ -7,56 +6,27 @@ class ProgressBar extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      showTooltip: false,
-      tooltipX: 0,
     };
-    bindAll(this, ['handleMouseEnter', 'handleMouseLeave']);
-  }
-  handleMouseEnter(e) {
-    const { showTooltip } = this.state;
-
-    if (!showTooltip) {
-      this.setState({ showTooltip: true });
-      this.setState({ tooltipX: e.clientX });
-    }
-  }
-  handleMouseLeave(e) {
-    const { showTooltip } = this.state;
-
-    if (showTooltip) {
-      this.setState({ showTooltip: false });
-    }
   }
   renderTooltip() {
     const { steps } = this.props;
 
     const stepsHTML = steps.map((step, i) => {
-      let className = 'progress-bar-tooltip';
+      let className = 'tooltip';
 
       if (step.get('completed')) {
-        className += ' progress-bar-tooltip--completed';
+        className += ' tooltip--completed';
       }
 
       return (
         <div className={className} key={i}>
-          <div className="progress-bar-tooltip__title">{i + 1}. {step.get('title')}</div>
-          <div className="progress-bar-tooltip__assignee">{step.get('assignees').toJS()[0]}</div>
+          <div className="tooltip__title">{i + 1}. {step.get('title')}</div>
+          <div className="tooltip__assignee">{step.get('assignees').toJS()[0]}</div>
         </div>
       );
     });
 
-    const { showTooltip, tooltipX } = this.state;
-    let className = 'progress-bar__tooltip';
-
-    if (showTooltip) {
-      className += ' progress-bar__tooltip--show';
-    }
-
-    // const styles = {
-    //   left: `${tooltipX - 9}px`,
-    // };
-    //
-    // console.log('styles', styles);
+    const className = 'progress-bar__tooltip';
 
     return (
       <div className={className}>
@@ -70,14 +40,17 @@ class ProgressBar extends Component {
       completed,
       onClick,
     } = this.props;
-    const progressLength = 100 - ((completed * 100) / length);
-    const styles = {
-      WebkitClipPath: `inset(0 ${progressLength}% 0 0 round 5px)`,
-    };
+    const styles = {};
+    console.log('completed', completed);
+
+    if (completed) {
+      const progressLength = 100 - ((completed * 100) / length);
+      styles.WebkitClipPath = `inset(0 ${progressLength}% 0 0 round 5px)`;
+    }
 
     return (
       <div className="progress-bar" onClick={onClick}>
-        <div className="progress-bar__filling" style={styles} onMouseEnter={this.handleMouseEnter} onMouseLeave={this.handleMouseLeave} />
+        <div className="progress-bar__progress" style={styles} />
         {this.renderTooltip()}
       </div>
     );
