d372b6c926441110c317816d67568f9bf01a3649
Kasper Pihl Tornøe <kasper@pihl.it>
2017-01-19 10:21:35 -0800

    Initial logic for tabmenu

--- webapp/src/react/context-menus/tab-menu/TabMenu.js
+++ webapp/src/react/context-menus/tab-menu/TabMenu.js
@@ -1,23 +1,107 @@
 import React, { Component, PropTypes } from 'react';
+import { setupDelegate, randomString } from 'classes/utils';
+import TabBar from 'components/tab-bar/TabBar';
 
 class TabMenu extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      tabIndex: 0,
+      query: '',
+      loading: false,
+      results: [],
+    };
+    if (typeof props.initialTabIndex === 'number') {
+      this.state.tabIndex = props.initialTabIndex;
+    }
+    this.callDelegate = setupDelegate(props.delegate);
   }
   componentDidMount() {
   }
+  componentWillUnmount() {
+    this.qId = undefined;
+  }
+  tabDidChange(nav, index) {
+    if (this.state.tabIndex !== index) {
+      this.setState({ tabIndex: index });
+    }
+  }
+  renderSearchField() {
+    const { search } = this.props;
+    const { query } = this.state;
+  }
+  renderTabBar() {
+    const { tabs } = this.props;
+    const { tabIndex } = this.state;
+    if (!tabs || !tabs.length) {
+      return undefined;
+    }
+    return (
+      <TabBar
+        tabs={tabs}
+        activeTab={tabIndex}
+        delegate={this}
+      />
+    );
+  }
+  resultsCallback(qId, results) {
+    if (this.qId && qId === this.qId) {
+      this.setState({
+        results,
+        loading: false,
+      });
+    }
+  }
+  fetchResults() {
+    const {
+      tabs,
+      tabIndex,
+      query,
+      loading,
+    } = this.state;
+
+    if (!loading) {
+      this.setState({ loading: true });
+    }
+
+    // Setup id to make sure we get the latest.
+    this.qId = randomString(6);
+    const callback = this.resultsCallback.bind(this, this.qId);
+
+    let params = ['onResults', callback];
+    if (query.length) {
+      params = ['onSearchResults', query, callback];
+    } else if (tabs && tabs.length) {
+      params = ['onTabResults', tabIndex, callback];
+    }
+
+    const results = this.callDelegate.apply(null, params);
+    if (results) {
+      this.resultsCallback(this.qId, results);
+      this.qId = undefined;
+      this.setState({ loading: false });
+    }
+  }
+  renderResultList() {
+    const {
+      results,
+      loading,
+    } = this.state;
+  }
   render() {
     return (
-      <div className="className" />
+      <div className="tab-menu" />
     );
   }
 }
 
 export default TabMenu;
 
-const { string } = PropTypes;
+const { object, string, arrayOf, number } = PropTypes;
 
 TabMenu.propTypes = {
-  removeThis: string.isRequired,
+  search: string,
+  initialTabIndex: number,
+  tabs: arrayOf(string),
+  delegate: object.isRequired,
 };
