3279c08d33724af27266df936df603c471994a00
Kasper Pihl Tornøe <kasper@pihl.it>
2017-01-20 09:10:52 -0800

    Removed image from index

--- webapp/src/react/preview/elements/index.js
+++ webapp/src/react/preview/elements/index.js
@@ -1,11 +1,9 @@
 import header from './header/Header';
 import message from './message/Message';
 import note from './note/Note';
-import image from './image/Image';
 
 export {
   header,
   message,
   note,
-  image,
 };
