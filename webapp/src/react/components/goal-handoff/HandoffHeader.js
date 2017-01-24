c4b57474e503ab403db2c956995859bfe4ea0252
iremlopsum <kristjanfou@gmail.com>
2017-01-24 15:37:10 -0800

    handoff header

--- webapp/src/react/components/goal-handoff/HandoffHeader.js
+++ webapp/src/react/components/goal-handoff/HandoffHeader.js
@@ -1,21 +1,56 @@
 import React, { Component, PropTypes } from 'react';
+import HOCAssigning from 'components/assigning/HOCAssigning';
 import { map } from 'react-immutable-proptypes';
+
+import './styles/handoff-header.scss';
 
 class HandoffHeader extends Component {
   constructor(props) {
     super(props);
     this.state = {};
+    this.handleClick = this.handleClick.bind(this);
   }
-  componentDidMount() {
+  handleClick(e) {
+    const { onChangeClick } = this.props;
+
+    onChangeClick(e);
   }
   renderFrom() {
+    const { from } = this.props;
 
+    return (
+      <div className="handoff-header__section handoff-header__section--left">
+        <div className="handoff-header__top">
+          <div className="handoff-header__title">{from.title}</div>
+          <div className="handoff-header__assignees">
+            <HOCAssigning assignees={from.assignees} />
+          </div>
+        </div>
+        <div className="handoff-header__subtitle">{from.subtitle}</div>
+      </div>
+    );
   }
   renderTo() {
+    const { to } = this.props;
 
+    if (!to) {
+      return undefined;
+    }
+
+    return (
+      <div className="handoff-header__section handoff-header__section--right">
+        <div className="handoff-header__top">
+          <div className="handoff-header__title">{to.title}</div>
+          <button className="handoff-header__button" onClick={this.handleClick}>change</button>
+          <div className="handoff-header__assignees">
+            <HOCAssigning assignees={to.assignees} />
+          </div>
+        </div>
+        <div className="handoff-header__subtitle">{to.subtitle}</div>
+      </div>
+    );
   }
   render() {
-    console.log(this.props);
     return (
       <div className="handoff-header">
         {this.renderFrom()}
@@ -27,10 +62,18 @@ class HandoffHeader extends Component {
 
 export default HandoffHeader;
 
-const { func } = PropTypes;
+const { shape, func, string, array } = PropTypes;
 
 HandoffHeader.propTypes = {
-  from: map,
-  to: map,
-  onChangeClick: func,
+  from: shape({
+    title: string,
+    subtitle: string,
+    assignees: array,
+  }),
+  to: shape({
+    title: string,
+    subtitle: string,
+    assignees: array,
+  }),
+  onChangeClick: func.isRequired,
 };
