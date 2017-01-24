25fe2aabc726e0c850c99efc347f740acecf8765
iremlopsum <kristjanfou@gmail.com>
2017-01-23 18:03:44 -0800

    font mixin(scss), strip-units func(scss) and step list styles

---
+++ webapp/src/react/components/step-list/StepList.js
@@ -0,0 +1,47 @@
+import React, { Component, PropTypes } from 'react';
+import { map } from 'react-immutable-proptypes';
+
+import './styles/step-list.scss';
+
+class StepList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  renderSteps() {
+    const { steps, completed } = this.props;
+
+    const renderSteps = steps.map((s, i) => {
+      let className = 'step-list__item';
+
+      if (i <= completed) {
+        className += ' step-list__item--completed';
+      } else if (i === completed + 1) {
+        className += ' step-list__item--current';
+      } else {
+        className += ' step-list__item--future';
+      }
+      return (
+        <div className={className}>{step.get('title')}</div>
+      );
+    });
+
+    return renderSteps;
+  }
+  render() {
+    return (
+      <div className="step-list">
+        {this.renderSteps()}
+      </div>
+    );
+  }
+}
+
+export default StepList;
+
+const { number } = PropTypes;
+
+StepList.propTypes = {
+  steps: map,
+  completed: number,
+};
