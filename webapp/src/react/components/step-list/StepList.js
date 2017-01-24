36f886767bd8f241664c86785fed97e9f6510e2d
iremlopsum <kristjanfou@gmail.com>
2017-01-23 18:08:35 -0800

    made steplist to be a stateless component

--- webapp/src/react/components/step-list/StepList.js
+++ webapp/src/react/components/step-list/StepList.js
@@ -1,41 +1,33 @@
-import React, { Component, PropTypes } from 'react';
+import React, { PropTypes } from 'react';
 import { map } from 'react-immutable-proptypes';
 
 import './styles/step-list.scss';
 
-class StepList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-  renderSteps() {
-    const { steps, completed } = this.props;
+const StepList = (props) => {
+  const { steps, completed } = props;
 
-    const renderSteps = steps.map((s, i) => {
-      let className = 'step-list__item';
+  const renderSteps = steps.map((s, i) => {
+    let className = 'step-list__item';
 
-      if (i <= completed) {
-        className += ' step-list__item--completed';
-      } else if (i === completed + 1) {
-        className += ' step-list__item--current';
-      } else {
-        className += ' step-list__item--future';
-      }
-      return (
-        <div className={className}>{s.get('title')}</div>
-      );
-    });
+    if (i <= completed) {
+      className += ' step-list__item--completed';
+    } else if (i === completed + 1) {
+      className += ' step-list__item--current';
+    } else {
+      className += ' step-list__item--future';
+    }
 
-    return renderSteps;
-  }
-  render() {
     return (
-      <div className="step-list">
-        {this.renderSteps()}
-      </div>
+      <div className={className}>{s.get('title')}</div>
     );
-  }
-}
+  });
+
+  return (
+    <div className="step-list">
+      {renderSteps}
+    </div>
+  );
+};
 
 export default StepList;
 
