f79a35d2d9315be5e7936ebc5fbd9f827a353bc6
iremlopsum <kristjanfou@gmail.com>
2017-01-25 13:15:04 -0800

    handoff meassage

--- webapp/src/react/views/goals/goal-step/HandoffMessage.js
+++ webapp/src/react/views/goals/goal-step/HandoffMessage.js
@@ -1,5 +1,8 @@
 import React, { Component, PropTypes } from 'react';
+import Icon from 'Icon';
 import { map } from 'react-immutable-proptypes';
+
+import './styles/handoff-message.scss';
 
 class HandoffMessage extends Component {
   constructor(props) {
@@ -8,21 +11,52 @@ class HandoffMessage extends Component {
   }
   componentDidMount() {
   }
+  renderAvatar(src, name) {
+    const firstLetter = name.charAt(0);
 
+    if (src) {
+      return (
+        <div className="handoff-message__image">
+          <img src={src} alt="" />
+        </div>
+      );
+    }
+
+    return (
+      <div className="handoff-message__initials">{firstLetter}</div>
+    );
+  }
+  renderOpenInSlack() {
+
+  }
   render() {
     const {
       user,
       message,
       at,
     } = this.props;
+
     let name;
     let src;
+
     if (user) {
       name = user.get('name').split(' ')[0];
       src = user.get('profile_pic');
     }
+
     return (
-      <div className="handoff-message" />
+      <div className="handoff-message">
+        <div className="handoff-message__header">
+          {this.renderAvatar(src, name)}
+          <div className="handoff-message__name">
+            {name}
+            <Icon svg="SlackLogo" className="handoff-message__slack-icon" />
+          </div>
+          <div className="handoff-message__time">Jan 12</div>
+        </div>
+
+        <div className="handoff-message__message">{message}</div>
+      </div>
     );
   }
 }
