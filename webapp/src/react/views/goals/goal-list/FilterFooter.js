d675834080097acaa5ca62fbef7db5ede14ecb49
iremlopsum <kristjanfou@gmail.com>
2017-02-02 14:32:03 -0800

    input menu stules and filter footer api

--- webapp/src/react/views/goals/goal-list/FilterFooter.js
+++ webapp/src/react/views/goals/goal-list/FilterFooter.js
@@ -1,28 +1,40 @@
 import React, { Component, PropTypes } from 'react';
-
+import { bindAll } from 'classes/utils';
 import './styles/filter-footer.scss';
 
 class FilterFooter extends Component {
   constructor(props) {
     super(props);
     this.state = {};
+    bindAll(this, ['editFilter', 'toggleCompleted']);
+  }
+  editFilter() {
+
+  }
+  toggleCompleted() {
+
   }
   render() {
-    const showCompleted = false;
+    const { status, showCompleted, disableEdit } = this.props;
     let toggleClass = 'filter-footer__button filter-footer__button--toggle';
+    let editClass = 'filter-footer__button filter-footer__button--edit';
 
     if (showCompleted) {
       toggleClass += ' filter-footer__button--active';
     }
 
+    if (disableEdit) {
+      editClass += ' filter-footer__button--hide';
+    }
+
     return (
       <div className="filter-footer">
         <div className="filter-footer__section">
-          <div className="filter-footer__status">Showing 43 goals related to milestone The Connected Workspace</div>
-          <button className="filter-footer__button filter-footer__button--edit">Edit filter</button>
+          <div className="filter-footer__status">{status}</div>
+          <button className={editClass} onClick={this.editFilter}>Edit filter</button>
         </div>
         <div className="filter-footer__section">
-          <button className={toggleClass} />
+          <button className={toggleClass} onClick={this.toggleCompleted} />
           <div className="filter-footer__status">Include completed goals</div>
         </div>
       </div>
@@ -31,3 +43,12 @@ class FilterFooter extends Component {
 }
 
 export default FilterFooter;
+
+const { string, object, bool } = PropTypes;
+
+FilterFooter.propTypes = {
+  status: string,
+  delegate: object,
+  showCompleted: bool,
+  disableEdit: bool,
+};
