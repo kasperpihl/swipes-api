4efe771b65ba63b5fbe06d71ffb5bf05211d562e
iremlopsum <kristjanfou@gmail.com>
2017-02-17 17:16:04 -0800

    default row

--- webapp/src/react/views/preview/rows/default/DefaultRow.js
+++ webapp/src/react/views/preview/rows/default/DefaultRow.js
@@ -1,4 +1,5 @@
-import React, { PureComponent } from 'react';
+import React, { PureComponent, PropTypes } from 'react';
+import Icon from 'Icon';
 // import { map, list } from 'react-immutable-proptypes';
 
 class DefaultRow extends PureComponent {
@@ -8,15 +9,61 @@ class DefaultRow extends PureComponent {
   }
   componentDidMount() {
   }
+  renderIcon(icon) {
+    if (icon.src) {
+      return (
+        <div className="default-row__icon default-row__icon--image">
+          <img src={icon.src} alt="presentation" />
+        </div>
+      );
+    } else if (icon.icon) {
+      return (
+        <div className="default-row__icon default-row__icon--svg">
+          <Icon svg={icon.icon} className="default-row__svf" />
+        </div>
+      );
+    } else if (icon.color) {
+      const styles = {
+        backgroundColor: icon.color,
+      };
+
+      return (
+        <div className="default-row__icon default-row__icon--circle" style={styles} />
+      );
+    } else if (icon.initials) {
+      return (
+        <div className="default-row__icon default-row__icon--initials">{icon.initials}</div>
+      );
+    }
+
+    return undefined;
+  }
   render() {
+    const { leftIcon, title, onClick, secondary } = this.props;
+    let className = 'default-row';
+
+    if (secondary) {
+      className += ' default-row--secondary';
+    }
+
     return (
-      <div className="className" />
+      <div className={className} onClick={onClick}>
+        {this.renderIcon(leftIcon)}
+        <div className="default-row__title">
+          {title}
+        </div>
+      </div>
     );
   }
 }
 
 export default DefaultRow;
 
-// const { string } = PropTypes;
+const { string, object, func, bool } = PropTypes;
 
-DefaultRow.propTypes = {};
+DefaultRow.propTypes = {
+  leftIcon: object,
+  title: string,
+  onClick: func,
+  secondary: bool,
+};
