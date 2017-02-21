47eed3142cd49672fab2eba79b47309d89832dfe
iremlopsum <kristjanfou@gmail.com>
2017-02-20 16:33:35 -0800

    notification item

--- webapp/src/react/views/dashboard/NotificationWrapper.js
+++ webapp/src/react/views/dashboard/NotificationWrapper.js
@@ -1,52 +1,108 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
+import { mapContains, listOf } from 'react-immutable-proptypes';
 import Icon from 'Icon';
-import './styles/notification-wrapper';
 
-const NotificationWrapper = (props) => {
-  const {
-    message,
-    svg,
-    iconBgColor,
-    onClick,
-    timeago,
-    unread,
-  } = props;
-  const style = {};
-  let className = 'notification';
+import './styles/notification-item';
 
-  if (iconBgColor) {
-    style.fill = iconBgColor;
-    style.stroke = iconBgColor;
+class NotificationItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
   }
+  renderIcon() {
+    const { notification: n } = this.props;
 
-  if (unread) {
-    className += ' notification--unread';
+    if (!n.get('icon')) {
+      return undefined;
+    }
+
+    return (
+      <div className="notification__icon">
+        <Icon svg={n.get('icon')} className="notification__svg" />
+      </div>
+    );
   }
+  renderMessage() {
+    const { notification: n } = this.props;
 
-  return (
-    <div className={className} onClick={onClick}>
-      <div className="notification__icon" >
-        <Icon svg={svg} style={style} className="notification__svg" />
+    if (!n.get('message')) {
+      return undefined;
+    }
+
+    return (
+      <div className="notification__message">{n.get('message')}</div>
+    );
+  }
+  renderAttachments() {
+    const { notification: n } = this.props;
+    const attachments = n.get('attachments');
+
+    if (!attachments) {
+      return undefined;
+    }
+
+    const HTMLAttachments = attachments((a, i) => (
+      <div className="attachment">
+        <div className="attachment__icon">
+          <Icon svg="Flag" className="attachment__svg" />
+        </div>
+        <div className="attachment__title">{a.get('title')}</div>
       </div>
-      <div className="notification__message">
-        {message}
+      ));
+
+    return (
+      <div className="notification__attachments">
+        {HTMLAttachments}
       </div>
-      <div className="notification__timestamp">
-        {timeago}
+    );
+  }
+  renderContent() {
+    const { notification: n } = this.props;
+
+    return (
+      <div className="notification__content">
+        <div className="notification__subtitle">{n.get('subtitle')}</div>
+        <div className="notification__title">{n.get('title')}</div>
+        {this.renderMessage()}
+        {this.renderAttachments()}
       </div>
-    </div>
-  );
-};
+    );
+  }
+  renderTimestamp() {
+    const { notification: n } = this.props;
 
-export default NotificationWrapper;
+    if (!n.get('timestamp')) {
+      return undefined;
+    }
 
-const { string, object, func, bool } = PropTypes;
+    return <div className="notification__timestamp">{n.get('timestamp')}</div>;
+  }
+  render() {
+    return (
+      <div className="notification">
+        {this.renderIcon()}
+        {this.renderContent()}
+        {this.renderTimestamp()}
+      </div>
+    );
+  }
+}
 
-NotificationWrapper.propTypes = {
-  iconBgColor: string,
-  message: object,
-  unread: bool,
-  timeago: string,
-  svg: string,
-  onClick: func,
+export default NotificationItem;
+
+const { string, object } = PropTypes;
+
+NotificationItem.propTypes = {
+  delegate: object,
+  notification: mapContains({
+    icon: string,
+    subtitle: string,
+    title: string,
+    message: string,
+    attachments: listOf({
+      icon: string,
+      title: string,
+    }),
+    timestamp: string,
+  }),
 };
