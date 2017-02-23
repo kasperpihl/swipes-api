acc9f4c4855b53ef7bc1b78f9f14c78f88cc82da
Kasper Pihl Tornøe <kasper@pihl.it>
2017-02-22 16:36:01 -0800

    Fixed error on server

--- server/middlewares/swipes-error/index.js
+++ server/middlewares/swipes-error/index.js
@@ -2,6 +2,11 @@ import SwipesError from './swipes-error';
 
 const swipesErrorMiddleware = (err, req, res, next) => {
   if (typeof err === 'object' && err.name === 'SwipesError') {
+    if (typeof err.extra === 'string') {
+      err.extra = {
+        message: err.extra,
+      };
+    }
     return res.status(200).json({
       ok: false,
       error: err.message,
