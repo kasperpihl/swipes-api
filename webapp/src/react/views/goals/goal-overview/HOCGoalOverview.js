c17568cf16d9cb618fcbdab1b6a109f28bc4724a
Kasper Pihl Tornøe <kasper@pihl.it>
2017-02-24 14:10:35 -0800

    Starting history

--- webapp/src/react/views/goals/goal-overview/HOCGoalOverview.js
+++ webapp/src/react/views/goals/goal-overview/HOCGoalOverview.js
@@ -11,14 +11,16 @@ import SWView from 'SWView';
 import Button from 'Button';
 import HOCAttachments from 'components/attachments/HOCAttachments';
 import HOCHeaderTitle from 'components/header-title/HOCHeaderTitle';
-import HOCTeam from './HOCTeam';
-import HOCLastUpdate from './HOCLastUpdate';
+import HOCHistory from './HOCHistory';
 import GoalSide from '../goal-step/GoalSide';
 import './styles/goal-overview.scss';
 
 class HOCGoalOverview extends PureComponent {
   static minWidth() {
     return 650;
+  }
+  static maxWidth() {
+    return 900;
   }
   constructor(props) {
     super(props);
@@ -79,17 +81,16 @@ class HOCGoalOverview extends PureComponent {
       props: {
         items: [
           {
-            title: 'Handoff',
-            onClick: () => {
-              this.onHandoff();
-              contextMenu(null);
-            },
-          },
-          {
             title: 'Save as a Way',
             onClick: () => {
               const helper = this.getHelper();
               saveWay(options, helper.getObjectForWay());
+            },
+          },
+          {
+            title: 'Complete Goal',
+            onClick: () => {
+
             },
           },
           {
@@ -126,19 +127,16 @@ class HOCGoalOverview extends PureComponent {
   }
   renderLeft() {
     const { goal } = this.props;
-    const helper = this.getHelper();
-    const handoff = helper.getLastHandoff();
+
     return (
       <div className="goal-overview__column goal-overview__column--left">
         <Section title="Activity" />
-        <HOCLastUpdate handoff={handoff} />
+        <HOCHistory goal={goal} />
       </div>
     );
   }
   renderRight() {
     const { goal } = this.props;
-    const helper = this.getHelper();
-    const handoff = helper.getLastHandoff();
     return (
       <div className="goal-overview__column goal-overview__column--right">
         <GoalSide goal={goal} />
@@ -147,30 +145,8 @@ class HOCGoalOverview extends PureComponent {
           attachments={goal.get('attachments')}
           attachmentOrder={goal.get('attachment_order')}
           goalId={goal.get('id')}
-          flags={handoff.get('flags')}
           delegate={this}
         />
-      </div>
-    );
-  }
-  renderHandoffBar() {
-    const helper = this.getHelper();
-
-    if (!helper.amIAssigned()) {
-      return undefined;
-    }
-
-    return (
-      <div className="handoff-bar">
-        <div className="handoff-bar__label">It’s your turn to handoff work</div>
-        <div className="handoff-bar__actions">
-          <Button
-            text="Move forward"
-            primary
-            className="handoff-bar__button"
-            onClick={this.onHandoff}
-          />
-        </div>
       </div>
     );
   }
@@ -181,7 +157,7 @@ class HOCGoalOverview extends PureComponent {
     }
 
     return (
-      <SWView header={this.renderHeader()} footer={this.renderHandoffBar()}>
+      <SWView header={this.renderHeader()}>
         <div className="goal-overview">
           {this.renderLeft()}
           {this.renderRight()}
---
+++ webapp/src/react/views/goals/goal-overview/HOCHistory.js
@@ -0,0 +1,108 @@
+import React, { PureComponent } from 'react';
+import { connect } from 'react-redux';
+import moment from 'moment';
+// import * as a from 'actions';
+import { map } from 'react-immutable-proptypes';
+import { fromJS, Map } from 'immutable';
+import NotificationWrapper from '../../dashboard/NotificationWrapper';
+/* global msgGen */
+
+class HOCHistory extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  componentDidMount() {
+  }
+  getAttachments(flags) {
+    const { goal } = this.props;
+    if (!flags || !goal.get('attachments')) {
+      return undefined;
+    }
+    const at = flags.map(fId => (goal.getIn(['attachments', fId, 'title']))).filter(v => !!v);
+    return fromJS(at);
+  }
+  getStepTitle(stepId) {
+    const { goal } = this.props;
+    return goal.getIn(['steps', stepId, 'title']);
+  }
+  getNotificationForEvent(e) {
+    const { me } = this.props;
+    const type = e.get('type');
+    console.log(e.toJS());
+    let m = Map({
+      timeago: moment(e.get('done_at')).fromNow(),
+      seen: true,
+      message: e.get('message'),
+      attachments: this.getAttachments(e.get('flags')),
+    });
+    const stepTitle = this.getStepTitle(e.get('to'));
+    const from = msgGen.getUserString(e.get('done_by'));
+    const to = e.get('done_by') === me.get('id') ? 'yourself' : 'you';
+
+    switch (type) {
+      case 'created': {
+        m = m.set('subtitle', `${from} created this goal`);
+        m = m.set('icon', 'Plus');
+        break;
+      }
+      case 'notified': {
+        m = m.set('subtitle', `${from} notified ${to} in `);
+        m = m.set('icon', 'GotNotified');
+        break;
+      }
+      case 'complete_step': {
+        m = m.set('subtitle', `${from} handed this off to ${to}`);
+
+        m = m.set('icon', 'GotAssigned');
+        break;
+      }
+      case 'complete_goal': {
+        m = m.set('subtitle', `${from} completed this goal`);
+        m = m.set('icon', 'Star');
+        break;
+      }
+    }
+    return m;
+  }
+  renderEvent(e, i) {
+    const { me } = this.props;
+    if (e.get('type') === 'notified') {
+      if (e.get('assignees').indexOf(me.get('id')) === -1) {
+        return undefined;
+      }
+    }
+    return (
+      <NotificationWrapper
+        key={i}
+        i={i}
+        delegate={this}
+        notification={this.getNotificationForEvent(e)}
+      />
+    );
+  }
+  render() {
+    const { goal } = this.props;
+    const history = goal.get('history');
+    return (
+      <div className="history">
+        {history.map((e, i) => this.renderEvent(e, i)).reverse()}
+      </div>
+    );
+  }
+}
+// const { string } = PropTypes;
+
+HOCHistory.propTypes = {
+  goal: map,
+  me: map,
+};
+
+function mapStateToProps(state) {
+  return {
+    me: state.get('me'),
+  };
+}
+
+export default connect(mapStateToProps, {
+})(HOCHistory);
