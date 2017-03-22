572fb4d9ff6bc9563c972037a4bbe22cbea4bddb
Kasper Pihl Tornøe <kasper@pihl.it>
2017-03-21 19:12:19 -0700

    No need for relatives in npm script. Tisho shouldnt this middleware return swipeserror

--- server/middlewares/jwt-auth-middleware.js
+++ server/middlewares/jwt-auth-middleware.js
@@ -12,7 +12,7 @@ const authParseToken = (req, res, next) => {
     const parsedToken = parseToken(token);
 
     if (!parsedToken) {
-      return res.status(200).json({ ok: false, err: 'not_authed' });
+      return res.status(200).json({ ok: false, error: 'not_authed' });
     }
 
     res.locals.user_id = parsedToken.content.iss;
@@ -22,7 +22,7 @@ const authParseToken = (req, res, next) => {
     return next();
   }
 
-  return res.status(200).json({ ok: false, err: 'not_authed' });
+  return res.status(200).json({ ok: false, error: 'not_authed' });
 };
 const authCheckToken = (req, res, next) => {
   const {
@@ -33,13 +33,13 @@ const authCheckToken = (req, res, next) => {
   dbCheckToken({ user_id, token: dbToken })
     .then((results) => {
       if (results.length === 0) {
-        return res.status(200).json({ ok: false, err: 'not_authed' });
+        return res.status(200).json({ ok: false, error: 'not_authed' });
       }
 
       return next();
     })
     .catch(() => {
-      return res.status(200).json({ ok: false, err: 'not_authed' });
+      return res.status(200).json({ ok: false, error: 'not_authed' });
     });
 };
 
