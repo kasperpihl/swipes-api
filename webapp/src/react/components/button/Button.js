f0fbe689880d9770ea1bc01c7203bf7a8067bdd7
iremlopsum <kristjanfou@gmail.com>
2017-03-31 14:22:08 -0700

    button selected state

--- webapp/src/react/components/button/Button.js
+++ webapp/src/react/components/button/Button.js
@@ -113,6 +113,7 @@ class Button extends Component {
       small,
       alignIcon,
       frameless,
+      selected,
       tabIndex: tabIndexProps,
       loading: lol,
       className: classNameFromButton,
@@ -164,6 +165,10 @@ class Button extends Component {
 
     if (successState) {
       className += ' g-button--result g-button--success';
+    }
+
+    if (selected) {
+      className += ' g-button--selected';
     }
 
     if (classNameFromButton && typeof classNameFromButton === 'string') {
