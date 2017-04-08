634065916b9ed79c2611b445048386c69db1b2e9
iremlopsum <kristjanfou@gmail.com>
2017-04-08 16:20:06 -0700

    onboarding

--- webapp/src/react/views/onboarding/Onboarding.js
+++ webapp/src/react/views/onboarding/Onboarding.js
@@ -1,26 +1,70 @@
-import React, { PureComponent } from 'react'
+import React, { PureComponent } from 'react';
 // import { map, list } from 'react-immutable-proptypes';
+import SWView from 'SWView';
+import Icon from 'Icon';
+import HOCHeaderTitle from 'components/header-title/HOCHeaderTitle';
+import './styles/onboarding.scss';
 
 class Onboarding extends PureComponent {
   constructor(props) {
-    super(props)
-    this.state = {}
+    super(props);
+    this.state = {};
   }
   componentDidMount() {
   }
-  renderItem(item) {
-    const { title, subtitle, completed } = item;
+  renderProgressBar() {
+    return undefined;
+  }
+  renderHeader() {
+    return (
+      <HOCHeaderTitle title="Getting started" subtitle="Welcome to your workspace. There are couple of things to learn before you start." >
+        {this.renderProgressBar()}
+      </HOCHeaderTitle>
+    );
+  }
+  renderItems() {
+    const { items } = this.props;
+
+    const itemsHTML = items.map((item, i) => {
+      let className = 'onboarding__item';
+
+      if (item.completed) {
+        className += ' onboarding__item--completed';
+      }
+
+      return (
+        <div className={className} key={`onboarding-${i}`}>
+          <div className="onboarding__indicator">
+            <Icon icon="Checkmark" className="onboarding__svg" />
+          </div>
+          <div className="onboarding__content">
+            <div className="onboarding__title">{item.title}</div>
+            <div className="onboarding__subtitle">{item.subtitle}</div>
+          </div>
+          <div className="onboarding__button">
+            <Icon icon="ArrowRightLine" className="onboarding__svg" />
+          </div>
+        </div>
+      );
+    });
+
+    return (
+      <div className="onboarding">
+        {itemsHTML}
+      </div>
+    );
   }
   render() {
     const { items } = this.props;
     return (
-      <div className="onboarding">
-      </div>
-    )
+      <SWView header={this.renderHeader()}>
+        {this.renderItems()}
+      </SWView>
+    );
   }
 }
 
-export default Onboarding
+export default Onboarding;
 
 // const { string } = PropTypes;
 
