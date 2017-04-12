fda1e43b160492208e01df10f49028c9c1ea6063
Kasper Pihl Tornøe <kasper@pihl.it>
2017-04-11 17:49:15 -0700

    handling user_invited event

--- mobile/swipes-core-js/reducers/users.js
+++ mobile/swipes-core-js/reducers/users.js
@@ -12,6 +12,7 @@ export default function usersReducer(state = initialState, action) {
       });
       return users;
     }
+    case 'user_invited':
     case 'users.invite':{
       return state.set(payload.user.id, fromJS(payload.user));
     }
