e749217f30f7effd6f6737d199875e49c607153c
iremlopsum <kristjanfou@gmail.com>
2017-04-19 15:14:30 -0700

    styles for selector

--- webapp/src/react/context-menus/goal-selector/HOCGoalSelector.js
+++ webapp/src/react/context-menus/goal-selector/HOCGoalSelector.js
@@ -8,10 +8,10 @@ import GoalsUtil from 'swipes-core-js/classes/goals-util';
 // import { map, list } from 'react-immutable-proptypes';
 import { fromJS } from 'immutable';
 import { Creatable } from 'react-select';
-import 'react-select/dist/react-select.css';
 
 import GoalRow from './GoalRow';
 import './styles/goal-selector.scss';
+import './styles/react-select/default.scss';
 
 class HOCGoalSelector extends PureComponent {
   constructor(props) {
@@ -23,31 +23,28 @@ class HOCGoalSelector extends PureComponent {
   render() {
     const { milestoneId, goals } = this.props;
     const props = {
-      options: goals.sort((g1, g2) => {
-        return g1.get('title').localeCompare(g2.get('title'));
-      }).map((g) => {
-        const helper = new GoalsUtil(g)
+      options: goals.sort((g1, g2) => g1.get('title').localeCompare(g2.get('title'))).map((g) => {
+        const helper = new GoalsUtil(g);
         return {
           ...this.getLoading(g.get('id')),
           title: g.get('title'),
           completed: !!helper.getIsCompleted(),
           hasThisMilestone: (g.get('milestone_id') === milestoneId),
         };
-
       }).toArray(),
       ref: function setValue(selectInstance) {
         const originalSetValue = selectInstance.setValue;
         console.log('here1!');
-        selectInstance.setValue = function() {
+        selectInstance.setValue = function () {
           originalSetValue.apply(this, arguments);
           console.log('here!');
           selectInstance.setState({
             isOpen: true,
           });
-        }
+        };
       },
       optionRenderer: GoalRow,
-    }
+    };
     return (
       <div className="goal-selector">
         <Creatable
@@ -63,7 +60,7 @@ HOCGoalSelector.propTypes = {};
 
 function mapStateToProps(state) {
   return {
-    goals: state.get('goals')
+    goals: state.get('goals'),
   };
 }
 
