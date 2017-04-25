06533cf595fdd8b98f58656e4504234376530c1f
Kasper Pihl Tornøe <kasper@pihl.it>
2017-04-24 21:41:03 -0700

    Counter stuff

--- webapp/src/react/pages/counter/HOCCounter.js
+++ webapp/src/react/pages/counter/HOCCounter.js
@@ -13,7 +13,12 @@ class HOCCounter extends PureComponent {
     super(props);
     this.state = {
       shoot: false,
+      counter: 0,
+      prevCelebrate: 0,
     };
+  }
+  componentWillMount() {
+    this.fetchAndSet();
   }
   componentDidMount() {
     this.hitWin = new Audio('https://s3-us-west-2.amazonaws.com/staging.swipesapp.com/uploads/ONY8E94FL/1493094279-UZTYMBVGO/hitwin.mp3');
@@ -28,6 +33,28 @@ class HOCCounter extends PureComponent {
     }, 10000);
 
     console.log('yo');
+  }
+  componentDidUpdate(prevProps, prevState) {
+
+  }
+  componentWillUnmount() {
+    this._unmounted = true;
+  }
+  fetchAndSet() {
+    const headers = new Headers({
+      'Content-Type': 'application/json',
+      method: 'GET',
+    });
+    fetch('http://localhost:1337', { headers }).then((r) => {
+      if (r && r.ok) return r.json();
+      return Promise.reject({ message: r.statusText, code: r.status });
+    }).then((res) => {
+      if(res.result !== this.state.counter && !this._unmounted) {
+        this.setState({ counter: res.result});
+      }
+    }).catch((e) => {
+      console.log('err', e);
+    })
   }
   render() {
     const leftConfig = {
@@ -45,10 +72,10 @@ class HOCCounter extends PureComponent {
       elementCount: 300,
       decay: 0.95,
     };
-
+    const { counter } = this.state;
     return (
       <div className="counter">
-        <div className="counter__count">101</div>
+        <div className="counter__count">{counter}</div>
         <div className="counter__confetti counter__confetti--left">
           <Confetti active={this.state.shoot} config={leftConfig} />
         </div>
