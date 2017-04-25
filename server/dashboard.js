88343d2e73c8d107816a2ce596d5100eeb021f23
Kasper Pihl Tornøe <kasper@pihl.it>
2017-04-24 21:24:13 -0700

    Added little dashboard server for counter

---
+++ server/dashboard.js
@@ -0,0 +1,35 @@
+import http from 'http';
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+const app = express();
+import r from 'rethinkdb';
+import db from './db';
+
+app.use(cors({
+  origin: '*',
+  methods: 'HEAD, GET, POST',
+  allowedHeader: 'Content-Type, Range, Accept, X-Requested-With, Session, Content-Length, X-Requested-With',
+  exposedHeaders: 'Accept-Ranges, Content-Encoding, Content-Length, Content-Range',
+}));
+
+const server = http.createServer(app);
+app.all('/', (req, res, next) => {
+  const q =
+    r.db('swipes')
+      .table('users')
+      .count();
+
+  db.rethinkQuery(q).then((result) => {
+    res.json({
+      ok: true,
+      result,
+    });
+  });
+
+})
+
+// start api rest server
+server.listen(1337);
+
+console.log('server started on port %s', 1337);
---
+++ server/webpack.dashboard.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const fs = require('fs');
+
+const nodeModules = {};
+fs.readdirSync('node_modules')
+  .filter((x) => {
+    return ['.bin'].indexOf(x) === -1;
+  })
+  .forEach((mod) => {
+    nodeModules[mod] = `commonjs ${mod}`;
+  });
+
+module.exports = {
+  context: __dirname,
+  devtool: 'eval',
+  entry: './dashboard',
+  target: 'node',
+  externals: nodeModules,
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'dashboard.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.json$/,
+        loader: 'json',
+      },
+      {
+        test: /\.js$/,
+        loaders: ['babel-loader', 'eslint-loader'],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+};
