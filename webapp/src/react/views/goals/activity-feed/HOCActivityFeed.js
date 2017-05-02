092646837bf072cca68271875bc91050c9cccde2
Kasper Pihl Tornøe <kasper@pihl.it>
2017-05-02 10:01:28 -0700

    Fixed attachment click activity

--- webapp/src/react/views/goals/activity-feed/HOCActivityFeed.js
+++ webapp/src/react/views/goals/activity-feed/HOCActivityFeed.js
@@ -29,8 +29,8 @@ class HOCActivityFeed extends PureComponent {
     if (att && selection.toString().length === 0) {
       window.analytics.sendEvent('Flag opened', {
         From: 'Activity Feed',
-        Type: att.getIn([id, 'link', 'service', 'type']),
-        Service: att.getIn([id, 'link', 'service', 'name']),
+        Type: att.getIn(['link', 'service', 'type']),
+        Service: att.getIn(['link', 'service', 'name']),
       });
       preview(this.context.target, att);
     }
