b9ff8336b6dba5e64d8f0c072ae9140e7b571192
Kasper Pihl Tornøe <kasper@pihl.it>
2017-05-22 15:49:26 -0700

    Started note structure

---
+++ mobile/src/views/preview-note/HOCPreviewNote.js
@@ -0,0 +1,31 @@
+import React, { PureComponent } from 'react';
+// import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+// import * as a from 'actions';
+// import * as ca from 'swipes-core-js/actions';
+// import { setupLoading } from 'swipes-core-js/classes/utils';
+// import { map, list } from 'react-immutable-proptypes';
+// import { fromJS } from 'immutable';
+
+class HOCPreviewNote extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  componentDidMount() {
+  }
+  render() {
+    return (
+    );
+  }
+}
+// const { string } = PropTypes;
+
+HOCPreviewNote.propTypes = {};
+
+function mapStateToProps() {
+  return {};
+}
+
+export default connect(mapStateToProps, {
+})(HOCPreviewNote);
