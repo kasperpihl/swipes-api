ab4a3ec4313c4dd2a5837f0ad4e1ff1b1ede6d59
iremlopsum <kristjanfou@gmail.com>
2017-05-22 16:33:47 -0700

    preview note

--- mobile/src/views/preview-note/HOCPreviewNote.js
+++ mobile/src/views/preview-note/HOCPreviewNote.js
@@ -1,6 +1,11 @@
 import React, { PureComponent } from 'react';
-import { View, WebView, StyleSheet } from 'react-native';
+import { View, WebView, StyleSheet, Platform } from 'react-native';
 import { connect } from 'react-redux';
+import Browser from 'react-native-browser';
+import {
+  CustomTabs,
+  ANIMATIONS_SLIDE
+} from 'react-native-custom-tabs';
 // import * as a from 'actions';
 // import * as ca from 'swipes-core-js/actions';
 // import { setupLoading } from 'swipes-core-js/classes/utils';
@@ -27,8 +32,36 @@ class HOCPreviewNote extends PureComponent {
   }
   componentDidMount() {
   }
-  onWebviewMessage(data) {
-    console.log(data);
+  onWebviewMessage(e) {
+    const data = JSON.parse(e.nativeEvent.data);
+    if (data.action === 'url') {
+      if (Platform.OS === 'android') {
+        CustomTabs.openURL(data.value, {
+          toolbarColor: '#ffffff',
+          enableUrlBarHiding: true,
+          showPageTitle: true,
+          enableDefaultShare: true,
+          animations: ANIMATIONS_SLIDE
+        }).then((launched: boolean) => {
+          console.log(`Launched custom tabs: ${launched}`);
+        }).catch(err => {
+          console.error(err)
+        });
+      } else {
+        Browser.open(data.value, {
+          showUrlWhileLoading: true,
+          // loadingBarTintColor: processColor('#d64bbd'),
+          navigationButtonsHidden: false,
+          showActionButton: true,
+          showDoneButton: true,
+          doneButtonTitle: 'Done',
+          showPageTitles: true,
+          disableContextualPopupMenu: false,
+          hideWebViewBoundaries: false,
+          // buttonTintColor: processColor('#d64bbd')
+        });
+      }
+    }
   }
   generateNoteUrl() {
     const { token, orgId, noteId } = this.props;
