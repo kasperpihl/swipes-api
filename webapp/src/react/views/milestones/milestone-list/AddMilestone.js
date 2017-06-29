4998c2c9855f9bcb09a18491a3d912b453525760
Kasper Pihl Tornøe <kasper@pihl.it>
2017-06-28 17:02:54 -0700

    Add milestone for kris

---
+++ webapp/src/react/views/milestones/milestone-list/AddMilestone.js
@@ -0,0 +1,42 @@
+import React, { PureComponent } from 'react'
+// import PropTypes from 'prop-types';
+// import { map, list } from 'react-immutable-proptypes';
+import { bindAll, setupDelegate, setupCachedCallback } from 'swipes-core-js/classes/utils';
+// import SWView from 'SWView';
+// import Button from 'Button';
+// import Icon from 'Icon';
+// import './styles/add-milestone.scss';
+
+class AddMilestone extends PureComponent {
+  constructor(props) {
+    super(props)
+    this.state = {
+      title: '',
+    };
+    setupDelegate(this);
+    this.callDelegate.bindAll('onAddMilestone');
+  }
+  componentDidMount() {
+  }
+  onChange(e) {
+    this.setState({ title: e.target.value });
+  }
+  onKeyDown(e) {
+    if(e.keyCode === 13) {
+      this.onAddMilestone(this.state.title);
+    }
+  }
+  render() {
+    const { isLoading, getLoading } = this.props;
+    return (
+      <div className="add-milestone">
+      </div>
+    )
+  }
+}
+
+export default AddMilestone
+
+// const { string } = PropTypes;
+
+AddMilestone.propTypes = {};
