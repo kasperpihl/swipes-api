7a551386e745d25dd3c5ffa4117cc38ddd008070
iremlopsum <kristjanfou@gmail.com>
2017-07-09 09:55:58 +0300

    renamed android apps package

---
+++ mobile/android/app/src/main/java/com/swipesapp/MainActivity.java
@@ -0,0 +1,18 @@
+package com.swipesapp;
+
+import com.facebook.react.ReactActivity;
+import com.github.droibit.android.reactnative.customtabs.CustomTabsPackage;
+import com.BV.LinearGradient.LinearGradientPackage;
+import com.horcrux.svg.SvgPackage;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "swipes";
+    }
+}
---
+++ mobile/android/app/src/main/java/com/swipesapp/MainApplication.java
@@ -0,0 +1,71 @@
+package com.swipesapp;
+
+import android.app.Application;
+import android.content.Context;
+
+import com.facebook.react.ReactApplication;
+import com.fileopener.FileOpenerPackage;
+import com.imagepicker.ImagePickerPackage;
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
+import com.kevinejohn.RNMixpanel.RNMixpanel;
+import com.robinpowered.react.Intercom.IntercomPackage;
+import io.intercom.android.sdk.Intercom;
+import com.reactlibrary.RNReactNativeDocViewerPackage;
+import com.microsoft.codepush.react.CodePush;
+import com.geektime.rnonesignalandroid.ReactNativeOneSignalPackage;
+import com.horcrux.svg.SvgPackage;
+import com.BV.LinearGradient.LinearGradientPackage;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
+import com.github.droibit.android.reactnative.customtabs.CustomTabsPackage;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainApplication extends Application implements ReactApplication {
+
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+
+    @Override
+    protected String getJSBundleFile() {
+      return CodePush.getJSBundleFile();
+    }
+
+    @Override
+    public boolean getUseDeveloperSupport() {
+      return BuildConfig.DEBUG;
+    }
+
+    @Override
+    protected List<ReactPackage> getPackages() {
+      return Arrays.<ReactPackage>asList(
+          new MainReactPackage(),
+            new FileOpenerPackage(),
+            new ImagePickerPackage(),
+            new RNDeviceInfo(),
+            new RNMixpanel(),
+            new IntercomPackage(),
+            new CodePush(BuildConfig.CODEPUSH_KEY, MainApplication.this, BuildConfig.DEBUG),
+            new RNReactNativeDocViewerPackage(),
+            new ReactNativeOneSignalPackage(),
+            new SvgPackage(),
+            new LinearGradientPackage(),
+            new CustomTabsPackage()
+      );
+    }
+  };
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    Intercom.initialize(this, "android_sdk-a53db85501edc71346df31810c45626fd00506a2", "q8xibmac");
+    SoLoader.init(this, /* native exopackage */ false);
+  }
+}
