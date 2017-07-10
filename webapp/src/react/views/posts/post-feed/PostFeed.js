ce6198c608916ae0779455778fdd4f633b0d88a2
iremlopsum <kristjanfou@gmail.com>
2017-07-10 21:19:29 +0300

    post feed

---
+++ webapp/src/react/views/posts/post-feed/PostFeed.js
@@ -0,0 +1,41 @@
+import React, { PureComponent } from 'react'
+// import PropTypes from 'prop-types';
+// import { map, list } from 'react-immutable-proptypes';
+// import { bindAll, setupDelegate, setupCachedCallback } from 'swipes-core-js/classes/utils';
+import SWView from 'SWView';
+import HOCHeaderTitle from 'components/header-title/HOCHeaderTitle';
+import Button from 'Button';
+// import Icon from 'Icon';
+// import './styles/post-feed.scss';
+
+class PostFeed extends PureComponent {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+  componentDidMount() {
+  }
+  renderHeader() {
+
+    return (
+      <HOCHeaderTitle>
+
+      </HOCHeaderTitle>
+    )
+  }
+  render() {
+    return (
+      <SWView
+        header={this.renderHeader()}
+      >
+
+      </SWView>
+    )
+  }
+}
+
+export default PostFeed
+
+// const { string } = PropTypes;
+
+PostFeed.propTypes = {};
