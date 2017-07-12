b2e4940b97373b5a64c63e043de00d81bdb16895
Kasper Pihl Tornøe <kasper@pihl.it>
2017-07-12 19:11:29 +0300

    wire up some delegate for notifications

--- webapp/src/react/views/notifications/HOCNotifications.js
+++ webapp/src/react/views/notifications/HOCNotifications.js
@@ -3,7 +3,7 @@ import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
 // import * as a from 'actions';
 // import * as ca from 'swipes-core-js/actions';
-// import { setupLoading } from 'swipes-core-js/classes/utils';
+import { setupLoading } from 'swipes-core-js/classes/utils';
 // import { map, list } from 'react-immutable-proptypes';
 // import { fromJS } from 'immutable';
 import Notifications from './Notifications';
@@ -12,15 +12,36 @@ class HOCNotifications extends PureComponent {
   constructor(props) {
     super(props);
     this.state = {};
-    // setupLoading(this)
+    setupLoading(this);
   }
   componentDidMount() {
+  }
+  componentWillUnmount() {
+    this._unmounted = true;
+  }
+  onMarkAll() {
+    const { markNotifications, notifications } = this.props;
+    const nToMark = notifications.toArray().filter(n => !n.get('seen_at')).map(n => n.get('id'));
+
+    this.setLoading('marking');
+    markNotifications(nToMark).then(() => {
+      if (!this._unmounted) {
+        this.clearLoading('marking');
+      }
+    });
+  }
+  onNotificationOpen(id) {
+    
   }
   render() {
     const { notifications: n } = this.props;
 
     return (
-      <Notifications delegate={this} notifications={n} />
+      <Notifications
+        delegate={this}
+        notifications={n}
+        {...this.bindLoading()}
+      />
     );
   }
 }
