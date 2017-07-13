dbe2275c3bae4beac2292cd2516745e1f8626c8e
iremlopsum <kristjanfou@gmail.com>
2017-07-13 11:56:52 +0300

    fixed icon name and path to constants

--- mobile/swipes-core-js/reducers/posts.js
+++ mobile/swipes-core-js/reducers/posts.js
@@ -1,4 +1,4 @@
-import * as types from 'constants'
+import * as types from '../constants'
 import { fromJS } from 'immutable'
 import { reducerInitToMap } from '../classes/utils';
 const initialState = fromJS({});
