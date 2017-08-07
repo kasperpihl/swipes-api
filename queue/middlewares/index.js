5c442e5ab4995f8d6a991a5a4442c985409496dc
thinklinux <tihomir.valkanov@gmail.com>
2017-08-07 11:50:41 +0200

    dont send push if the user mentions itself

--- queue/middlewares/index.js
+++ queue/middlewares/index.js
@@ -310,10 +310,11 @@ const post_comment_mention_push_notification = [
   posts.postAddCommentMentionPushNotificationData,
   (req, res, next) => {
     const {
+      user_id,
       mention_ids,
     } = res.locals;
 
-    res.locals.user_ids = mention_ids;
+    res.locals.user_ids = mention_ids.filter(userId => userId !== user_id);
 
     return next();
   },
