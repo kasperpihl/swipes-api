3ce9b917b42ce3441858b9a71aaf3ebf866ceb79
Kasper Pihl Tornøe <kasper@pihl.it>
2017-08-11 16:51:31 +0300

    moved onboarding under account

--- webapp/src/react/views/index.js
+++ webapp/src/react/views/index.js
@@ -1,11 +1,16 @@
+// Account
 import AccountList from './account/account-list/HOCAccountList';
 import Billing from './account/billing/HOCBilling';
+import Organization from './account/organization/HOCOrganization';
+import Profile from './account/profile/HOCProfile';
+import Services from './account/services/Services';
+import Onboarding from './account/onboarding/HOCOnboarding';
+
 import Browser from './browser/HOCBrowser';
 import CreatePost from './posts/compose-post/HOCCreatePost';
 import Find from './find/HOCFind';
 import GoalOverview from './goals/goal-overview/HOCGoalOverview';
 import ActivityFeed from './goals/activity-feed/HOCActivityFeed';
-import Onboarding from './onboarding/HOCOnboarding';
 import MilestoneList from './milestones/milestone-list/HOCMilestoneList';
 import MilestoneOverview from './milestones/milestone-overview/HOCMilestoneOverview';
 import NotFound from './not-found/NotFound';
@@ -13,10 +18,7 @@ import Notifications from './notifications/HOCNotifications';
 import PostFeed from './posts/post-feed/HOCPostFeed';
 import PostView from './posts/post-view/HOCPostView';
 import Preview from './preview/HOCPreview';
-import Profile from './account/profile/HOCProfile';
-import Organization from './account/organization/HOCOrganization';
 import Search from './search/HOCSearch';
-import Services from './account/services/Services';
 import SideNote from './side-note/HOCSideNote';
 import TakeAction from './take-action/HOCTakeAction';
 
