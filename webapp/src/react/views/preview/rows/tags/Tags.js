22d2181932d712eeac676f7a10bd39b2d1829bd6
Kasper Pihl Tornøe <kasper@pihl.it>
2017-08-11 11:27:32 +0300

    removed warning for proptyper

--- webapp/src/react/views/preview/rows/tags/Tags.js
+++ webapp/src/react/views/preview/rows/tags/Tags.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 // import { map, list } from 'react-immutable-proptypes';
 
 import './styles/tags.scss';
