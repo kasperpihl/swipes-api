e640da942ae8a2c2566954d2caefbc26817f5da5
iremlopsum <kristjanfou@gmail.com>
2017-08-30 17:25:15 +0300

    empty state in welcomemessage

--- webapp/src/react/registration/WelcomeMessage.js
+++ webapp/src/react/registration/WelcomeMessage.js
@@ -12,8 +12,9 @@ class WelcomeMessage extends Component {
   render() {
     return (
       <div className="welcome-message">
-        <div className="welcome-message__paragraph">We are thrilled to have you join in and are committed to make this a great experience for you and your team! </div>
-        <div className="welcome-message__paragraph">Swipes brings together your team, communication, and files, all in one place. You can see what to work on next, you can pass on work to one another, give feedback and see how your part adds up to the big team effort.</div>
+        <div className="welcome-message__illustration"></div>
+        <div className="welcome-message__paragraph">This is the place for your team to communicate and create great work together.</div>
+        <div className="welcome-message__paragraph">We are thrilled to have you here!</div>
         <div className="welcome-message__signature">
           <Icon icon="SwipesSignature" className="welcome-message__svg" />
         </div>
