bf749b57102a98f08cf8cfb3b6906a97ee12d172
thinklinux <tihomir.valkanov@gmail.com>
2017-09-01 15:51:22 +0300

    new sofi picture

--- server/api/routes/middlewares/init.js
+++ server/api/routes/middlewares/init.js
@@ -29,10 +29,10 @@ const SOFI = {
   profile: {
     first_name: 'S.O.F.I.',
     photos: {
-      '192x192': 'https://s3.amazonaws.com/cdn.swipesapp.com/sofi/swipes-team-ui-ios-sofix2.png',
-      '64x64': 'https://s3.amazonaws.com/cdn.swipesapp.com/sofi/swipes-team-ui-ios-sofix2.png',
-      '96x96': 'https://s3.amazonaws.com/cdn.swipesapp.com/sofi/swipes-team-ui-ios-sofix2.png',
-      original: 'https://s3.amazonaws.com/cdn.swipesapp.com/sofi/swipes-team-ui-ios-sofix2.png',
+      '192x192': 'https://s3.amazonaws.com/cdn.swipesapp.com/sofi/swipes-sofi.png',
+      '64x64': 'https://s3.amazonaws.com/cdn.swipesapp.com/sofi/swipes-sofi.png',
+      '96x96': 'https://s3.amazonaws.com/cdn.swipesapp.com/sofi/swipes-sofi.png',
+      original: 'https://s3.amazonaws.com/cdn.swipesapp.com/sofi/swipes-sofi.png',
     },
   },
   services: [],
