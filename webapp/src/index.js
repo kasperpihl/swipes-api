2ed407832a042df2bc986cf6d274244db7dfe67c
Kasper Pihl Tornøe <kasper@pihl.it>
2017-10-02 11:54:43 +0300

    trying localforage

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -23,6 +23,27 @@ window.__DEV__ = (process.env.NODE_ENV !== 'production');
 window.__API_URL__ = `${location.origin}`;
 window.getURLParameter = name => decodeURIComponent((new RegExp(`[?|&]${name}=` + '([^&;]+?)(&|#|;|$)').exec(location.search) || [null, ''])[1].replace(/\+/g, '%20')) || null; // eslint-disable-line
 
+// Detect which browser and what version.
+window.sayswho = (function(){
+    var ua= navigator.userAgent, tem, 
+    M= ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+    if(/trident/i.test(M[1])){
+        tem=  /\brv[ :]+(\d+)/g.exec(ua) || [];
+        return 'IE '+(tem[1] || '');
+    }
+    if(M[1]=== 'Chrome'){
+        tem= ua.match(/\b(OPR|Edge)\/(\d+)/);
+        if(tem!= null) return tem.slice(1).join(' ').replace('OPR', 'Opera');
+    }
+    M= M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
+    if((tem= ua.match(/version\/(\d+)/i))!= null) M.splice(1, 1, tem[1]);
+    return M.join(' ');
+})().split(' ');
+window.__BROWSER__ = sayswho[0];
+window.__BROWSER_VERSION__ = sayswho[1];
+
+
+
 const store = configureStore();
 
 window.ipcListener = new IpcListener(store);
