c38bc09222f27b1517521cb1890d75594fbdf9cb
Kasper Pihl Tornøe <kasper@pihl.it>
2017-10-06 15:13:51 +0300

    added support for edge

--- webapp/src/react/Root.js
+++ webapp/src/react/Root.js
@@ -66,9 +66,9 @@ class Root extends PureComponent {
         <SuccessStateGradient />
         <HOCAutoCompleting />
         <HOCTooltip />
-        <HOCTrial />
         <DevTools />
         {this.renderRoutes()}
+        <Route path="/" component={HOCTrial} />
       </div>
     )
   }
