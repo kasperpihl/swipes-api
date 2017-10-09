5138071a23c54cdf82b449ca82809f41e3b23501
Kasper Pihl Tornøe <kasper@pihl.it>
2017-10-09 11:58:01 +0300

    added edge 10

--- webapp/src/utils/globals.js
+++ webapp/src/utils/globals.js
@@ -28,7 +28,7 @@ const isBrowserSupported = (browser, ver) => {
     case 'chrome': return ver >= 50;
     case 'safari': return ver >= 10;
     case 'firefox': return ver >= 50;
-    case 'edge': return ver >= 15;
+    case 'edge': return ver >= 10;
     default:
       return true;
   }
