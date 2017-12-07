0aa03688ccf00971fb39199760a3beb1378cb155
Kasper Pihl Tornøe <kasper@pihl.it>
2017-12-07 10:38:31 +0200

    added support for redirect to staging

--- server/index.js
+++ server/index.js
@@ -88,6 +88,17 @@ app.use('/v1', (req, res, next) => {
 // Get the config table into res.locals.config
 app.use('/v1', getConfig);
 app.use('/v1', (req, res, next) => {
+  let shouldRedirect = false;
+  if(res.locals.config.redirectToStaging) {
+    Object.entries(res.locals.config.redirectToStaging).forEach(([header, rVal]) => {
+      if(''+rVal === ''+req.header(`sw-${header}`)) {
+        shouldRedirect = true;
+      }
+    });
+    if(shouldRedirect) {
+      return res.redirect(307, 'https://staging.swipesapp.com/v1' + req.path);
+    }
+  }
   if(res.locals.config.maintenance) {
     return next(new SwipesError('maintenance', { maintenance: true }));
   }
