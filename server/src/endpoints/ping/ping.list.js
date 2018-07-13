54c442b1383c418189949e0bf7bb5fa70e97fa83
Kasper Pihl Tornøe <kasper@pihl.it>
2018-07-13 16:58:21 +0200

    giving up on joins rethinkdb. Subquery seems to be the only thing working properly

--- server/src/endpoints/ping/ping.list.js
+++ server/src/endpoints/ping/ping.list.js
@@ -28,11 +28,11 @@ export default endpointCreate({
   if(input.type === 'sent') {
     query = r.table('pings')
               .orderBy({ index: r.desc('sent_at') })
-              .slice(skip, skip + limit)
               .filter({
                 created_by: res.locals.user_id,
                 organization_id: input.organization_id,
               })
+              .slice(skip, skip + limit)
               .merge(ping => ({
                 receivers: r.table('ping_receivers')
                             .getAll(ping('id'), { index: 'ping_id' })
@@ -40,23 +40,22 @@ export default endpointCreate({
                             .coerceTo('array')
               }));
   } else {
-    // OMG RETHINKDB : A join counts on the skip and limit, so we have to double.......... facepalm
     query = r.table('ping_receivers')
               .orderBy({ index: r.desc('sent_at') })
-              .slice(skip * 2, skip * 2 + limit * 2)
               .filter({
                 received_by: res.locals.user_id,
                 organization_id: input.organization_id,
               })
-              .eqJoin('ping_id', r.table('pings'), { ordered: true })
-              .map((obj) => obj('right').merge({
-                receiver: obj('left'),
-              }))
+              .slice(skip, skip + limit)
               .merge(obj => ({
+                ping: r.table('pings').get(obj('ping_id')),
                 receivers: r.table('ping_receivers')
-                            .getAll(obj('id'), { index: 'ping_id' })
+                            .getAll(obj('ping_id'), { index: 'ping_id' })
                             .pluck('received_by', 'read_at', 'thanked_at')
                             .coerceTo('array')
+              }))
+              .map((obj) => obj('ping').merge({
+                receivers: obj('receivers'),
               }));
   }
 
