d5118da12a478c1c6873550c451a89e32c24c8aa
Kasper Pihl Tornøe <kasper@pihl.it>
2018-07-28 16:42:51 +0200

    removed swipes-core-js from mobile folder

--- mobile/swipes-core-js/actions/users.js
+++
@@ -1,7 +0,0 @@
-import * as ca from './';
-
-export const signup = obj => ca.api.request('users.signup', { ...obj });
-
-export const confirmEmail = confirmationToken => ca.api.request('users.confirmEmail', {
-  confirmation_token: confirmationToken,
-})
