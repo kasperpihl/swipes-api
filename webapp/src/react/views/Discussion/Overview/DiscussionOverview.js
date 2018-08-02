7f82550d167603f3ae717c08e6bd58a30b516802
Kasper Pihl Tornøe <kasper@pihl.it>
2018-08-02 16:30:23 +0200

    fixed keeping the scroll above

--- webapp/src/react/views/Discussion/Overview/DiscussionOverview.js
+++ webapp/src/react/views/Discussion/Overview/DiscussionOverview.js
@@ -25,15 +25,22 @@ export default class DiscussionOverview extends PureComponent {
     this.doScrollToBottom();
   }
   checkForScrollToBottom(props) {
+    // Before render, let's look at situation and determine scroll.
+    // Assume scroll to bottom
     this.shouldScrollToBottom = true;
     if(this.scroller) {
       const { scrollTop, clientHeight, scrollHeight } = this.scroller;
       const scrollPos = scrollHeight - clientHeight - scrollTop;
+      // If scrollPos is 0 (bottom), we should keep scroll to the bottom!
+      // If scrollPos is not 0 (not bottom)
       if(scrollPos > 0) {
+        // Do not auto scroll to bottom
         this.shouldScrollToBottom = false;
-        const first = props.pagination.results.first();
+        // Check if the first item has changed, then the new items is above
+        // and we should compensate for the scroll.
+        const first = props.pagination.results.last();
         if(first && first.get('id') !== this.oldestElementId) {
-          this.oldestElementId = props.pagination.results.getIn([0, 'id']);
+          this.oldestElementId = first.get('id');
           this.lastHeight = scrollHeight;
           this.lastScrollTop = scrollTop;
         }
@@ -46,6 +53,7 @@ export default class DiscussionOverview extends PureComponent {
       const { clientHeight, scrollHeight } = this.scroller;  
       this.scroller.scrollTop = scrollHeight - clientHeight;
     } else if(this.lastHeight) {
+      // Compensate for the new items above.
       const { clientHeight, scrollHeight } = this.scroller;
       this.scroller.scrollTop = this.lastScrollTop + (scrollHeight - this.lastHeight);
       this.lastHeight = undefined;
