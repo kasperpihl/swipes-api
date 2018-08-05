0f987e48f95c895714680a326660935823ff4333
Kasper Pihl Tornøe <kasper@pihl.it>
2018-08-05 09:20:26 +0200

    added swiss mixins, plugins and variables

---
+++ mobile/src/swiss/init.js
@@ -0,0 +1,11 @@
+import './variables/colors';
+
+import './mixins/_border';
+import './mixins/_borderRadius';
+import './mixins/_flex';
+import './mixins/_font';
+import './mixins/_margin';
+import './mixins/_padding';
+import './mixins/_size';
+
+import './plugins/hexToRGB';
