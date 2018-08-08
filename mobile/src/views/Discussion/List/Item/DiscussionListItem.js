81acf9286a0421e0449a572d73628db98562d5de
thinklinux <tihomir.valkanov@gmail.com>
2018-08-08 15:31:13 +0300

    DiscussionListItem - done

--- mobile/src/views/Discussion/List/Item/DiscussionListItem.js
+++ mobile/src/views/Discussion/List/Item/DiscussionListItem.js
@@ -1,6 +1,7 @@
 import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
 import * as ca from 'swipes-core-js/actions';
+import timeGetDayOrTime from 'swipes-core-js/utils/time/timeGetDayOrTime';
 import SplitImage from 'components/SplitImage/SplitImage';
 import RippleButton from 'RippleButton';
 import SW from './DiscussionListItem.swiss';
@@ -8,7 +9,7 @@ import SW from './DiscussionListItem.swiss';
 @connect(state => ({
   myId: state.me.get('id'),
 }), {
-  apiRequest: ca.api.request,
+  // apiRequest: ca.api.request,
 })
 export default class DiscussionListItem extends PureComponent {
   constructor(props) {
@@ -19,17 +20,40 @@ export default class DiscussionListItem extends PureComponent {
   }
 
   render() {
-    const { followers, topic, timestamp } = this.props;
+    const {
+      followers,
+      topic,
+      last_comment,
+      last_comment_at,
+      last_comment_by,
+      myId,
+    } = this.props;
+    const lastCommentByFirstName = msgGen.users.getFirstName(last_comment_by);
+    let unread = false;
+    const subscriber = followers.find(f => f.user_id === myId);
+    if(subscriber && (!subscriber.read_at || subscriber.read_at < last_comment_at)) {
+      unread = true;
+    }
 
     return (
-      <SW.Wrapper>
-        <SW.LeftSide>
-          <SplitImage followers={followers} size={40}></SplitImage>
-        </SW.LeftSide>
-        {/* <SW.RightSide>
-          {topic}
-        </SW.RightSide> */}
-      </SW.Wrapper>
+        <SW.Wrapper>
+          <SW.LeftSide>
+            <SplitImage followers={followers} size={40}></SplitImage>
+          </SW.LeftSide>
+          <SW.Middle>
+            <SW.LineOfText numberOfLines={1} topic unread={unread}>
+              {topic}
+            </SW.LineOfText>
+            <SW.LineOfText numberOfLines={1}>
+              {lastCommentByFirstName}: {last_comment}
+            </SW.LineOfText>
+          </SW.Middle>
+          <SW.RightSide>
+            <SW.LineOfText alignRight time unread={unread}>
+              {timeGetDayOrTime(last_comment_at)}
+            </SW.LineOfText>
+          </SW.RightSide>
+        </SW.Wrapper>
     );
   }
 }
--- mobile/src/views/Discussion/List/Item/DiscussionListItem.swiss.js
+++ mobile/src/views/Discussion/List/Item/DiscussionListItem.swiss.js
@@ -1,16 +1,51 @@
 import { styleSheet } from 'swiss-react';
 import { Text } from 'react-native';
+import { viewSize } from 'globalStyles';
 
 export default styleSheet('DiscussionListItem', {
   Wrapper: {
-    flex: '1',
-    flexDirection: 'column',
+    flex: 1,
+    flexDirection: 'row',
+    marginLeft: 15,
+    width: viewSize.width - 30,
   },
   LeftSide: {
-    padding: '10',
-    fontSize: '12',
+    paddingVertical: 10,
+    paddingRight: 10,
+    fontSize: 12,
+  },
+  Middle: {
+    flex: 1,
+    width: '100%',
+    paddingVertical: 10,
+    paddingRight: 10,
   },
   RightSide: {
+    flex: 0,
+    paddingVertical: 10,
+    width: 55,
+  },
+  LineOfText: {
     _el: Text,
+    fontSize: 12,
+    width: '100%',
+    color: '$sw2',
+    marginTop: 2,
+    topic: {
+      fontSize: 14,
+      color: '$sw1',
+      unread: {
+        fontWeight: 'bold',
+      },
+    },
+    alignRight: {
+      textAlign: 'right',
+    },
+    time: {
+      unread: {
+        color: '$sw1',
+        fontWeight: 'bold',
+      },
+    },
   },
 });
