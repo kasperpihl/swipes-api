1173b90fc599179d3f1f9de22b6f020e6b4aaff0
Kasper Pihl Tornøe <kasper@pihl.it>
2018-09-24 15:39:41 +0300

    development: fixed comment attachment to open

--- webapp/src/react/views/Comment/Item/CommentItem.js
+++ webapp/src/react/views/Comment/Item/CommentItem.js
@@ -1,23 +1,36 @@
 import React, { PureComponent } from 'react';
-import { withDelegate } from 'react-delegate';
+import { connect } from 'react-redux';
+
 import { attachmentIconForService } from 'swipes-core-js/classes/utils';
 import HOCAssigning from 'components/assigning/HOCAssigning';
 import Attachment from 'src/react/components/attachment/Attachment';
+import * as linkActions from 'src/redux/link/linkActions';
 
 import chain from 'src/utils/chain';
 import parseNewLines from 'src/utils/parseNewLines';
 import parseLinks from 'src/utils/parseLinks';
 import parseMentions from 'src/utils/parseMentions';
+import navWrapper from 'src/react/app/view-controller/NavWrapper';
 
 import CommentReaction from '../Reaction/CommentReaction';
 import SW from './CommentItem.swiss';
 
-@withDelegate(['onAttachmentClick'])
+@navWrapper
+@connect(
+  null,
+  {
+    preview: linkActions.preview,
+  }
+)
 export default class CommentItem extends PureComponent {
+  onAttachmentClick = att => e => {
+    const { preview, target } = this.props;
+    preview(target, att);
+  };
   renderAttachments() {
-    const { comment, onAttachmentClickCached } = this.props;
+    const { comment } = this.props;
 
-    if(!comment.get('attachments') || !comment.get('attachments').size) {
+    if (!comment.get('attachments') || !comment.get('attachments').size) {
       return undefined;
     }
     return (
@@ -26,15 +39,15 @@ export default class CommentItem extends PureComponent {
           <Attachment
             title={att.get('title')}
             key={i}
-            onClick={onAttachmentClickCached(i, att)}
+            onClick={this.onAttachmentClick(att)}
             icon={attachmentIconForService(att.getIn(['link', 'service']))}
           />
         ))}
       </SW.Attachments>
-    )
+    );
   }
   render() {
-    const { comment, postId } = this.props;
+    const { comment, postId } = this.props;
     const name = msgGen.users.getFullName(comment.get('sent_by'));
 
     return (
@@ -48,7 +61,9 @@ export default class CommentItem extends PureComponent {
             <SW.Timestamp prefix=" — " simple date={comment.get('sent_at')} />
           </SW.Name>
           <SW.Message>
-            {chain(parseNewLines, parseMentions, parseLinks)(comment.get('message'))}
+            {chain(parseNewLines, parseMentions, parseLinks)(
+              comment.get('message')
+            )}
           </SW.Message>
           {this.renderAttachments()}
         </SW.Content>
@@ -61,6 +76,6 @@ export default class CommentItem extends PureComponent {
           />
         </SW.Actions>
       </SW.Container>
-    )
+    );
   }
 }
