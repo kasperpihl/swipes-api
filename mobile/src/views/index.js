a270d1a6965f927e84bc28ea93589b74d83de2fb
thinklinux <tihomir.valkanov@gmail.com>
2018-10-01 17:12:01 +0300

    delete old posts files

--- mobile/src/views/index.js
+++ mobile/src/views/index.js
@@ -6,12 +6,9 @@ import Milestones from './milestones/HOCMilestones';
 import MilestoneOverview from './milestones/HOCMilestoneOverview';
 import NoMilestoneOverview from './no-milestone-overview/HOCNoMilestoneOverview';
 import PreviewNote from './preview-note/HOCPreviewNote';
-// import PostFeed from './posts/post-feed/HOCPostFeed';
 import DiscussionList from './Discussion/List/DiscussionList';
 import DiscussionOverview from './Discussion/Overview/DiscussionOverview';
 import HOCDiscussionCreate from './Discussion/Composer/HOCDiscussionCreate';
-// import PostView from './posts/post-view/HOCPostView';
-// import PostCreate from './posts/post-create/HOCPostCreate';
 import Notifications from './notifications/HOCNotifications';
 import Update from './update/HOCUpdate';
 import Search from './search/HOCSearch';
@@ -24,8 +21,6 @@ export {
   Milestones,
   MilestoneOverview,
   PreviewNote,
-  // PostView,
-  // PostCreate,
   Notifications,
   NoMilestoneOverview,
   Update,
