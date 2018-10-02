f9f569a81be42c3158a60e61d71879ea0da58509
Kasper Pihl Tornøe <kasper@pihl.it>
2018-10-02 15:01:00 +0300

    Projects: did the new reactions, for real!

--- server/src/endpoints/comment/comment.react.js
+++ server/src/endpoints/comment/comment.react.js
@@ -4,74 +4,96 @@ import endpointCreate from 'src/utils/endpointCreate';
 import dbRunQuery from 'src/utils/db/dbRunQuery';
 import dbSendUpdates from 'src/utils/db/dbSendUpdates';
 import dbUpdateQuery from 'src/utils/db/dbUpdateQuery';
-import mentionsMultiString from 'src/utils/mentions/mentionsMultiString';
+import dbInsertQuery from 'src/utils/db/dbInsertQuery';
 import mentionsClean from 'src/utils/mentions/mentionsClean';
-import dbSendNotifications from 'src/utils/db/dbSendNotifications';
-import dbClearNotifications from 'src/utils/db/dbClearNotifications';
 
 const expectedInput = {
   comment_id: string.require(),
   reaction: string,
 };
 
-export default endpointCreate({
-  endpoint: '/comment.react',
-  expectedInput,
-}, async (req, res) => {
-  // Get inputs
-  const { user_id } = res.locals;
-  const {
-    comment_id,
-    reaction,
-  } = res.locals.input;
+export default endpointCreate(
+  {
+    endpoint: '/comment.react',
+    expectedInput,
+  },
+  async (req, res) => {
+    // Get inputs
+    const { user_id, organization_id } = res.locals;
+    const { comment_id, reaction } = res.locals.input;
 
-  const q = dbUpdateQuery('comments', comment_id, {
-    reactions: {
-      [user_id]: reaction || r.literal(),
-    },
-  });
+    const q = dbUpdateQuery('comments', comment_id, {
+      reactions: {
+        [user_id]: reaction || r.literal(),
+      },
+    });
 
-  const commentRes = await dbRunQuery(q);
+    const commentRes = await dbRunQuery(q);
+    const comment = commentRes.changes[0].new_val;
 
-  // Create response data.
-  res.locals.output = {
-    updates: [
+    let updates = [
       {
         type: 'comment',
-        data: commentRes.changes[0].new_val,
+        data: comment,
       },
-    ],
-  };
-  res.locals.messageGroupId = comment_id;
-}).background(async (req, res) => {
+    ];
+
+    if (reaction) {
+      // Show like in the recent history.
+
+      const updateDiscussionQ = dbUpdateQuery(
+        'discussions',
+        comment.discussion_id,
+        {
+          last_comment_at: comment.updated_at,
+          last_comment: `loved the comment: ${mentionsClean(
+            comment.message
+          ).slice(0, 60)}...`,
+          last_comment_by: user_id,
+        }
+      );
+      // Updating read_at to be newest comment.
+      // Also ensuring that user follows discussion
+      const updateFollowerQ = dbInsertQuery(
+        'discussion_followers',
+        {
+          user_id,
+          id: `${comment.discussion_id}-${user_id}`,
+          discussion_id: comment.discussion_id,
+          read_at: comment.updated_at,
+          organization_id,
+        },
+        {
+          conflict: 'update',
+        }
+      );
+      const result = await Promise.all([
+        dbRunQuery(updateFollowerQ),
+        dbRunQuery(updateDiscussionQ),
+      ]);
+      const discussion = result[1].changes[0].new_val;
+
+      // Fetch the latest followers to ensure they're up to date.
+      const followersQ = r
+        .table('discussion_followers')
+        .getAll(discussion.id, { index: 'discussion_id' })
+        .pluck('user_id', 'read_at');
+
+      discussion.followers = await dbRunQuery(followersQ);
+      updates = [
+        {
+          type: 'discussion',
+          data: discussion,
+        },
+      ].concat(updates);
+    }
+
+    // Create response data.
+    res.locals.output = {
+      updates,
+    };
+    res.locals.messageGroupId = comment_id;
+  }
+).background(async (req, res) => {
   dbSendUpdates(res.locals);
-
-  const { organization_id, user_id } = res.locals;
-  const comment = res.locals.output.updates[0].data;
-
-  const reactionists = Object.keys(comment.reactions);
-
-  if (!reactionists.length) {
-    return dbClearNotifications(`${comment.sent_by}-${comment.id}-reaction`);
-  }
-
-  const mentionString = mentionsMultiString(reactionists, {
-    number: 2,
-    preferId: user_id,
-  });
-  if (reactionists.indexOf(user_id) > -1) {
-    [user_id].concat(reactionists.filter(u => u !== user_id));
-  }
-
-  dbSendNotifications({
-    id: `${comment.sent_by}-${comment.id}-reaction`,
-    user_id: comment.sent_by,
-    organization_id,
-    title: `${mentionString} loved your comment: ${mentionsClean(comment.message).slice(0, 60)}...`,
-    done_by: reactionists,
-    target: {
-      id: comment.discussion_id,
-      item_id: comment.id,
-    },
-  });
 });
