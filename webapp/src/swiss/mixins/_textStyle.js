e6b6a4c802e467eb8e7379f6180ca56107da0016
thinklinux <tihomir.valkanov@gmail.com>
2018-10-05 14:20:10 +0300

    updating _textStyle

--- webapp/src/swiss/mixins/_textStyle.js
+++ webapp/src/swiss/mixins/_textStyle.js
@@ -4,44 +4,44 @@ addMixin('textStyle', (props, style = 'item') => {
   switch (style) {
     case 'body':
       return {
-        _font: ['13px', '18px', 400],
+        _font: ['13px', '18px', 300],
         color: '$sw1',
       };
     case 'bodySubtitle':
       return {
-        _font: ['13px', '18px', 400],
+        _font: ['13px', '18px', 300],
         color: '$sw2',
       };
     case 'breadcrumb':
       return {
-        _font: ['13px', '18px', 400],
+        _font: ['13px', '18px', 300],
         color: '$sw2',
         textTransform: 'uppercase',
       };
     case 'caption':
       return {
-        _font: ['11px', '18px', 400],
+        _font: ['11px', '18px', 300],
         color: '$sw2',
       };
     case 'cardTitle':
       return {
-        _font: ['27px', '42px', 500],
+        _font: ['27px', '42px', 400],
         color: '$sw1',
       };
     case 'item':
       return {
-        _font: ['13px', '18px', 500],
+        _font: ['13px', '18px', 400],
         color: '$sw1',
       };
     case 'tabActive':
       return {
-        _font: ['11px', '24px', 500],
+        _font: ['11px', '24px', 400],
         color: '$sw1',
         textTransform: 'uppercase',
       };
     case 'tabInactive':
       return {
-        _font: ['11px', '24px', 500],
+        _font: ['11px', '24px', 400],
         color: '$sw2',
         textTransform: 'uppercase',
       };
@@ -50,7 +50,7 @@ addMixin('textStyle', (props, style = 'item') => {
         `unsupported textStyle: ${style}. Check _textStyle.js for support`
       );
       return {
-        _font: ['13px', '18px', 500],
+        _font: ['13px', '18px', 400],
         color: '$red',
       };
   }
