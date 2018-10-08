df65798ef87f7bac028cfbcd997d681f7ab2a6d8
Kasper Pihl Tornøe <kasper@pihl.it>
2018-10-08 15:25:49 +0300

    development: prettified update handler

--- server/src/middlewares/checkUpdates.js
+++ server/src/middlewares/checkUpdates.js
@@ -1,34 +1,5 @@
-import { version } from 'src/../package.json';
-
-import {
-  getDownloadLinks,
-} from 'src/_legacy-api/utils';
-
-const newerVersionExist = (client, server) => {
-  server = server || '0';
-  client = client || '0';
-
-  let newerVersion = false;
-  if (server.indexOf('.') > -1) {
-    const serverVals = server.split('.');
-    const clientVals = client.split('.');
-    let isBigger = false;
-    serverVals.forEach((serverVal, i) => {
-      const clientVal = parseInt(clientVals[i] || '0', 10);
-      serverVal = parseInt(serverVal || '0', 10);
-      if (clientVal > serverVal) {
-        isBigger = true;
-      }
-      if (serverVal > clientVal && !isBigger) {
-        newerVersion = true;
-      }
-    });
-  } else {
-    newerVersion = (parseInt(server, 10) > parseInt(client, 10));
-  }
-
-  return newerVersion;
-};
+import { getDownloadLinks } from 'src/_legacy-api/utils';
+import newerVersionExist from 'src/utils/newerVersionExist';
 
 const makeUpdateHandler = (res, next) => {
   let didRun = false;
@@ -58,17 +29,16 @@ const makeUpdateHandler = (res, next) => {
   return updateHandler;
 };
 
-
 export default (req, res, next) => {
-  const { versions } = res.locals.config;
+  const { versions } = res.locals.config;
 
   const handleUpdate = makeUpdateHandler(res, next);
 
-  const testHeaders = (prefix, url, ...headers) => {
+  const testHeaders = (prefix, url, ...headers) => {
     let required = false;
     let hasNewerVersion = false;
     let version = null;
-    headers.forEach((header) => {
+    headers.forEach(header => {
       const clientVersion = req.header(`sw-${header}`);
       const newestServerVersion = versions[`newest-${header}`];
       const requiredServerVersion = versions[`required-${header}`];
@@ -91,16 +61,21 @@ export default (req, res, next) => {
   const testReload = (...args) => testHeaders('reload', ...args);
   const testUpdate = (...args) => testHeaders('update', ...args);
 
-
   const platform = req.header('sw-platform');
   switch (platform) {
     case 'ios': {
-      testUpdate('itms-apps://itunes.apple.com/app/apple-store/id1250630942?mt=8', 'ios-build-number');
+      testUpdate(
+        'itms-apps://itunes.apple.com/app/apple-store/id1250630942?mt=8',
+        'ios-build-number'
+      );
       testReload(null, 'ios-code-push-version');
       break;
     }
     case 'android': {
-      testUpdate('market://details?id=com.swipesapp.release', 'android-build-number');
+      testUpdate(
+        'market://details?id=com.swipesapp.release',
+        'android-build-number'
+      );
       testReload(null, 'android-code-push-version');
       break;
     }
