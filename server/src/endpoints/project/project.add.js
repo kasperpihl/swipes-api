a1c67e4f823aa16ec5f3d8f9a9593c0bc89eb6d0
thinklinux <tihomir.valkanov@gmail.com>
2018-10-23 21:32:28 +0200

    project add endpoint

--- server/src/endpoints/project/project.add.js
+++ server/src/endpoints/project/project.add.js
@@ -1,49 +1,19 @@
-import r from 'rethinkdb';
-import { array, string, any } from 'valjs';
 import endpointCreate from 'src/utils/endpointCreate';
+import db from 'src/utils/db/dbPool';
 import idGenerate from 'src/utils/idGenerate';
-import dbInsertQuery from 'src/utils/db/dbInsertQuery';
-import dbSendUpdates from 'src/utils/db/dbSendUpdates';
-import dbRunQuery from 'src/utils/db/dbRunQuery';
 
-const expectedInput = {
-  title: string.require(),
-  privacy: any.of('public', 'private'),
-  notifyPeople: array.of(string),
-  organization_id: string.min(1).require(),
-};
+const expectedInput = {};
 
 export default endpointCreate(
   {
     endpoint: '/project.add',
-    expectedInput,
+    expectedInput
   },
   async (req, res, next) => {
-    // Get inputs
-    const { user_id } = res.locals;
-    const { title, privacy, organization_id } = res.locals.input;
-
-    const projectId = idGenerate('P', 15);
-
-    const projectQuery = dbInsertQuery('projects', {
-      organization_id,
-      created_at: r.now(),
-      id: projectId,
-      title,
-      created_by: user_id,
-      privacy: privacy || 'public',
-      archived: false,
-    });
-
-    const projectRes = await dbRunQuery(projectQuery);
-    const project = projectRes.changes[0].new_val;
+    // Add new project
+    await db('INSERT INTO projects (id) VALUES ($1)', [idGenerate('P')]);
 
     // Create response data.
-    res.locals.output = {
-      updates: [{ type: 'project', data: project }],
-    };
-    res.locals.messageGroupId = project.id;
+    res.locals.output = {};
   }
-).background(async (req, res) => {
-  dbSendUpdates(res.locals);
-});
+).background(async (req, res) => {});
