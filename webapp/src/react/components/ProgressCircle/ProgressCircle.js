af9bc33645639797ece4edda5a8098d978cd9a31
Kasper Pihl Tornoe <kasper@pihl.it>
2018-11-08 10:44:12 +0100

    Projects: initial empty files stepslider and progresscircle

---
+++ webapp/src/react/components/ProgressCircle/ProgressCircle.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import SW from './ProjectCircle.swiss';
+
+export default ProjectCircle (props) => {
+  return (
+    <SW.Wrapper />
+  );
+};
---
+++ webapp/src/react/components/StepSlider/StepSlider.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import SW from './StepSlider.swiss';
+
+export default StepSlider (props) => {
+  return (
+    <SW.Wrapper />
+  );
+};
