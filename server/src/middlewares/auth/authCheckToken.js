4cf1d3279a233cc60933b8917b1accf426872614
thinklinux <tihomir.valkanov@gmail.com>
2018-12-18 12:47:21 +0200

    fixing checking the token haha

--- server/src/middlewares/auth/authCheckToken.js
+++ server/src/middlewares/auth/authCheckToken.js
@@ -1,17 +1,17 @@
 import { query } from 'src/utils/db/db';
 
-export default (req, res, next) => {
-  const { user_id, dbToken } = res.locals;
+export default async (req, res, next) => {
+  const { user_id, token } = res.locals;
 
-  dbCheckToken({ user_id, token: dbToken })
-    .then(results => {
-      if (results.length === 0) {
-        throw Error('not_authed');
-      }
+  // Check user token
+  const tokenQ = await query(
+    'SELECT token FROM tokens WHERE user_id=$1 AND token=$2 AND revoked=$3',
+    [user_id, token, false]
+  );
 
-      return next();
-    })
-    .catch(e => {
-      throw Error('database_error');
-    });
+  if (tokenQ.rows.length === 0) {
+    throw Error('not_authed');
+  }
+
+  return next();
 };
