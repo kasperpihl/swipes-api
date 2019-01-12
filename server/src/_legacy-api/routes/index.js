b6a1f2d113438094ddf67dd14c1683b0f60d8c5e
Kasper Pihl Tornøe <kasper@pihl.it>
2019-01-12 18:50:28 +0100

    RIP: posts, goals, steps, attachments, onboarding, notifications, ways, stream, tokens... babam

--- server/src/_legacy-api/routes/index.js
+++ server/src/_legacy-api/routes/index.js
@@ -1,49 +1,25 @@
 import * as users from './users';
 import * as links from './links';
 import * as services from './services';
-import * as goals from './goals';
-import * as steps from './steps';
-import * as attachments from './attachments';
 import * as files from './files';
-import * as notifications from './notifications';
 import * as notes from './notes';
-import * as find from './find';
-import * as milestones from './milestones';
-import * as ways from './ways';
-import * as stream from './stream';
-import * as tokens from './tokens';
 import * as me from './me';
 import * as organizations from './organizations';
-import * as dashboard from './dashboard';
-import * as posts from './posts';
-import * as exporter from './export_plans_csv';
 
 const v1NotAuthed = [
   users.notAuthed,
   services.notAuthed,
-  stream.notAuthed,
   organizations.notAuthed,
-  dashboard.notAuthed,
   me.notAuthed
 ];
 const v1Authed = [
   links.authed,
   services.authed,
   users.authed,
-  goals.authed,
-  steps.authed,
-  attachments.authed,
   files.authed,
-  notifications.authed,
   notes.authed,
-  find.authed,
-  milestones.authed,
-  ways.authed,
-  tokens.authed,
   me.authed,
-  organizations.authed,
-  posts.authed,
-  exporter.authed
+  organizations.authed
 ];
 const v1Multipart = [me.multipart];
 
