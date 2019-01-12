b19168ae4e747973b73dd5b99db3e2354761008a
Kasper Pihl Tornøe <kasper@pihl.it>
2019-01-12 11:50:30 +0100

    organization.delete org wip

---
+++ server/src/endpoints/organization/organization.delete.js
@@ -0,0 +1,67 @@
+import { string } from 'valjs';
+import endpointCreate from 'src/utils/endpointCreate';
+import { transaction } from 'src/utils/db/db';
+import userOrganizationCheck from 'src/utils/userOrganizationCheck';
+
+const expectedInput = {
+  organization_id: string.require()
+};
+
+export default endpointCreate(
+  {
+    expectedInput
+  },
+  async (req, res) => {
+    const { user_id, input } = res.locals;
+    const { organization_id } = input;
+
+    // Ensure I have the rights to delete organization.
+    await userOrganizationCheck(user_id, organization_id, {
+      owner: true
+    });
+
+    // TODO: cancel stripe subscription if any.
+
+    // creating a new user from scratch
+    await transaction([
+      {
+        text: `
+          DELETE FROM organizations
+          WHERE organization_id = $1
+        `,
+        values: [organization_id]
+      },
+      {
+        text: `
+          DELETE FROM projects
+          WHERE owned_by = $1
+        `,
+        values: [organization_id]
+      },
+      {
+        text: `
+          DELETE FROM discussions
+          WHERE owned_by = $1
+        `,
+        values: [organization_id]
+      },
+      {
+        text: `
+          DELETE FROM permissions
+          WHERE owned_by = $1
+        `,
+        values: [organization_id]
+      },
+      {
+        text: `
+          DELETE FROM user_permissions
+          WHERE permission_to = $1
+        `,
+        values: [organization_id]
+      }
+    ]);
+
+    // Create response data.
+    res.locals.output = {};
+  }
+);
