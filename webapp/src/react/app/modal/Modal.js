cf31e285df861c20ea248fc54cb9bd42b1ef2071
Kasper Pihl Tornoe <kasper@pihl.it>
2019-01-15 20:18:56 +0100

    Projects: fixed modal clickable on the sides of background

--- webapp/src/react/app/modal/Modal.js
+++ webapp/src/react/app/modal/Modal.js
@@ -5,11 +5,14 @@ import * as mainActions from 'src/redux/main/mainActions';
 import SW from './Modal.swiss';
 import * as views from 'src/react/views';
 
-@connect((state, props) => ({
-  modal: state.main.getIn(['modals', props.target]),
-}), {
-  clear: mainActions.modal,
-})
+@connect(
+  (state, props) => ({
+    modal: state.main.getIn(['modals', props.target])
+  }),
+  {
+    clear: mainActions.modal
+  }
+)
 export default class HOCModal extends PureComponent {
   constructor(props) {
     super(props);
@@ -21,28 +24,31 @@ export default class HOCModal extends PureComponent {
     clear(target);
   }
   clickedBackground(e) {
-    const isBackground = e.target.classList.contains('modal');
+    const isBackground =
+      e.target.classList.contains('Modal_Container') ||
+      e.target.classList.contains('Modal_Content');
     if (isBackground) {
       this.hideModal();
     }
   }
   render() {
-    const { modal } = this.props;
-    
-    let Comp = modal && modal.component || null;
-    if(typeof Comp === 'string') {
+    const { modal } = this.props;
+
+    let Comp = (modal && modal.component) || null;
+    if (typeof Comp === 'string') {
       Comp = views[Comp];
     }
 
-    const props = modal && modal.props || {};
+    const props = (modal && modal.props) || {};
 
     return (
       <SW.Container
         className="modal"
-        position={modal && modal.position || 'center'}
+        position={(modal && modal.position) || 'center'}
         show={modal && !!modal.component}
-        onClick={this.clickedBackground}>
-        <SW.Content position={modal && modal.position || 'center'}>
+        onClick={this.clickedBackground}
+      >
+        <SW.Content position={(modal && modal.position) || 'center'}>
           {Comp && <Comp hideModal={this.hideModal} {...props} />}
         </SW.Content>
       </SW.Container>
