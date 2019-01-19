2dbb2c0c2b3c60af618cff3de4bd54c5f7b1b694
Kasper Pihl Tornøe <kasper@pihl.it>
2019-01-19 13:04:33 +0100

    Convert billingplanconfirm to use hooks

--- webapp/src/react/views/Billing/Plan/Confirm/BillingPlanConfirm.js
+++ webapp/src/react/views/Billing/Plan/Confirm/BillingPlanConfirm.js
@@ -1,48 +1,49 @@
-import React, { PureComponent } from 'react';
-import { setupLoading } from 'swipes-core-js/classes/utils';
+import React from 'react';
+import useLoader from 'src/react/_hooks/useLoader';
 import request from 'swipes-core-js/utils/request';
 import Button from 'src/react/components/Button/Button';
 import SW from './BillingPlanConfirm.swiss';
 
-export default class BillingPlanConfirm extends PureComponent {
-  constructor(props) {
-    super(props);
-    setupLoading(this);
-  }
-  handleConfirm = e => {
-    const { hideModal, plan, organizationId } = this.props;
-    this.setLoading('confirm');
+const BillingPlanConfirm = ({
+  hideModal,
+  plan,
+  organizationId,
+  currentPlan
+}) => {
+  const loader = useLoader();
+
+  const handleConfirm = e => {
+    loader.set('confirm');
 
     request('billing.updatePlan', { plan, organizationId }).then(res => {
       if (res.ok) {
-        this.clearLoading('confirm', 'Changed', 1500, () => {
+        loader.success('confirm', 'Changed', 1500, () => {
           if (hideModal) {
             hideModal();
           }
         });
       } else {
-        this.clearLoading('confirm', '!Error', 3000);
+        loader.error('confirm', '!Error', 3000);
       }
     });
   };
 
-  render() {
-    const { plan, currentPlan } = this.props;
-    let content = `You are about to change your billing plan from ${currentPlan} to ${plan}. Any unused time from the current subscription will be converted in credits that will be used for future payments.
+  const content = `You are about to change your billing plan from ${currentPlan} to ${plan}. Any unused time from the current subscription will be converted in credits that will be used for future payments.
     
     Click 'Confirm’ to change the plan.`;
 
-    return (
-      <SW.Wrapper>
-        <SW.ComposerWrapper>{content}</SW.ComposerWrapper>
-        <SW.ActionBar>
-          <Button
-            title="Confirm"
-            onClick={this.onConfirm}
-            {...this.getLoading('confirm')}
-          />
-        </SW.ActionBar>
-      </SW.Wrapper>
-    );
-  }
-}
+  return (
+    <SW.Wrapper>
+      <SW.ComposerWrapper>{content}</SW.ComposerWrapper>
+      <SW.ActionBar>
+        <Button
+          title="Confirm"
+          onClick={handleConfirm}
+          {...loader.get('confirm')}
+        />
+      </SW.ActionBar>
+    </SW.Wrapper>
+  );
+};
+
+export default BillingPlanConfirm;
