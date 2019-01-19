ed74bde485b3597e02ec6e3b0d60adf1ef1f8319
Kasper Pihl Tornøe <kasper@pihl.it>
2019-01-19 12:35:25 +0100

    refactor useLoader a little

--- webapp/src/react/_hooks/useLoader.js
+++ webapp/src/react/_hooks/useLoader.js
@@ -6,47 +6,6 @@ const useLoader = () => {
   const [loadingState, setLoadingState] = useState({});
   const timers = useRef({});
 
-  const startTimerIfNeeded = (loadingId, duration, callback) => {
-    if (!duration || !timers.current) {
-      return;
-    }
-
-    clearTimeout(timers.current[loadingId]);
-
-    timers.current[loadingId] = setTimeout(() => {
-      clear(loadingId);
-      if (typeof callback === 'function') {
-        callback();
-      }
-    }, duration);
-  };
-
-  const clear = loadingId => {
-    // unmounted
-    if (!timers.current) return;
-
-    clearTimeout(timers.current[loadingId]);
-    const newState = { ...loadingState };
-    delete newState[loadingId];
-    setLoadingState(newState);
-  };
-
-  const createUpdateHandler = (loadingKey, defaultLabel) => (
-    loadingId,
-    label = defaultLabel,
-    duration,
-    callback
-  ) => {
-    if (!timers.current) return;
-
-    const newState = Object.assign({}, loadingState, {
-      [loadingId]: { [loadingKey]: label }
-    });
-    setLoadingState(newState);
-
-    startTimerIfNeeded(loadingId, duration, callback);
-  };
-
   // Get a memoized object that only change when loading state change 💪
   const loader = useMemo(
     () => ({
@@ -69,6 +28,44 @@ const useLoader = () => {
       timers.current = null;
     };
   }, []);
+
+  function createUpdateHandler(loadingKey, defaultLabel) {
+    return (loadingId, label = defaultLabel, duration, callback) => {
+      if (!timers.current) return;
+
+      const newState = Object.assign({}, loadingState, {
+        [loadingId]: { [loadingKey]: label }
+      });
+      setLoadingState(newState);
+
+      startTimerIfNeeded(loadingId, duration, callback);
+    };
+  }
+
+  function startTimerIfNeeded(loadingId, duration, callback) {
+    if (!duration || !timers.current) {
+      return;
+    }
+
+    clearTimeout(timers.current[loadingId]);
+
+    timers.current[loadingId] = setTimeout(() => {
+      clear(loadingId);
+      if (typeof callback === 'function') {
+        callback();
+      }
+    }, duration);
+  }
+
+  function clear(loadingId) {
+    // unmounted
+    if (!timers.current) return;
+
+    clearTimeout(timers.current[loadingId]);
+    const newState = { ...loadingState };
+    delete newState[loadingId];
+    setLoadingState(newState);
+  }
 
   return loader;
 };
