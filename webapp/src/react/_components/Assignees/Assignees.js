27546e5b27d2783f4701e587faeec4a2519888bc
Kasper Pihl Tornøe <kasper@pihl.it>
2019-01-22 09:44:16 +0100

    Assignees component

---
+++ webapp/src/react/_components/Assignees/Assignees.js
@@ -0,0 +1,43 @@
+import React, { Fragment } from 'react';
+import UserImage from 'src/react/_components/UserImage/UserImage';
+import SW from './Assignees.swiss';
+
+export default function Assignees(props) {
+  let { organizationId, users, maxImages, size, onClick, children } = props;
+
+  maxImages = maxImages || 3;
+  size = size || 24;
+
+  const userCount = users.size || users.length || 0;
+  const imageCount = Math.min(userCount, maxImages);
+  const extraNumber = Math.max(userCount - maxImages, 0);
+
+  if (!userCount) {
+    return children || null;
+  }
+
+  return (
+    <SW.ProvideContext size={size} imageCount={imageCount}>
+      <SW.Wrapper onClick={onClick}>
+        <SW.AbsoluteWrapper>
+          {users.map((userId, i) =>
+            i < maxImages ? (
+              <Fragment key={i}>
+                <SW.WhiteBackground index={i} />
+                <SW.ImageWrapper index={i}>
+                  <UserImage
+                    userId={userId}
+                    organizationId={organizationId}
+                    size={size}
+                  />
+                </SW.ImageWrapper>
+              </Fragment>
+            ) : null
+          )}
+        </SW.AbsoluteWrapper>
+
+        {!!extraNumber && <SW.ExtraNumber>+{extraNumber}</SW.ExtraNumber>}
+      </SW.Wrapper>
+    </SW.ProvideContext>
+  );
+}
---
+++ webapp/src/react/_components/Assignees/Assignees.swiss.js
@@ -0,0 +1,43 @@
+import { styleSheet } from 'swiss-react';
+
+export default styleSheet('Assignees', {
+  Wrapper: {
+    _flex: ['row', 'right', 'center'],
+    flexDirection: 'row-reverse',
+    flex: 'none'
+  },
+  AbsoluteWrapper: {
+    width: get => `${(get('size') - 6) * get('imageCount') + 6}px`,
+    height: get => `${get('size')}px`
+  },
+  WhiteBackground: {
+    _size: get => `${get('size') + 2}px`,
+    position: 'absolute',
+    background: '$sw5',
+    borderRadius: get => `${(get('size') + 2) / 2}px`,
+    top: '-1px',
+    right: get => `${get('index') * (get('size') - 6) - 1}px`
+  },
+  ImageWrapper: {
+    _size: get => `${get('size')}px`,
+    _flex: 'center',
+    _borderRadius: get => `${get('size') / 2}px`,
+    position: 'absolute',
+    right: get => `${get('index') * (get('size') - 6)}px`,
+    top: 0,
+    overflow: 'hidden'
+  },
+
+  ExtraNumber: {
+    _font: ['11px', '18px', 500],
+    'size>=30': {
+      _font: ['12px', '18px', 500]
+    },
+    'size>=36': {
+      _font: ['13px', '18px', 500]
+    },
+    color: '$sw2',
+    letterSpacing: '-0.3px',
+    marginRight: '6px'
+  }
+});
