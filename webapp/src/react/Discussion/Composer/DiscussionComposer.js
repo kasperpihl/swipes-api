1a8f568d51ef826fd0701e7a95ba15aca48da9ce
Kasper Pihl Tornøe <kasper@pihl.it>
2019-01-22 10:26:13 +0100

    DiscussionComposer WIP

--- webapp/src/react/Discussion/Composer/DiscussionComposer.js
+++ webapp/src/react/Discussion/Composer/DiscussionComposer.js
@@ -1,16 +1,29 @@
 import React, { PureComponent } from 'react';
-import UserImage from 'src/react/_components/UserImage/UserImage';
+import { connect } from 'react-redux';
 import request from 'swipes-core-js/utils/request';
 import withLoader from 'src/react/_hocs/withLoader';
+import OrgPicker from 'src/react/_components/OrgPicker/OrgPicker';
+import Assignees from 'src/react/_components/Assignees/Assignees';
+import Button from 'src/react/_components/Button/Button';
 import SW from './DiscussionComposer.swiss';
+import FMSW from 'src/react/_components/FormModal/FormModal.swiss';
 import { fromJS } from 'immutable';
 
 @withLoader
+@connect(state => ({
+  myId: state.me.get('user_id'),
+  organization: state.organization
+}))
 export default class DiscussionComposer extends PureComponent {
-  state = {
-    topic: '',
-    followers: fromJS([])
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      topic: '',
+      publicChecked: true,
+      orgValue: props.myId,
+      followers: fromJS([])
+    };
+  }
 
   onAssigningClose(followers) {
     if (followers) {
@@ -39,6 +52,17 @@ export default class DiscussionComposer extends PureComponent {
       }
     });
   };
+  handlePrivacyChange = e => {
+    this.setState({
+      publicChecked: !this.state.publicChecked
+    });
+  };
+  handleOrgChange = orgValue => {
+    this.setState({
+      orgValue,
+      followers: fromJS([])
+    });
+  };
   handleTopicChange = e => {
     this.setState({
       topic: e.target.value
@@ -46,55 +70,71 @@ export default class DiscussionComposer extends PureComponent {
   };
   renderActionBar() {
     const { followers } = this.state;
-    const buttonProps = !!followers.size
-      ? {
-          compact: true
-        }
-      : {
-          sideLabel: 'Tag'
-        };
+    const { loader } = this.props;
 
     return (
       <SW.ActionBar>
-        <SW.AssignSection>
-          <HOCAssigning
-            assignees={followers}
-            delegate={this}
-            size={24}
-            buttonProps={buttonProps}
-            maxImages={9}
-          />
-        </SW.AssignSection>
+        <SW.AssignSection />
         <SW.Seperator />
-        <SW.PostButton
-          title="Post"
-          onClick={this.handlePostSubmit}
-          status={loader.get('discussion')}
-        />
+        <SW.PostButton />
       </SW.ActionBar>
     );
   }
-
   render() {
-    const { topic } = this.state;
+    const { topic, orgValue, followers, publicChecked } = this.state;
+    const { myId, loader } = this.props;
     const topicPlaceholder = 'Topic';
 
     return (
-      <SW.Wrapper>
-        <SW.ComposerWrapper>
-          <UserImage userId="me" size={36} />
-          <SW.InputWrapper>
-            <SW.Input
+      <FMSW.Wrapper>
+        <FMSW.Title>New Discussion</FMSW.Title>
+        <FMSW.InputContainer>
+          <FMSW.InputWrapper>
+            <FMSW.Label>1. Set the topic</FMSW.Label>
+            <FMSW.Input
               value={topic}
               onChange={this.handleTopicChange}
               placeholder={topicPlaceholder}
               type="text"
               autoFocus
             />
-          </SW.InputWrapper>
-        </SW.ComposerWrapper>
-        {this.renderActionBar()}
-      </SW.Wrapper>
+          </FMSW.InputWrapper>
+          <FMSW.InputWrapper>
+            <FMSW.Label>2. Choose belonging</FMSW.Label>
+            <OrgPicker value={orgValue} onChange={this.handleOrgChange} />
+          </FMSW.InputWrapper>
+          {orgValue !== myId && (
+            <>
+              <FMSW.InputWrapper>
+                <FMSW.Label>3. Choose privacy</FMSW.Label>
+                <SW.CheckboxWrapper onClick={this.handlePrivacyChange}>
+                  <input
+                    onChange={this.handlePrivacyChange}
+                    type="checkbox"
+                    checked={publicChecked}
+                  />
+                  <SW.CheckboxValue>
+                    Public - anyone in the organization join the discussion
+                  </SW.CheckboxValue>
+                </SW.CheckboxWrapper>
+              </FMSW.InputWrapper>
+              <FMSW.InputWrapper>
+                <FMSW.Label>4. Tag people</FMSW.Label>
+                <Assignees users={followers} size={24} maxImages={9}>
+                  <Button.Standard title="Assign People" />
+                </Assignees>
+              </FMSW.InputWrapper>
+            </>
+          )}
+        </FMSW.InputContainer>
+        <FMSW.ButtonWrapper>
+          <Button.Rounded
+            title="Create discussion"
+            onClick={this.handlePostSubmit}
+            status={loader.get('discussion')}
+          />
+        </FMSW.ButtonWrapper>
+      </FMSW.Wrapper>
     );
   }
 }
--- webapp/src/react/_components/OrgPicker/OrgPicker.swiss.js
+++ webapp/src/react/_components/OrgPicker/OrgPicker.swiss.js
@@ -2,11 +2,15 @@ import { styleSheet } from 'swiss-react';
 
 export default styleSheet('OrgPicker', {
   Wrapper: {
-    _flex: ['column', 'left', 'top']
+    _flex: ['row', 'left', 'center'],
+    flexWrap: 'wrap'
   },
   InputWrapper: {
     _flex: ['row', 'left', 'center'],
-    padding: '3px 0'
+    padding: '6px',
+    '&:first-child': {
+      paddingLeft: 0
+    }
   },
   Label: {
     _el: 'span',
