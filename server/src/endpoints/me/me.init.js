765da65eb7722c152ad2304562caaebaf9b102cd
Kasper Pihl Tornoe <kasper@pihl.it>
2019-02-02 09:26:53 +0100

    cleanup init and add unread as object

--- server/src/endpoints/me/me.init.js
+++ server/src/endpoints/me/me.init.js
@@ -1,15 +1,15 @@
-import { bool, string, object } from 'valjs';
+import { object, array } from 'valjs';
 import endpointCreate from 'src/utils/endpoint/endpointCreate';
+import sqlCheckPermissions from 'src/utils/sql/sqlCheckPermissions';
 import { query } from 'src/utils/db/db';
 import sofiCreate from 'src/utils/sofiCreate';
 
-const expectedInput = {
-  timestamp: string.format('iso8601')
-};
+const expectedInput = {};
 const expectedOutput = {
   me: object.require(),
-  timestamp: string.format('iso8601').require(),
-  full_fetch: bool.require(),
+  users: array.require(),
+  organizations: array.require(),
+  unread: object.require(),
   sofi: object.require()
 };
 
@@ -20,10 +20,7 @@ export default endpointCreate(
   },
   async (req, res) => {
     const { user_id } = res.locals;
-
-    const { timestamp } = res.locals.input;
-
-    const full_fetch = !timestamp;
+    const now = new Date().toISOString();
 
     const meRes = await query(
       `
@@ -65,44 +62,32 @@ export default endpointCreate(
       `,
       [user_id]
     );
-    // const discussionTs = user.settings.discussion_counter_ts || defTs;
-    // const discussionCounterQ = r
-    //   .table('discussions')
-    //   .orderBy({ index: r.desc('last_comment_at') })
-    //   .filter(disc =>
-    //     disc('organization_id')
-    //       .eq(organization_id)
-    //       .and(
-    //         r.table('discussion_followers').get(disc('id').add(`-${user_id}`))
-    //       )
-    //       .and(disc('archived').eq(false))
-    //       .and(disc('last_comment_at').gt(r.ISO8601(discussionTs)))
-    //   )
-    //   .pluck('id', 'last_comment_at')
-    //   .map(obj => ({
-    //     id: obj('id'),
-    //     ts: obj('last_comment_at')
-    //   }))
-    //   .limit(10)
-    //   .coerceTo('ARRAY');
 
-    const now = new Date().toISOString();
-
-    // const counter = {
-    //   discussion: result[4],
-    //   discussionTs: user.settings.discussion_counter_ts || null,
-    //   myId: user_id
-    // };
+    const unreadRes = await query(
+      `
+        SELECT d.discussion_id, d.last_comment_at
+        FROM permissions as per
+        INNER JOIN discussions as d
+        ON d.discussion_id = per.permission_from
+        WHERE ${sqlCheckPermissions('per.granted_to', user_id)}
+        AND d.followers->>$1 IS NOT NULL
+        AND d.deleted=FALSE
+        AND d.followers->>$1 = 'n'
+        OR d.followers->>$1 < to_char(d.last_comment_at::timestamp at time zone 'UTC', 'YYYY-MM-DD"T"HH24:MI:SS.MS"Z"')
+      `,
+      [user_id]
+    );
+    const unread = {};
+    unreadRes.rows.forEach(r => (unread[r.discussion_id] = r.last_comment_at));
 
     // Create response data.
     res.locals.output = {
       me: meRes.rows[0],
       organizations: orgRes.rows,
-      users: usersRes.rows,
-      sofi: sofiCreate(),
-      full_fetch,
+      unread,
       timestamp: now,
-      unread: 0
+      users: usersRes.rows,
+      sofi: sofiCreate()
     };
   }
 );
