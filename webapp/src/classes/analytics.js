8262a27df9584a53aff439562663b3756d1ef11b
Kasper Pihl Tornoe <kasper@pihl.it>
2019-02-07 10:16:02 +0100

    removing intercom

--- webapp/src/classes/analytics.js
+++ webapp/src/classes/analytics.js
@@ -1,4 +1,3 @@
-/* global Intercom */
 import mixpanel from 'mixpanel-browser';
 const blockedMixpanelEvents = [];
 
@@ -9,9 +8,6 @@ export default class Analytics {
     if (this.enable) {
       // amplitude.getInstance().init("fea8942630d7141403673df1c646ecc7");
       mixpanel.init('a1b6f31fc988c7e4a7f40c267e315f5d');
-      Intercom('boot', {
-        app_id: 'q8xibmac'
-      });
     }
     this.store = store;
     this.userId = null;
@@ -36,7 +32,6 @@ export default class Analytics {
   }
   logout() {
     if (this.enable) {
-      Intercom('shutdown');
       mixpanel.reset();
     }
   }
@@ -44,7 +39,6 @@ export default class Analytics {
     const defs = this.getDefaultEventProps();
     if (this.enable) {
       const props = Object.assign(defs, data);
-      Intercom('trackEvent', name, props);
       if (blockedMixpanelEvents.indexOf(name) === -1) {
         mixpanel.track(name, props);
       } else {
@@ -57,26 +51,7 @@ export default class Analytics {
 
     if (me && me.get('id') && me.get('id') !== this.userId) {
       this.userId = me.get('id');
-      this.orgId = me.getIn(['organizations', 0, 'id']);
       if (this.enable) {
-        const intercomObj = {
-          name: msgGen.users.getFullName(me),
-          'Is admin': msgGen.me.isAdmin(),
-          'Is paying': msgGen.me.isPaying(),
-          user_id: me.get('id'),
-          email: msgGen.users.getEmail(me),
-          created_at: me.get('created_at')
-        };
-        const org = me.getIn(['organizations', 0]);
-
-        if (org) {
-          intercomObj.company = {
-            id: org.get('id'),
-            name: org.get('name'),
-            created_at: org.get('created_at')
-          };
-        }
-        Intercom('update', intercomObj);
         mixpanel.identify(this.userId);
       }
     }
