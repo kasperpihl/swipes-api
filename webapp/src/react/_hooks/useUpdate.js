3e29295abdb062bbddaa82e0a8532cdcd10013cb
Kasper Pihl Tornoe <kasper@pihl.it>
2019-02-17 10:05:01 +0100

    Remove logs from useUpdate

--- webapp/src/react/_hooks/useUpdate.js
+++ webapp/src/react/_hooks/useUpdate.js
@@ -2,17 +2,12 @@ import { useEffect } from 'react';
 
 export default function useUpdate(handler) {
   useEffect(() => {
-    console.log('subscribing');
     if (typeof window === 'undefined' || typeof window.socket === 'undefined') {
       console.warn('useUpdate expects window and window.socket to be set');
       return;
     }
-    const unsub = window.socket.subscribe(update => {
+    return window.socket.subscribe(update => {
       handler(update);
     });
-    return () => {
-      console.log('unsubscribing');
-      unsub();
-    };
   }, []);
 }
