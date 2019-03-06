cdb1e5b137268a1ee1b2abea65903cb605795c15
Kasper Pihl Tornøe <kasper@pihl.it>
2019-03-06 10:15:32 +0100

    development: default select org if personal is not available

--- webapp/src/react/_components/OrgPicker/OrgPicker.js
+++ webapp/src/react/_components/OrgPicker/OrgPicker.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useRef, useEffect } from 'react';
 import { connect } from 'react-redux';
 import cachedCallback from 'src/utils/cachedCallback';
 import SW from './OrgPicker.swiss';
@@ -10,23 +10,37 @@ export default connect(state => ({
 
 function OrgPicker({ organizations, value, onChange, disablePersonal }) {
   const myId = useContext(MyIdContext);
+  const foundCheckedRef = useRef(false);
   const handleClickCached = cachedCallback((v, e) => {
     e.stopPropagation();
     onChange(v);
   });
 
-  const renderInput = (myValue, title) => (
-    <SW.InputWrapper key={myValue} onClick={handleClickCached(myValue)}>
-      <input
-        type="radio"
-        value={myValue}
-        checked={myValue === value}
-        name="org"
-        onChange={handleClickCached(myValue)}
-      />
-      <SW.Label>{title}</SW.Label>
-    </SW.InputWrapper>
-  );
+  useEffect(() => {
+    if (!foundCheckedRef.current) {
+      onChange(organizations.first().get('organization_id'));
+    }
+  });
+
+  foundCheckedRef.current = false;
+  const renderInput = (myValue, title) => {
+    const checked = myValue === value;
+    if (checked) {
+      foundCheckedRef.current = true;
+    }
+    return (
+      <SW.InputWrapper key={myValue} onClick={handleClickCached(myValue)}>
+        <input
+          type="radio"
+          value={myValue}
+          checked={myValue === value}
+          name="org"
+          onChange={handleClickCached(myValue)}
+        />
+        <SW.Label>{title}</SW.Label>
+      </SW.InputWrapper>
+    );
+  };
 
   return (
     <SW.Wrapper>
