9836f0474b4dc7606d22520441dc325fe1cb740f
Kasper Pihl Tornøe <kasper@pihl.it>
2019-03-18 14:33:52 +0100

    development: parseGiphy utils

--- webapp/src/utils/parseGiphys.js
+++ webapp/src/utils/parseGiphys.js
@@ -1,4 +1,21 @@
 import React from 'react';
-export default function(link) {
-  return <img src={`${link}`} style={{ width: '200px' }} />;
+
+export default function parseGiphys(string) {
+  if (!Array.isArray(string)) {
+    string = [string];
+  }
+  let finalArr = [];
+  let didMatch = false;
+  string.forEach(s => {
+    if (typeof s === 'string') {
+      const regex = /<!giphy*\|(.*)\|(.*)>/gm;
+      const match = regex.exec(string);
+      if (string && match) {
+        finalArr.push(<img src={match[1]} style={{ width: '200px' }} />);
+        didMatch = true;
+      }
+    }
+  });
+
+  return didMatch ? finalArr : string;
 }
