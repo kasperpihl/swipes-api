994b96aecb9fa0f70186570e3d959c7857137171
Kasper Pihl Tornøe <kasper@pihl.it>
2019-03-21 15:20:15 +0200

    development: tweaks to onboarding

--- server/src/utils/sql/sqlOnboardingDiscussionQueries.js
+++ server/src/utils/sql/sqlOnboardingDiscussionQueries.js
@@ -1,5 +1,6 @@
 import idGenerate from 'src/utils/idGenerate';
 import sqlInsertQuery from 'src/utils/sql/sqlInsertQuery';
+import sqlToIsoString from 'src/utils/sql/sqlToIsoString';
 import sqlPermissionInsertQuery from 'src/utils/sql/sqlPermissionInsertQuery';
 
 export default function sqlOnboardingDiscussionQueries(organizationId, userId) {
@@ -12,7 +13,7 @@ export default function sqlOnboardingDiscussionQueries(organizationId, userId) {
     last_comment_by: userId,
     last_comment: 'just created this discussion.',
     last_comment_at: 'now()',
-    followers: `jsonb_build_object('${userId}', 'n')`
+    followers: `jsonb_build_object('${userId}', ${sqlToIsoString('now()')})`
   }));
 
   return [
