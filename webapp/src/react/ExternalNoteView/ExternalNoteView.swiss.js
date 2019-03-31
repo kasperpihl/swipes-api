d82b15073e18efd71437b2d1e7be508d160eb468
thinklinux <tihomir.valkanov@gmail.com>
2019-03-31 17:08:47 +0300

    trying to fix the stupid note

--- webapp/src/react/ExternalNoteView/ExternalNoteView.swiss.js
+++ webapp/src/react/ExternalNoteView/ExternalNoteView.swiss.js
@@ -3,12 +3,14 @@ import { styleSheet } from 'swiss-react';
 export default styleSheet('ExternalNoteView', {
   Wrapper: {
     padding: '0 15px',
-    width: '100%',
+    _size: ['100%'],
+    flex: 1,
+    _flex: ['column', 'center', 'top']
   },
   Loading: {
     position: 'absolute',
     left: '50%',
     top: '50%',
-    transform: 'translateY(-50%) translateX(-50%)',
-  },
+    transform: 'translateY(-50%) translateX(-50%)'
+  }
 });
