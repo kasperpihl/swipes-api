a63c5f921c7dc93eae4b348b74f852329a866638
Kasper Pihl Tornoe <kasper@pihl.it>
2019-03-31 16:54:46 +0200

    development: fixing the innocent note

--- webapp/src/react/ExternalNoteView/ExternalNoteView.swiss.js
+++ webapp/src/react/ExternalNoteView/ExternalNoteView.swiss.js
@@ -4,8 +4,7 @@ export default styleSheet('ExternalNoteView', {
   Wrapper: {
     padding: '0 15px',
     _size: ['100%'],
-    flex: 1,
-    _flex: ['column', 'center', 'top']
+    overflowY: 'auto'
   },
   Loading: {
     position: 'absolute',
