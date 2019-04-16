434b6b1b00e5df78af5ed278bfde9e75edebd27f
Pepo Dyakov <petar.dyakov@icloud.com>
2019-04-16 11:53:16 +0300

    refactored code for gettign next payment date

--- webapp/src/react/Billing/Payment/Active/BillingPaymentActive.js
+++ webapp/src/react/Billing/Payment/Active/BillingPaymentActive.js
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import moment from 'moment';
 
-import request from 'core/utils/request';
+import useRequest from 'core/react/_hooks/useRequest';
 
 import BillingPaymentModal from 'src/react/Billing/Payment/Modal/BillingPaymentModal';
 import Button from 'src/react/_components/Button/Button';
@@ -11,43 +11,43 @@ import Loader from '_shared/loaders/Loader';
 import SW from './BillingPaymentActive.swiss';
 
 export default function BillingPaymentActive({ openModal, team }) {
-  const [nextPaymentDate, changeNextPaymentDate] = useState();
   const handleCardDetails = () => {
     openModal(BillingPaymentModal, {
       teamId: team.get('team_id')
     });
   };
-  useEffect(() => {
-    request('billing.get', {
-      team_id: team.get('team_id')
-    }).then(res => {
-      if (res.customer) {
-        const periodStart =
-          res.customer.subscriptions.data[0].current_period_end;
-        changeNextPaymentDate(moment(periodStart).format('MMM DD, YYYY'));
-      }
-    });
-  }, [nextPaymentDate]);
 
-  if (typeof nextPaymentDate === 'undefined') {
+  const req = useRequest('billing.get', {
+    team_id: team.get('team_id')
+  });
+
+  if (typeof req.result === 'undefined') {
     return (
       <SW.Wrapper loading>
         <Loader size={36} />
       </SW.Wrapper>
     );
+  } else {
+    const periodStart =
+      req.result.customer.subscriptions.data[0].current_period_end;
+    const nextPaymentDate = moment(periodStart).format('MMM DD, YYYY');
+    return (
+      <SW.Wrapper>
+        <SW.Title>Thank you for your purchase.</SW.Title>
+        <Spacing height={9} />
+        <SW.StatusWrapper>
+          <SW.StatusLabel>Your subscription status is:</SW.StatusLabel>
+          <SW.Status>Active</SW.Status>
+        </SW.StatusWrapper>
+        <Spacing height={9} />
+        <SW.NextPaymentDate>{`Your next payment is due ${nextPaymentDate}`}</SW.NextPaymentDate>
+        <Spacing height={9} />
+        <Button
+          title="Change card details"
+          onClick={handleCardDetails}
+          border
+        />
+      </SW.Wrapper>
+    );
   }
-  return (
-    <SW.Wrapper>
-      <SW.Title>Thank you for your purchase.</SW.Title>
-      <Spacing height={9} />
-      <SW.StatusWrapper>
-        <SW.StatusLabel>Your subscription status is:</SW.StatusLabel>
-        <SW.Status>Active</SW.Status>
-      </SW.StatusWrapper>
-      <Spacing height={9} />
-      <SW.NextPaymentDate>{`Your next payment is due ${nextPaymentDate}`}</SW.NextPaymentDate>
-      <Spacing height={9} />
-      <Button title="Change card details" onClick={handleCardDetails} border />
-    </SW.Wrapper>
-  );
 }
